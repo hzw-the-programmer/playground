6de3f85800a755c37710f90678a48b166851be58
hezhiwen <hezhiwen.coder@bytedance.com>
2021-08-03 15:07:54 +0800

    rsa

---
+++ go/rsa/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"fmt"
+	"crypto"
+)
+
+func main() {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		panic(err)
+	}
+	publicKey := privateKey.PublicKey
+
+	encryptedBytes, err := rsa.EncryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		&publicKey,
+		[]byte("super secret message"),
+		nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("encrypted bytes: ", encryptedBytes)
+
+	decryptedBytes, err := privateKey.Decrypt(nil, encryptedBytes, &rsa.OAEPOptions{Hash: crypto.SHA256})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("decrypted message: ", string(decryptedBytes))
+}
