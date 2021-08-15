d7d12cc78be1371014bc3db90c9887bce50718ed
hzw <hzw@home>
2021-08-15 09:53:26 +0800

    rust thread

--- rust/thread/src/main.rs
+++ rust/thread/src/main.rs
@@ -2,12 +2,16 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
+    let v = vec![10, 9, 8, 7];
+    let handle = thread::spawn(move || {
+        println!("child: {:?}", v);
         for i in 1..10 {
             println!("child: {}", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
+
+    //drop(v);
 
     for i in 1..5 {
         println!("main: {}", i);
