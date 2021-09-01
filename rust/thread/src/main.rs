08d2fd1eedab5938940c242f4c127705be153b17
hzw <hzw@home>
2021-09-01 23:48:08 +0800

    rust

--- rust/thread/src/main.rs
+++ rust/thread/src/main.rs
@@ -1,10 +1,13 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, Barrier};
+use threadpool::ThreadPool;
+use std::sync::mpsc::channel;
 
 fn main() {
     // t1();
-    t2();
+    // t2();
+    t3();
 }
 
 fn t1() {
@@ -46,4 +49,19 @@ fn t2() {
         handle.join().unwrap();
     }
     println!("j = {}", j);
+}
+
+fn t3() {
+    let n_workers = 4;
+    let n_jobs = 8;
+    let pool = ThreadPool::new(n_workers);
+    
+    let (tx, rx) = channel();
+    for _ in 0..n_jobs {
+        let tx = tx.clone();
+        pool.execute(move || {
+            tx.send(1).expect("channel will be there waiting for pool");
+        });
+    }
+    println!("{}", rx.iter().take(n_jobs).fold(0, |a, b| a + b));
 }
