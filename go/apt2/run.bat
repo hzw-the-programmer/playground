8e546ad9bdd0dcba91af63fd1ec533889545bbfb
hezhiwen <hezhiwen.coder@bytedance.com>
2021-10-12 11:42:01 +0800

    refactor

--- go/apt2/run.bat
+++ go/apt2/run.bat
@@ -6,12 +6,12 @@ REM SET vc=%prj%\MoDIS_VC9\app\Debug
 SET arm=%prj%\MS_Code\build\PRJ_builddir\obj\app
 SET vc=%prj%\MS_Code\build\PRJ_builddir\win\app\Debug
 SET first_commit_file=%prj%\
-SET provide=provide.txt
-SET exclude=exclude.txt
+SET provide=provide\provide.txt
+SET exclude=exclude\exclude.txt
 SET libout=
 SET provideout=
 
-apppkg.exe ^
+bin\apppkg.exe ^
          -prjdir=%prj% ^
          -appdir=%app% ^
          -metainfofn=%app%\ra_feature_def.h ^
