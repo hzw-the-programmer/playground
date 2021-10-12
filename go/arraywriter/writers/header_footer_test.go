85f26ae0b14547026c9c93bbe8c8cb35d85cc119
hezhiwen <hezhiwen.coder@bytedance.com>
2021-10-12 20:36:54 +0800

    writers

--- go/arraywriter/writers/header_footer_test.go
+++ go/arraywriter/writers/header_footer_test.go
@@ -3,29 +3,55 @@ package writers
 import (
 	"bytes"
 	"testing"
+	"fmt"
+	"io"
 )
 
 func TestHeaderFooter(t *testing.T) {
-	cols := 7
-	header := "static const unsigned char en = {\n"
-	content := "hello world!"
-	footer := "\n};"
-	want := `static const unsigned char en = {
+	tests := []struct {
+		header  string
+		content string
+		footer  string
+		cols    int
+		hex     bool
+		want    string
+	}{
+		{
+			header:  "static const unsigned char en = {\n",
+			content: "hello world!",
+			footer:  "\n};",
+			cols:    7,
+			hex:     true,
+			want: `static const unsigned char en = {
     0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77,
     0x6f, 0x72, 0x6c, 0x64, 0x21,
-};`
+};`,
+		},
+	}
 
-	var buf bytes.Buffer
-	hex := NewHex(&buf, cols)
-	width := NewWidth(&buf, hex, cols)
-	w := NewHeaderFooter(&buf, width, header, footer)
+	for i, test := range tests {
+		name := fmt.Sprintf("%d", i)
+		t.Run(name, func(t *testing.T) {
+			var buf bytes.Buffer
+			var hex io.Writer
+			var width io.Writer
 
-	w.Write([]byte(content))
-	w.Close()
+			hex = &buf
+			if test.hex {
+				hex = NewHex(&buf, test.cols)
+			}
 
-	got := buf.String()
+			width = NewWidth(&buf, hex, test.cols)
+			w := NewHeaderFooter(&buf, width, test.header, test.footer)
 
-	if got != want {
-		t.Errorf("\ngot:\n%s\nwant:\n%s", got, want)
+			w.Write([]byte(test.content))
+			w.Close()
+
+			got := buf.String()
+
+			if got != test.want {
+				t.Errorf("\ngot:\n%s\nwant:\n%s", got, test.want)
+			}
+		})
 	}
 }
