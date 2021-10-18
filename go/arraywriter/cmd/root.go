e4cbe7ab51c5c82f455b9b6a54092d1a310aee82
hezhiwen <hezhiwen.coder@bytedance.com>
2021-10-18 18:14:54 +0800

    initConfig

--- go/arraywriter/cmd/root.go
+++ go/arraywriter/cmd/root.go
@@ -44,6 +44,8 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	fmt.Println("root.Execute")
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -68,6 +70,8 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+	fmt.Println("root.initConfig")
+
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
