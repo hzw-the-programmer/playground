433bdc228aad4b185e40c630217d1a08a774f43a
hzw <hzw@home>
2022-04-15 22:07:52 +0800

    rust reqwest download

---
+++ rust/reqwest1/src/bin/download.rs
@@ -0,0 +1,36 @@
+use tempfile::Builder;
+use std::io::copy;
+use std::fs::File;
+
+mod errors {
+    use error_chain::error_chain;
+    error_chain! {
+        foreign_links {
+            Io(std::io::Error);
+            HttpRequest(reqwest::Error);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> errors::Result<()> {
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    //let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let target = "https://httpbin.org/ip";
+    let resp = reqwest::get(target).await?;
+    let mut dest = {
+        let fname = resp
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() {None} else {Some(name)})
+            .unwrap_or("tmp.bin");
+        println!("file to download: '{}'", fname);
+        let fname = tmp_dir.path().join(fname);
+        println!("will be downloaded under: '{:?}'", fname);
+        File::create(fname)?
+    };
+    let content = resp.text().await?;
+    copy(&mut content.as_bytes(), &mut dest);
+    Ok(())
+}
