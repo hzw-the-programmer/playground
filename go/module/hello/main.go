ce09850c9a66e9007788534be62bb0ce773a016f
hezhiwen <hezhiwen.coder@bytedance.com>
2022-04-18 15:10:19 +0800

    go module

--- go/module/hello/main.go
+++ go/module/hello/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"example.com/greetings"
 	"fmt"
+	"log"
 )
 
 func main() {
-	messages := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	messages, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(messages)
 }
