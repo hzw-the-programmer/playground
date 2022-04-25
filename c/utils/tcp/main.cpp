5daaf9d2d06efbf0a62d597c0922b59e989c6050
hezhiwen <hezhiwen.coder@bytedance.com>
2022-04-25 21:03:34 +0800

    tcp

--- c/utils/tcp/main.cpp
+++ c/utils/tcp/main.cpp
@@ -7,9 +7,16 @@
 // link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
 
+static int getaddrinfo_test(int argc, char **argv);
+static int client_test(int argc, char **argv);
+
 int __cdecl main(int argc, char **argv)
 {
+    //return getaddrinfo_test(argc, argv);
+    return client_test(argc, argv);
+}
 
+static int getaddrinfo_test(int argc, char **argv) {
     //-----------------------------------------
     // Declare and initialize variables
     WSADATA wsaData;
@@ -163,4 +170,107 @@ int __cdecl main(int argc, char **argv)
     WSACleanup();
 
     return 0;
+}
+
+static int client_test(int argc, char **argv) {
+    WSADATA wsaData;
+    int iResult;
+
+    struct addrinfo *result = NULL;
+    struct addrinfo *ptr = NULL;
+    struct addrinfo hints;
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    // Attempt to connect to the first address returned by
+    // the call to getaddrinfo
+    ptr=result;
+
+    // Create a SOCKET for connecting to server
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    // Connect to server.
+    iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+    }
+
+    // Should really try the next address returned by getaddrinfo
+    // if the connect call failed
+    // But for this simple example we just free the resources
+    // returned by getaddrinfo and print an error message
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET) {
+        printf("Unable to connect to server!\n");
+        WSACleanup();
+        return 1;
+    }
+
+    const char *sendbuf = "GET / HTTP/1.1";
+    // Send an initial buffer
+    iResult = send(ConnectSocket, sendbuf, (int) strlen(sendbuf), 0);
+    if (iResult == SOCKET_ERROR) {
+        printf("send failed: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+    printf("Bytes Sent: %ld\n", iResult);
+
+    // shutdown the connection for sending since no more data will be sent
+    // the client can still use the ConnectSocket for receiving data
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed: %d\n", WSAGetLastError());
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    #define DEFAULT_BUFLEN 512
+    int recvbuflen = DEFAULT_BUFLEN;
+    char recvbuf[DEFAULT_BUFLEN];
+
+    // Receive data until the server closes the connection
+    do {
+        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            printf("Bytes received: %d\n", iResult);
+            recvbuf[iResult] = 0;
+            printf("%s", recvbuf);
+        } else if (iResult == 0) {
+            printf("Connection closed\n");
+        } else {
+            printf("recv failed: %d\n", WSAGetLastError());
+        }
+    } while (iResult > 0);
 }
