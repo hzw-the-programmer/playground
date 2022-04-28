3a75844fd12249d4f16c77dda31b1114c2e11297
hzw <hzw@home>
2022-04-28 23:56:35 +0800

    rust borrow

--- rust/drop/src/bin/borrow.rs
+++ rust/drop/src/bin/borrow.rs
@@ -1,5 +1,6 @@
 use drop::Object;
 use std::borrow::Borrow;
+use std::ops::Deref;
 
 fn main() {
     let tests: Vec<fn()> = vec![test0, test1, test2, test3];
@@ -18,8 +19,66 @@ fn test0() {
     println!("{:?}", r1);
 }
 
-fn test1() {}
-fn test2() {}
+fn test1() {
+    let s = S {
+        f1: Object { id: 1 },
+        f2: Object { id: 2 },
+    };
+    let r = &s;
+    let rr = &&s;
+    let rrr = &&&s;
+    let rrrr = &&&&s;
+
+    /*
+        T
+    */
+    s.fn1();
+
+    /*
+        &T
+        T
+    */
+    r.fn1();
+
+    /*
+        &&T
+        &T
+        T
+    */
+    rr.fn1();
+    rrr.fn1();
+    s.fn2();
+    r.fn2();
+    rr.fn2();
+    rrr.fn2();
+
+    /*
+        &&&&T
+        &&&T
+        &&T
+        &T
+        T
+    */
+    rrrr.fn2();
+}
+
+fn test2() {
+    let s = S {
+        f1: Object { id: 1 },
+        f2: Object { id: 2 },
+    };
+    let r = &s;
+    let rr = &&s;
+
+    /*
+        T
+        Object
+    */
+    println!("{}", s.Id());
+    println!("{}", r.Id());
+    println!("{}", rr.Id());
+}
+
 fn test3() {}
 
 #[derive(Debug)]
@@ -36,6 +95,41 @@ impl Drop for S {
 
 impl Borrow<Object> for S {
     fn borrow(&self) -> &Object {
-        return &self.f1;
+        &self.f1
+    }
+}
+
+trait T {
+    fn fn2(&self);
+}
+
+impl S {
+    fn fn1(&self) {
+        println!("S.fn1 called");
+    }
+}
+
+impl T for S {
+    fn fn2(&self) {
+        println!("S.fn2 called");
+    }
+}
+
+impl T for &S {
+    fn fn2(&self) {
+        println!("&S.fn2 called");
+    }
+}
+
+impl T for &&S {
+    fn fn2(&self) {
+        println!("&&S.fn2 called");
+    }
+}
+
+impl Deref for S {
+    type Target = Object;
+    fn deref(&self) -> &Self::Target {
+        &self.f2
     }
 }
