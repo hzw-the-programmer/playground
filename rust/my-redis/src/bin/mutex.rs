aac95e3f252cbc7bc097851b7fc14ff393a9ee9e
hzw <hzw@home>
2022-04-30 21:41:46 +0800

    mutex

---
+++ rust/my-redis/src/bin/mutex.rs
@@ -0,0 +1,18 @@
+use std::sync::{Mutex, MutexGuard};
+use tokio::task::yield_now;
+
+#[tokio::main]
+async fn main() {
+    let m = Mutex::new(0);
+    //increment_and_do_stuff(&m).await;
+    let handle = tokio::spawn(increment_and_do_stuff(m));
+    handle.await.unwrap();
+}
+
+async fn increment_and_do_stuff(m: Mutex<i32>) {
+    {
+        let mut mg: MutexGuard<i32> = m.lock().unwrap();
+        *mg += 1;
+    }
+    yield_now().await;
+}
