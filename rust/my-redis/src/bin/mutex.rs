a7e81ce2b6977e171bc75cfd4b52769365718002
hzw <hzw@home>
2022-04-30 21:53:39 +0800

    mutex

--- rust/my-redis/src/bin/mutex.rs
+++ rust/my-redis/src/bin/mutex.rs
@@ -6,8 +6,10 @@ async fn main() {
     //let m = Mutex::new(0);
     //increment_and_do_stuff(&m).await;
     //let handle = tokio::spawn(increment_and_do_stuff(m));
-    let ci = CanIncrement { m: Mutex::new(0) };
-    let handle = tokio::spawn(increment_and_do_stuff_1(ci));
+    //let ci = CanIncrement { m: Mutex::new(0) };
+    //let handle = tokio::spawn(increment_and_do_stuff_1(ci));
+    let m = tokio::sync::Mutex::new(0);
+    let handle = tokio::spawn(increment_and_do_stuff_2(m));
     handle.await.unwrap();
 }
 
@@ -21,6 +23,12 @@ async fn increment_and_do_stuff(m: Mutex<i32>) {
 
 async fn increment_and_do_stuff_1(ci: CanIncrement) {
     ci.increment();
+    yield_now().await;
+}
+
+async fn increment_and_do_stuff_2(m: tokio::sync::Mutex<i32>) {
+    let mut mg = m.lock().await;
+    *mg += 1;
     yield_now().await;
 }
 
