efb01e807cb48bae44bd3d8f55f569f7d262224e
hzw <hzw@home>
2022-04-30 12:53:21 +0800

    spawn future

--- rust/my-redis/src/main.rs
+++ rust/my-redis/src/main.rs
@@ -6,9 +6,7 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        tokio::spawn(async move {
-            process(socket).await;
-        });
+        tokio::spawn(process(socket));
     }
 }
 
