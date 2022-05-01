78d2264adb7e6ba78c598a92b0e6e274b08f4988
hzw <hzw@home>
2022-05-01 17:10:05 +0800

    manager

--- rust/my-redis/src/main.rs
+++ rust/my-redis/src/main.rs
@@ -1,48 +1,75 @@
 use bytes::Bytes;
-use mini_redis::{Connection, Frame};
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use tokio::net::{TcpListener, TcpStream};
-
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+use mini_redis::client;
+use tokio::sync::{mpsc, oneshot};
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let db = Arc::new(Mutex::new(HashMap::new()));
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        let db = db.clone();
-        tokio::spawn(process(socket, db));
-    }
-}
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
 
-async fn process(socket: TcpStream, db: Db) {
-    use mini_redis::Command::{self, Get, Set};
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
 
-    let addr = socket.local_addr();
-    let mut connection = Connection::new(socket);
-
-    while let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("{:?}: {:?}", addr, frame);
-        let response = match Command::from_frame(frame).unwrap() {
-            Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
-            }
-            Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone())
-                } else {
-                    Frame::Null
+            match cmd {
+                Get { key, resp } => {
+                    let r = client.get(&key).await;
+                    resp.send(r).unwrap();
+                }
+                Set { key, val, resp } => {
+                    let r = client.set(&key, val).await;
+                    resp.send(r).unwrap();
                 }
             }
-            cmd => panic!("unimplementd: {:?}", cmd),
-        };
-        //println!("{:?}", frame);
+        }
+    });
 
-        connection.write_frame(&response).await.unwrap();
-    }
+    let task1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+
+        tx.send(Command::Get {
+            key: "hello".to_string(),
+            resp: resp_tx,
+        })
+        .await
+        .unwrap();
+
+        let resp = resp_rx.await.unwrap();
+        println!("Got: {:?}", resp);
+    });
+
+    let task2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+
+        tx2.send(Command::Set {
+            key: "hello".to_string(),
+            val: "world".into(),
+            resp: resp_tx,
+        })
+        .await
+        .unwrap();
+
+        let resp = resp_rx.await.unwrap();
+        println!("Got: {:?}", resp);
+    });
+
+    manager.await.unwrap();
+    task1.await.unwrap();
+    task2.await.unwrap();
 }
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        resp: Responder<()>,
+    },
+}
+
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
