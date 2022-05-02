de2afc08225850277ddc311aded741d6fce327b5
hzw <hzw@home>
2022-05-02 21:48:44 +0800

    echo uppercase

--- rust/reqwest1/src/bin/http.rs
+++ rust/reqwest1/src/bin/http.rs
@@ -1,3 +1,4 @@
+use futures::TryStreamExt;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::convert::Infallible;
@@ -22,6 +23,15 @@ async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
         (&Method::GET, "/") => *response.body_mut() = Body::from("Try POSTing data to /echo"),
         (&Method::POST, "/echo") => {
             *response.body_mut() = req.into_body();
+        }
+        (&Method::POST, "/echo/uppercase") => {
+            let mapping = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+            *response.body_mut() = Body::wrap_stream(mapping);
         }
         _ => *response.status_mut() = StatusCode::NOT_FOUND,
     }
