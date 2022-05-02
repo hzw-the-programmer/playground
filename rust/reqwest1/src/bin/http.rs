ff2d1f4d557752a8803864e347a0cb3c1a25d470
hzw <hzw@home>
2022-05-02 21:36:35 +0800

    http echo

--- rust/reqwest1/src/bin/http.rs
+++ rust/reqwest1/src/bin/http.rs
@@ -20,7 +20,9 @@ async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => *response.body_mut() = Body::from("Try POSTing data to /echo"),
-        (&Method::POST, "/echo") => {}
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        }
         _ => *response.status_mut() = StatusCode::NOT_FOUND,
     }
     Ok(response)
