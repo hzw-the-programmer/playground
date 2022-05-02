0e9161bbefc3f8b23b3a45346bd8dedfd8778a2b
hzw <hzw@home>
2022-05-02 13:56:43 +0800

    echo server copy

---
+++ rust/my-redis/src/bin/echo-server-copy.rs
@@ -0,0 +1,17 @@
+use tokio::io::{self, Result};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (mut rd, mut wr) = socket.split();
+            if io::copy(&mut rd, &mut wr).await.is_err() {
+                eprintln!("failed to copy");
+            }
+        });
+    }
+    //Ok(())
+}
