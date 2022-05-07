0a6200fbeecdd0935de815b844e14998c0614291
hzw <hzw@home>
2022-05-07 22:11:05 +0800

    vec

--- rust/drop/src/bin/vec.rs
+++ rust/drop/src/bin/vec.rs
@@ -165,5 +165,31 @@ fn test5() {
     );
 }
 
-fn test6() {}
+fn test6() {
+    let mut v1 = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+        Object { id: 4 },
+    ];
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    //let e = &v1[0];
+    let i = v1.iter();
+    v1.push(Object { id: 5 });
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    // println!("{:?}", e);
+    // for e in i {
+    //     println!("{:?}", e);
+    // }
+}
+
 fn test7() {}
