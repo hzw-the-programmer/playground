a8b4360996c1e7407deba3d58ea8229553e13a31
hzw <hzw@home>
2022-05-07 19:49:22 +0800

    rust drop

--- rust/drop/src/bin/iter.rs
+++ rust/drop/src/bin/iter.rs
@@ -1,7 +1,11 @@
 use drop::Object;
 
 fn main() {
-    let tests: Vec<fn()> = vec![test0, test1, test2, test3, test4, test5];
+    let tests: Vec<fn()> = vec![
+        test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11,
+        test12, test13, test14, test15, test16, test17, test18, test19, test20, test21, test22,
+        test23, test24,
+    ];
 
     drop::tests(&tests);
 }
@@ -82,6 +86,259 @@ fn test5() {
         assert_eq!(i, obj);
         i += 1;
     }
+}
+
+fn test6() {
+    let a = [1, 2, 3];
+    let m = a.iter().map(|e| {
+        //let i: i32 = e;
+        e * 2
+    });
+    for i in m {
+        let i: i32 = i;
+        println!("{:?}", i);
+    }
+}
+
+fn test7() {
+    let a = [
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let m = a.iter().map(|e| Object { id: e.id + 1 });
+    for e in m {
+        println!("{:?}", e);
+    }
+    //let b: Vec<Object> = m.collect();
+    println!("{:?}", a);
+    //println!("{:?}", b);
+}
+
+fn test8() {
+    let a = [
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let m = a.into_iter().map(|e| {
+        //let i: i32 = e;
+        Object { id: e.id + 1 }
+    });
+    for e in m {
+        println!("{:?}", e);
+    }
+    //let b: Vec<Object> = m.collect();
+    println!("{:?}", a);
+    //println!("{:?}", b);
+}
+
+fn test9() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let m = a.into_iter().map(|e| {
+        //let i: i32 = e;
+        Object { id: e.id + 1 }
+    });
+    /*
+    for e in m {
+        println!("{:?}", e);
+    }
+    */
+    println!("before collect");
+    let b: Vec<Object> = m.collect();
+    println!("after collect");
+    //println!("{:?}", a);
+    println!("{:?}", b);
+}
+
+fn test10() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let m = a.iter().map(|e| {
+        //let i: i32 = e;
+        Object { id: e.id + 1 }
+    });
+    /*
+    for e in m {
+        println!("{:?}", e);
+    }
+    */
+    println!("before collect");
+    let b: Vec<Object> = m.collect();
+    println!("after collect");
+    println!("{:?}", a);
+    println!("{:?}", b);
+}
+
+fn test11() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let m = a.into_iter().take(3);
+    for e in m {
+        println!("{:?}", e);
+    }
+    println!("test11 finish");
+}
+
+fn test12() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let m = a.iter().take(3);
+    for e in m {
+        println!("{:?}", e);
+    }
+    println!("test11 finish");
+}
+
+fn test13() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{}", a.iter().any(|o| o.id == 0));
+}
+
+fn test14() {
+    let a = vec![0, 1, 2, 3];
+    println!("{}", a.iter().any(|&o| o == 0));
+}
+
+fn test15() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{}", a.iter().count());
+}
+
+fn test16() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{}", a.into_iter().count());
+}
+
+fn test17() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!(
+        "{}",
+        a.into_iter().any(|o| {
+            println!("{:?}", o);
+            o.id == 2
+        })
+    );
+    println!("finish");
+}
+
+fn test18() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{:?}", a.into_iter().last());
+    println!("finish");
+}
+
+fn test19() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{:?}", a.iter().last());
+    println!("finish");
+}
+
+fn test20() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{:?}", a.into_iter().nth(1));
+    println!("finish");
+}
+
+fn test21() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    println!("{:?}", a.iter().nth(1));
+    println!("finish");
+}
+
+fn test22() {
+    let a = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let mut i = a.into_iter();
+    println!("{:?}", i.nth(1));
+    println!("finish");
+}
+
+fn test23() {
+    let v = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let mut i = v.into_iter();
+    println!("{}", i.any(|o| { o.id == 2 }));
+    println!("finish");
+}
+
+fn test24() {
+    let v = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+    ];
+    let mut i = v.into_iter();
+    println!("{:?}", i.find(|o| { o.id == 2 }));
+    println!("finish");
 }
 
 fn t1(objs: &[Object]) {
---
+++ rust/drop/src/bin/vec.rs
@@ -0,0 +1,169 @@
+use drop::Object;
+
+fn main() {
+    let tests: Vec<fn()> = vec![
+        test0, test1, test2, test3, test4, test5, test6,
+        test7, // test8, test9, test10, test11,
+              // test12, test13, test14, test15, test16, test17, test18, test19, test20, test21, test22,
+              // test23, test24,
+    ];
+
+    drop::tests(&tests);
+}
+
+fn test0() {
+    let mut v = vec![];
+
+    {
+        let o0 = Object { id: 0 };
+        let o1 = Object { id: 1 };
+        let o2 = Object { id: 2 };
+        v.push(o0);
+        v.push(o1);
+    }
+
+    {
+        let o3 = Object { id: 3 };
+        let o4 = Object { id: 4 };
+        v[0] = o3;
+    }
+
+    println!("finish");
+}
+
+fn test1() {
+    let mut v1 = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+        Object { id: 4 },
+    ];
+
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+
+    let v2 = v1.split_off(2);
+
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    println!(
+        "v2.len() == {}, v2.capacity() = {}",
+        v2.len(),
+        v2.capacity(),
+    );
+}
+
+fn test2() {
+    let mut v1 = vec![];
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    v1.push(Object { id: 0 });
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    v1.push(Object { id: 1 });
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    v1.push(Object { id: 2 });
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    v1.push(Object { id: 3 });
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+    v1.push(Object { id: 4 });
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+}
+
+fn test3() {
+    let mut v1 = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+        Object { id: 4 },
+    ];
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+
+    {
+        let d = v1.drain(2..);
+    }
+
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+}
+
+fn test4() {
+    let mut v1 = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+        Object { id: 4 },
+    ];
+
+    let d = v1.drain(2..);
+    let v2: Vec<Object> = d.collect();
+    println!(
+        "v2.len() == {}, v2.capacity() = {}",
+        v2.len(),
+        v2.capacity(),
+    );
+
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+}
+
+fn test5() {
+    let mut v1 = vec![
+        Object { id: 0 },
+        Object { id: 1 },
+        Object { id: 2 },
+        Object { id: 3 },
+        Object { id: 4 },
+    ];
+
+    v1.clear();
+    println!(
+        "v1.len() == {}, v1.capacity() = {}",
+        v1.len(),
+        v1.capacity(),
+    );
+}
+
+fn test6() {}
+fn test7() {}
