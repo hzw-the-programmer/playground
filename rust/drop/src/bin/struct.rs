dc7f95d02c7aace37278a4920f8b9b10c6608a7f
hzw <hzw@home>
2022-05-08 19:15:20 +0800

    struct

--- rust/drop/src/bin/struct.rs
+++ rust/drop/src/bin/struct.rs
@@ -3,7 +3,10 @@
 use drop::Object;
 
 fn main() {
-    let tests: Vec<fn()> = vec![test0, test1, test2, test3, test4, test5, test6, test7];
+    let tests: Vec<fn()> = vec![
+        test0, test1, test2, test3, test4, test5, test6, test7, test8, test9, test10, test11,
+        test12, test13,
+    ];
 
     drop::tests(&tests);
 }
@@ -108,8 +111,80 @@ fn test7() {
     println!("finish")
 }
 
+fn test8() {
+    let f1 = Object { id: 1 };
+    let f2 = Object { id: 2 };
+    let s = S { f1, f2 };
+
+    func1(s.f2);
+    // println!("{:?}", s);
+    println!("{:?}", s.f1);
+    // println!("{:?}", s.f2);
+    println!("finish")
+}
+
+fn test9() {
+    let f1 = Object { id: 1 };
+    let f2 = Object { id: 2 };
+    let mut s = S { f1, f2 };
+
+    func1(s.f2);
+    s.f2 = Object { id: 3 };
+    println!("{:?}", s);
+    println!("{:?}", s.f1);
+    println!("{:?}", s.f2);
+    println!("finish")
+}
+
+fn test10() {
+    let f1 = Object { id: 1 };
+    let f2 = Object { id: 2 };
+    let s = S { f1, f2 };
+
+    func2(s);
+    println!("finish")
+}
+
+fn test11() {
+    let f1 = Object { id: 1 };
+    let f2 = Object { id: 2 };
+    let s = S { f1, f2 };
+
+    func3(s);
+    // println!("{:?}", s);
+    println!("finish")
+}
+
+fn test12() {
+    let f1 = Object { id: 1 };
+    let f2 = Object { id: 2 };
+    let s = S { f1, f2 };
+
+    func4(s);
+    // println!("{:?}", s);
+    println!("finish")
+}
+
+fn test13() {}
+
 #[derive(Debug)]
 struct S {
     f1: Object,
     f2: Object,
+}
+
+fn func1(o: Object) {
+    println!("func1: {:?}", o);
+}
+
+fn func2(S { f1, f2 }: S) {
+    println!("func2: {:?} {:?}", f1, f2);
+}
+
+fn func3(S { f1, ref f2 }: S) {
+    println!("func3: {:?} {:?}", f1, f2);
+}
+
+fn func4(S { ref f1, ref f2 }: S) {
+    println!("func4: {:?} {:?}", f1, f2);
 }
