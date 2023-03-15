5e0a50db69eff9958909db8138880658219407a6
hezhiwen <hezhiwen.coder@bytedance.com>
2023-03-15 17:42:59 +0800

    utils

--- c/utils/src/io/len_reader_writer_test.c
+++ c/utils/src/io/len_reader_writer_test.c
@@ -3,13 +3,14 @@
 #include "len_reader_writer.h"
 #include "utils.h"
 
-static void len_writer_test_1() {
-    char *in_0 = "hzw";
-    struct {
-        uint32_t flags;
-        slice_t in;
-        slice_t out;
-    } tests[] = {
+typedef struct {
+    uint32_t flags;
+    slice_t in;
+    slice_t out;
+} fixture_t;
+
+static void fixtures_1_len_writer_test(len_writer_t *w) {
+    fixture_t fixtures[] = {
         {
             LEN_SIZE_1,
             {"hzw", 3},
@@ -26,24 +27,31 @@ static void len_writer_test_1() {
             {"\0\0\0\5bj&tj", 9},
         },
     };
-    
+
+    int i;
+
+    for (i = 0; i < ARRAY_SIZE(fixtures); i++) {
+        fixture_t *fixture = &fixtures[i];
+        w->flags = fixture->flags;
+        assert(len_writer_write_slice(w, &fixture->in) == fixture->in.len);
+        assert(buf_buffered(w->buf) == fixture->out.len);
+        assert(memcmp(fixture->out.data, buf_read_ptr(w->buf), fixture->out.len) == 0);
+        buf_read_inc(w->buf, fixture->out.len);
+    }
+}
+
+static void len_writer_test_1() {
     int pool[128];
     len_writer_t w = {0};
 
-    int i, ret;
-
     w.buf = buf_static((uint8_t*)pool, sizeof(pool));
+    fixtures_1_len_writer_test(&w);
+}
 
-    for (i = 0; i < ARRAY_SIZE(tests); i++) {
-        w.flags = tests[i].flags;
-        ret = len_writer_write_slice(&w, &tests[i].in);
-        assert(ret == tests[i].in.len);
-        assert(buf_buffered(w.buf) == tests[i].out.len);
-        assert(memcmp(tests[i].out.data, buf_read_ptr(w.buf), tests[i].out.len) == 0);
-        buf_read_inc(w.buf, tests[i].out.len);
-    }
+static void len_reader_test_1() {
 }
 
 void len_reader_writer_test() {
     len_writer_test_1();
+    len_reader_test_1();
 }
