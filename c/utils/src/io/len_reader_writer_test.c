01a5893da82edbdd32d1bd5558528540999036d3
hezhiwen <hezhiwen.coder@bytedance.com>
2023-03-15 17:21:07 +0800

    utils

--- c/utils/src/io/len_reader_writer_test.c
+++ c/utils/src/io/len_reader_writer_test.c
@@ -1,47 +1,47 @@
 #include <string.h>
 #include <assert.h>
 #include "len_reader_writer.h"
+#include "utils.h"
 
 static void len_writer_test_1() {
-    slice_t in_0 = {"hzw", 3};
-    slice_t out_0 = {"\3hzw", 4};
-    slice_t in_1 = {"bj&tj", 5};
-    slice_t out_1 = {"\0\5bj&tj", 7};
-    slice_t in_2 = {"bj&tj", 5};
-    slice_t out_2 = {"\0\0\0\5bj&tj", 9};
+    char *in_0 = "hzw";
+    struct {
+        uint32_t flags;
+        slice_t in;
+        slice_t out;
+    } tests[] = {
+        {
+            LEN_SIZE_1,
+            {"hzw", 3},
+            {"\3hzw", 4},
+        },
+        {
+            LEN_SIZE_2,
+            {"bj&tj", 5},
+            {"\0\5bj&tj", 7},
+        },
+        {
+            LEN_SIZE_4,
+            {"bj&tj", 5},
+            {"\0\0\0\5bj&tj", 9},
+        },
+    };
     
     int pool[128];
     len_writer_t w = {0};
-    
-    slice_t in;
-    slice_t out;
-    int ret;
+
+    int i, ret;
 
     w.buf = buf_static((uint8_t*)pool, sizeof(pool));
 
-    in = in_0;
-    out = out_0;
-    w.flags = LEN_SIZE_1;
-    ret = len_writer_write_slice(&w, &in);
-    assert(ret == in.len);
-    assert(memcmp(out.data, buf_read_ptr(w.buf), out.len) == 0);
-    buf_read_inc(w.buf, out.len);
-
-    in = in_1;
-    out = out_1;
-    w.flags = LEN_SIZE_2;
-    ret = len_writer_write_slice(&w, &in);
-    assert(ret == in.len);
-    assert(memcmp(out.data, buf_read_ptr(w.buf), out.len) == 0);
-    buf_read_inc(w.buf, out.len);
-
-    in = in_2;
-    out = out_2;
-    w.flags = LEN_SIZE_4;
-    ret = len_writer_write_slice(&w, &in);
-    assert(ret == in.len);
-    assert(memcmp(out.data, buf_read_ptr(w.buf), out.len) == 0);
-    buf_read_inc(w.buf, out.len);
+    for (i = 0; i < ARRAY_SIZE(tests); i++) {
+        w.flags = tests[i].flags;
+        ret = len_writer_write_slice(&w, &tests[i].in);
+        assert(ret == tests[i].in.len);
+        assert(buf_buffered(w.buf) == tests[i].out.len);
+        assert(memcmp(tests[i].out.data, buf_read_ptr(w.buf), tests[i].out.len) == 0);
+        buf_read_inc(w.buf, tests[i].out.len);
+    }
 }
 
 void len_reader_writer_test() {
