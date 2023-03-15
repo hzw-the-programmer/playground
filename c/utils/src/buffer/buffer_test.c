ae7215e2fa2a005d842fa847de5b1a9f18c00412
hezhiwen <hezhiwen.coder@bytedance.com>
2023-03-15 11:10:39 +0800

    utils

--- c/utils/src/buffer/buffer_test.c
+++ c/utils/src/buffer/buffer_test.c
@@ -1,18 +1,27 @@
 #include <assert.h>
 #include "buffer/buffer.h"
 
+static buf_t* buf_static(uint8_t *ptr, int len) {
+    buf_t *buf = NULL;
+
+    buf = (buf_t*)ptr;
+    buf->cap = len - sizeof(*buf);
+    buf->w = buf->r = 0;
+    buf->ptr = ptr + sizeof(*buf);
+
+    return buf;
+}
+
 static void buf_test() {
     int pool[(sizeof(buf_t)+16)/sizeof(int)];
-    buf_t *buf = (buf_t*)pool;
+    buf_t *buf = buf_static((uint8_t*)pool, sizeof(pool));
     uint8_t i;
-    
-    buf->cap = 16;
-    buf->w = buf->r = 0;
-    buf->ptr = (uint8_t*)pool + sizeof(buf_t);
 
     for (i = 0; i < 128; i++) {
         int n = 1;
-        if (i > 15) n = 0;
+        if (i > 15) {
+            n = 0;
+        }
         assert(buf_write(buf, &i, 1) == n);
     }
 }
