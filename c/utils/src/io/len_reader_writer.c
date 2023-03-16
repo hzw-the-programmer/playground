ec32c52c2466f45aaacab4ef8d225c13009991f6
hezhiwen <hezhiwen.coder@bytedance.com>
2023-03-16 10:41:38 +0800

    utils

--- c/utils/src/io/len_reader_writer.c
+++ c/utils/src/io/len_reader_writer.c
@@ -1,38 +1,59 @@
 #include "len_reader_writer.h"
 
-int len_reader_read(void *arg, slice_t *slice) {
-    len_reader_t *r = arg;
-    return 0;
-}
-
-int len_writer_write(void *arg, const uint8_t *data, int len) {
+int len_writer_write(void *arg, const slice_t *slice) {
     len_writer_t *w = arg;
     uint8_t *header;
 
-    if ( LEN_SIZE(w) + len > buf_available(w->buf)) {
+    if ( LEN_SIZE(w) + slice->len > buf_available(w->buf)) {
         return -1;
     }
 
     header = buf_write_ptr(w->buf);
 
     buf_write_inc(w->buf, LEN_SIZE(w));
-    buf_write(w->buf, data, len);
+    buf_write_slice(w->buf, slice);
 
     if (LEN_SIZE(w) == 1) {
-        *header = len;
+        *header = slice->len;
     } else if (LEN_SIZE(w) == 2) {
-        *header = len>>8;
-        *(header+1) = len;
+        *header = slice->len>>8;
+        *(header+1) = slice->len;
     } else if (LEN_SIZE(w) == 4) {
-        *header = len>>24;
-        *(header+1) = len>>16;
-        *(header+2) = len>>8;
-        *(header+3) = len;
+        *header = slice->len>>24;
+        *(header+1) = slice->len>>16;
+        *(header+2) = slice->len>>8;
+        *(header+3) = slice->len;
     }
 
-    return len;
+    return slice->len;
 }
 
-int len_writer_write_slice(void *arg, const slice_t *slice) {
-    return len_writer_write(arg, slice->data, slice->len);
+int len_reader_read(void *arg, slice_t *slice) {
+    len_reader_t *r = arg;
+
+    if (!(r->flags & LEN_READED)) {
+        uint8_t *header;
+        if (buf_buffered(r->buf) < LEN_SIZE(r)) {
+            return -1;
+        }
+        header = buf_read_ptr(r->buf);
+        if (LEN_SIZE(r) == 1) {
+            r->len = *header;
+        } else if (LEN_SIZE(r) == 2) {
+            r->len = (*header<<8)|*(header+1);
+        } else if (LEN_SIZE(r) == 4) {
+            r->len = (*header<<24)|(*(header+1)<<16)|(*(header+2)<<8)|*(header+3);
+        }
+        r->flags |= LEN_READED;
+        buf_read_inc(r->buf, LEN_SIZE(r));
+    }
+
+    if (buf_buffered(r->buf) < r->len) {
+        return -1;
+    }
+    slice->data = buf_read_ptr(r->buf);
+    slice->len = r->len;
+    buf_read_inc(r->buf, r->len);
+
+    return r->len;
 }
--- c/utils/src/io/len_reader_writer_test.c
+++ c/utils/src/io/len_reader_writer_test.c
@@ -9,35 +9,36 @@ typedef struct {
     slice_t out;
 } fixture_t;
 
-static void fixtures_1_len_writer_test(len_writer_t *w) {
-    fixture_t fixtures[] = {
-        {
-            LEN_SIZE_1,
-            {"hzw", 3},
-            {"\3hzw", 4},
-        },
-        {
-            LEN_SIZE_2,
-            {"bj&tj", 5},
-            {"\0\5bj&tj", 7},
-        },
-        {
-            LEN_SIZE_4,
-            {"bj&tj", 5},
-            {"\0\0\0\5bj&tj", 9},
-        },
-    };
+fixture_t fixtures_1[] = {
+    {
+        LEN_SIZE_1,
+        {"hzw", 3},
+        {"\3hzw", 4},
+    },
+    {
+        LEN_SIZE_2,
+        {"bj&tj", 5},
+        {"\0\5bj&tj", 7},
+    },
+    {
+        LEN_SIZE_4,
+        {"bj&tj", 5},
+        {"\0\0\0\5bj&tj", 9},
+    },
+};
 
+static void fixtures_1_len_writer_test(len_writer_t *w) {
     int i;
 
-    for (i = 0; i < ARRAY_SIZE(fixtures); i++) {
-        fixture_t *fixture = &fixtures[i];
+    for (i = 0; i < ARRAY_SIZE(fixtures_1); i++) {
+        fixture_t *fixture = &fixtures_1[i];
         w->flags = fixture->flags;
-        assert(len_writer_write_slice(w, &fixture->in) == fixture->in.len);
+        assert(len_writer_write(w, &fixture->in) == fixture->in.len);
         assert(buf_buffered(w->buf) == fixture->out.len);
         assert(memcmp(fixture->out.data, buf_read_ptr(w->buf), fixture->out.len) == 0);
         buf_read_inc(w->buf, fixture->out.len);
     }
+    assert(buf_buffered(w->buf) == 0);
 }
 
 static void len_writer_test_1() {
@@ -49,6 +50,22 @@ static void len_writer_test_1() {
 }
 
 static void len_reader_test_1() {
+    int pool[128];
+    len_writer_t w = {0};
+    len_reader_t r = {0};
+    int i;
+    slice_t slice;
+
+    w.buf = buf_static((uint8_t*)pool, sizeof(pool));
+    fixtures_1_len_writer_test(&w);
+
+    r.buf = w.buf;
+    r.buf->r = 0;
+    for (i = 0; i < ARRAY_SIZE(fixtures_1); i++) {
+        fixture_t *fixture = &fixtures_1[i];
+        r.flags = fixture->flags;
+        assert(len_reader_read(&r, &slice) == fixture->in.len);
+    }
 }
 
 void len_reader_writer_test() {
