04d0490907a6864a07f64fb34950205bcba85575
hezhiwen <hezhiwen.coder@bytedance.com>
2023-06-23 23:46:35 +0800

    rust

--- rust/reqwest/src/main.rs
+++ rust/reqwest/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use std::io::Read;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+    println!("Body:\n{}", body);
+    Ok(())
 }
