98a6d1d1029b32433dff96889d15cb6039daa528
hezhiwen <hezhiwen.coder@bytedance.com>
2023-07-25 20:20:43 +0800

    picotls

--- c/2022/ConsoleSln1/ConsolePro1/main.c
+++ c/2022/ConsoleSln1/ConsolePro1/main.c
@@ -6,12 +6,18 @@ int on_extension_cb(ptls_on_extension_t* self, ptls_t* tls, uint8_t hstype, uint
 }
 
 int collect_extension_cb(ptls_t* tls, struct st_ptls_handshake_properties_t* properties, uint16_t type) {
-
+	//return 1;
+	return 0;
 }
 
 int on_client_hello_cb(ptls_on_client_hello_t* on_hello_cb_ctx, ptls_t* tls,
 	ptls_on_client_hello_parameters_t* params) {
+	return 0;
+}
 
+int collect_extensions_cb(ptls_t* tls, ptls_handshake_properties_t* properties,
+	ptls_raw_extension_t* slots) {
+	return 0;
 }
 
 int main() {
@@ -20,8 +26,11 @@ int main() {
 	char* key_file = "ec_key.pem";
 
 	ptls_context_t client_ctx = { 0 };
-	ptls_t* client_tls;
+	ptls_raw_extension_t client_extensions[2] = { 0 };
+	uint16_t client_extension_type = 1234;
+	uint8_t client_extension_data[] = {1,2,3};
 	ptls_handshake_properties_t client_prop = { 0 };
+	ptls_t* client_tls;
 	ptls_buffer_t client_buf;
 
 	ptls_context_t server_ctx = { 0 };
@@ -40,6 +49,14 @@ int main() {
 	client_ctx.cipher_suites = ptls_minicrypto_cipher_suites;
 	client_ctx.verify_certificate = NULL;
 
+	client_extensions[0].type = client_extension_type;
+	client_extensions[0].data.base = client_extension_data;
+	client_extensions[0].data.len = sizeof(client_extension_data);
+	client_extensions[1].type = UINT16_MAX;
+	client_extensions[1].data.base = NULL;
+	client_extensions[1].data.len = 0;
+	client_prop.additional_extensions = client_extensions;
+
 	client_tls = ptls_client_new(&client_ctx);
 	ptls_set_server_name(client_tls, host, strlen(host));
 
@@ -55,6 +72,7 @@ int main() {
 	server_ctx.on_client_hello = &on_client_hello;
 
 	server_prop.collect_extension = collect_extension_cb;
+	server_prop.collected_extensions = collect_extensions_cb;
 
 	server_tls = ptls_server_new(&server_ctx);
 	
