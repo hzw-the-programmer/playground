be60bf317282d846b9204613578ff60f91fd1996
hezhiwen <hezhiwen.coder@bytedance.com>
2023-08-03 15:23:49 +0800

    picotls

--- c/2022/ConsoleSln1/ConsolePro1/main.c
+++ c/2022/ConsoleSln1/ConsolePro1/main.c
@@ -5,13 +5,15 @@
 #define EXTENSION_2 0x5678
 
 typedef struct {
-	size_t extensions_len;
-	uint8_t extensions[16];
-	ptls_handshake_properties_t inner;
-} properties_wrapper;
+	size_t extension_data_len;
+	uint8_t extension_data[32];
+	ptls_raw_extension_t additional_extensions[3];
+	ptls_handshake_properties_t properties;
+} ctx_t;
 
-uint8_t extension_1_data[] = {'a', 'b', 'c', 'd'};
-uint8_t extension_2_data[] = { 'e', 'f', 'g', 'h', 'i'};
+uint8_t extension_1_data[] = "ext_1_data";
+uint8_t extension_2_client_data[] = "ext_2_c_data";
+uint8_t extension_2_server_data[] = "ext_2_server_data";
 
 char test_sni[] = "test.example.com";
 char test_alpn[] = "picotls";
@@ -45,11 +47,27 @@ int on_client_hello_cb(ptls_on_client_hello_t* on_hello_cb_ctx, ptls_t* tls,
 
 int collected_unknown_extensions_cb(ptls_t* tls, ptls_handshake_properties_t* properties,
 	ptls_raw_extension_t* slots) {
-	properties_wrapper* wrapper = (uint8_t*)properties - offsetof(properties_wrapper, inner);
+	ctx_t* ctx = (uint8_t*)properties - offsetof(ctx_t, properties);
 	while (slots->type != UINT16_MAX) {
 		if (slots->type == EXTENSION_2) {
-			wrapper->extensions_len = slots->data.len;
-			memcpy(wrapper->extensions, slots->data.base, slots->data.len);
+			assert(slots->data.len <= sizeof(ctx->extension_data));
+			ctx->extension_data_len = slots->data.len;
+			memcpy(ctx->extension_data, slots->data.base, slots->data.len);
+			if (ptls_is_server(tls)) {
+				ctx->additional_extensions[0].type = EXTENSION_1;
+				ctx->additional_extensions[0].data.base = extension_1_data;
+				ctx->additional_extensions[0].data.len = sizeof(extension_1_data) - 1;
+
+				ctx->additional_extensions[1].type = EXTENSION_2;
+				ctx->additional_extensions[1].data.base = extension_2_server_data;
+				ctx->additional_extensions[1].data.len = sizeof(extension_2_server_data) - 1;
+
+				ctx->additional_extensions[2].type = UINT16_MAX;
+				ctx->additional_extensions[2].data.base = NULL;
+				ctx->additional_extensions[2].data.len = 0;
+
+				properties->additional_extensions = ctx->additional_extensions;
+			}
 			break;
 		}
 		slots++;
@@ -61,84 +79,84 @@ int main() {
 	char* cert_file = "ec_cert.pem";
 	char* key_file = "ec_key.pem";
 
-	ptls_context_t client_ctx = { 0 };
-	ptls_raw_extension_t client_extensions[3] = { 0 };
-	ptls_handshake_properties_t client_prop = { 0 };
+	ptls_context_t client_tls_ctx = { 0 };
 	ptls_t* client_tls;
 	ptls_buffer_t client_buf;
+	ctx_t client_ctx = { 0 };
 
-	ptls_context_t server_ctx = { 0 };
+	ptls_context_t server_tls_ctx = { 0 };
+	ptls_t* server_tls;
 	ptls_on_extension_t on_extension;
 	ptls_on_client_hello_t on_client_hello;
-	properties_wrapper server_properties = { 0 };
-	ptls_t* server_tls;
 	ptls_buffer_t server_buf;
+	ctx_t server_ctx = { 0 };
 
 	int ret;
 	size_t len;
 
 	on_extension.cb = on_extension_cb;
 
-	client_ctx.random_bytes = ptls_minicrypto_random_bytes;
-	client_ctx.get_time = &ptls_get_time;
-	client_ctx.key_exchanges = ptls_minicrypto_key_exchanges;
-	client_ctx.cipher_suites = ptls_minicrypto_cipher_suites;
-	client_ctx.verify_certificate = NULL;
-	client_ctx.on_extension = &on_extension;
-	client_ctx.use_exporter = 1;
+	client_tls_ctx.random_bytes = ptls_minicrypto_random_bytes;
+	client_tls_ctx.get_time = &ptls_get_time;
+	client_tls_ctx.cipher_suites = ptls_minicrypto_cipher_suites;
+	client_tls_ctx.key_exchanges = ptls_minicrypto_key_exchanges;
+	client_tls_ctx.verify_certificate = NULL;
+	client_tls_ctx.on_extension = &on_extension;
+	client_tls_ctx.use_exporter = 1;
 
-	client_extensions[0].type = EXTENSION_1;
-	client_extensions[0].data.base = extension_1_data;
-	client_extensions[0].data.len = sizeof(extension_1_data);
-	
-	client_extensions[1].type = EXTENSION_2;
-	client_extensions[1].data.base = extension_2_data;
-	client_extensions[1].data.len = sizeof(extension_2_data);
-	
-	client_extensions[2].type = UINT16_MAX;
-	client_extensions[2].data.base = NULL;
-	client_extensions[2].data.len = 0;
-	
-	client_prop.additional_extensions = client_extensions;
-	
-	client_prop.client.negotiated_protocols.list = proposed_alpn;
-	client_prop.client.negotiated_protocols.count = sizeof(proposed_alpn) / sizeof(proposed_alpn[0]);
-
-	client_tls = ptls_client_new(&client_ctx);
+	client_tls = ptls_client_new(&client_tls_ctx);
 	ptls_set_server_name(client_tls, test_sni, sizeof(test_sni) - 1);
 
-	server_ctx.random_bytes = ptls_minicrypto_random_bytes;
-	server_ctx.get_time = &ptls_get_time;
-	server_ctx.key_exchanges = ptls_minicrypto_key_exchanges;
-	server_ctx.cipher_suites = ptls_minicrypto_cipher_suites;
-	ret = ptls_load_certificates(&server_ctx, cert_file);
-	ret = ptls_minicrypto_load_private_key(&server_ctx, key_file);
+	client_ctx.additional_extensions[0].type = EXTENSION_1;
+	client_ctx.additional_extensions[0].data.base = extension_1_data;
+	client_ctx.additional_extensions[0].data.len = sizeof(extension_1_data) - 1;
+	
+	client_ctx.additional_extensions[1].type = EXTENSION_2;
+	client_ctx.additional_extensions[1].data.base = extension_2_client_data;
+	client_ctx.additional_extensions[1].data.len = sizeof(extension_2_client_data) - 1;
+	
+	client_ctx.additional_extensions[2].type = UINT16_MAX;
+	client_ctx.additional_extensions[2].data.base = NULL;
+	client_ctx.additional_extensions[2].data.len = 0;
+	
+	client_ctx.properties.additional_extensions = client_ctx.additional_extensions;
+	client_ctx.properties.client.negotiated_protocols.list = proposed_alpn;
+	client_ctx.properties.client.negotiated_protocols.count = sizeof(proposed_alpn) / sizeof(proposed_alpn[0]);
+	client_ctx.properties.collect_extension = should_collect_unknown_extension_cb;
+	client_ctx.properties.collected_extensions = collected_unknown_extensions_cb;
+
+	server_tls_ctx.random_bytes = ptls_minicrypto_random_bytes;
+	server_tls_ctx.get_time = &ptls_get_time;
+	server_tls_ctx.cipher_suites = ptls_minicrypto_cipher_suites;
+	server_tls_ctx.key_exchanges = ptls_minicrypto_key_exchanges;
+	ret = ptls_load_certificates(&server_tls_ctx, cert_file);
+	ret = ptls_minicrypto_load_private_key(&server_tls_ctx, key_file);
+	server_tls_ctx.on_extension = &on_extension;
 	on_client_hello.cb = on_client_hello_cb;
-	server_ctx.on_extension = &on_extension;
-	server_ctx.on_client_hello = &on_client_hello;
-	server_ctx.use_exporter = 1;
+	server_tls_ctx.on_client_hello = &on_client_hello;
+	server_tls_ctx.use_exporter = 1;
 
-	server_properties.inner.collect_extension = should_collect_unknown_extension_cb;
-	server_properties.inner.collected_extensions = collected_unknown_extensions_cb;
+	server_tls = ptls_server_new(&server_tls_ctx);
 
-	server_tls = ptls_server_new(&server_ctx);
+	server_ctx.properties.collect_extension = should_collect_unknown_extension_cb;
+	server_ctx.properties.collected_extensions = collected_unknown_extensions_cb;
 	
 	ptls_buffer_init(&client_buf, "", 0);
-	ptls_handshake(client_tls, &client_buf, NULL, NULL, &client_prop);
+	ptls_handshake(client_tls, &client_buf, NULL, NULL, &client_ctx.properties);
 
 	ptls_buffer_init(&server_buf, "", 0);
 	len = client_buf.off;
-	ptls_handshake(server_tls, &server_buf, client_buf.base, &len, &server_properties.inner);
+	ptls_handshake(server_tls, &server_buf, client_buf.base, &len, &server_ctx.properties);
 
 	ptls_buffer_dispose(&client_buf);
 	ptls_buffer_init(&client_buf, "", 0);
 	len = server_buf.off;
-	ptls_handshake(client_tls, &client_buf, server_buf.base, &len, &client_prop);
+	ptls_handshake(client_tls, &client_buf, server_buf.base, &len, &client_ctx.properties);
 
 	ptls_buffer_dispose(&server_buf);
 	ptls_buffer_init(&server_buf, "", 0);
 	len = client_buf.off;
-	ptls_handshake(server_tls, &server_buf, client_buf.base, &len, &server_properties.inner);
+	ptls_handshake(server_tls, &server_buf, client_buf.base, &len, &server_ctx.properties);
 
 	ptls_cipher_suite_t *client_cipher;
 	uint8_t client_secret[64] = { 0 };
@@ -167,6 +185,9 @@ int main() {
 	assert(ptls_get_negotiated_protocol(server_tls) != NULL);
 	assert(strcmp(ptls_get_negotiated_protocol(server_tls), test_alpn) == 0);
 
-	assert(server_properties.extensions_len == sizeof(extension_2_data));
-	assert(memcmp(server_properties.extensions, extension_2_data, sizeof(extension_2_data)) == 0);
+	assert(server_ctx.extension_data_len == sizeof(extension_2_client_data) - 1);
+	assert(memcmp(server_ctx.extension_data, extension_2_client_data, sizeof(extension_2_client_data) - 1) == 0);
+
+	assert(client_ctx.extension_data_len == sizeof(extension_2_server_data) - 1);
+	assert(memcmp(client_ctx.extension_data, extension_2_server_data, sizeof(extension_2_server_data) - 1) == 0);
 }
