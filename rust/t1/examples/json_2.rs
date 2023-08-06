47bf590fb3cecce1f0a4cef9104ad23cbd44aeb7
hezhiwen <hezhiwen.coder@bytedance.com>
2023-08-06 23:36:23 +0800

    serde

--- rust/t1/examples/json_2.rs
+++ rust/t1/examples/json_2.rs
@@ -1,7 +1,9 @@
+use serde::Deserialize;
 use serde_json::{Result, Value};
 
 fn main() -> Result<()> {
     untyped_example()?;
+    typed_example()?;
 
     Ok(())
 }
@@ -18,7 +20,37 @@ fn untyped_example() -> Result<()> {
 
     let res: Value = serde_json::from_str(data)?;
 
-    println!("Please call {} at the number {}", res["name"], res["phones"][0]);
+    println!(
+        "Please call {} at the number {}",
+        res["name"], res["phones"][0]
+    );
 
     Ok(())
+}
+
+fn typed_example() -> Result<()> {
+    let data = r#"{
+        "name": "hzw",
+        "age": 36,
+        "phones": [
+            "+86 12345678901",
+            "+86 12345678902"
+        ]
+    }"#;
+
+    let p: Person = serde_json::from_str(data)?;
+
+    println!(
+        "Please call {}({}) at the number {}",
+        p.name, p.age, p.phones[0]
+    );
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct Person {
+    name: String,
+    age: i32,
+    phones: Vec<String>,
 }
