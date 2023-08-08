bacbbbdeca19c9bb3ac642f2ae6420abb15a3ef7
hezhiwen <hezhiwen.coder@bytedance.com>
2023-08-08 14:16:55 +0800

    picotls

--- c/2022/ConsoleSln1/ConsolePro1/main.c
+++ c/2022/ConsoleSln1/ConsolePro1/main.c
@@ -1,10 +1,13 @@
 extern int handshake_test();
+extern int handshake_test_2();
 extern void certificate_test();
 extern int cli_test(int argc, char* argv[]);
 
 int main(int argc, char* argv[]) {
 	handshake_test();
+	handshake_test_2();
 	certificate_test();
 	
-	return cli_test(argc, argv);
+	//return cli_test(argc, argv);
+	return 0;
 }
