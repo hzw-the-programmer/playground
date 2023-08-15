804cb4b73b5a13b626fb2afbbe6cd2f3c5cb35d4
hezhiwen <hezhiwen.coder@bytedance.com>
2023-08-15 15:41:35 +0800

    vs cmake

---
+++ c/2022/CMakeSln2/CMakeProject1/CMakeProject1.c
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include "CMakeProject1.h"
+
+typedef void (WINAPI* PGNSI)(LPSYSTEM_INFO);
+
+int main()
+{
+
+    // Call GetNativeSystemInfo if supported or GetSystemInfo otherwise.
+
+    PGNSI pGNSI;
+    SYSTEM_INFO si;
+
+    ZeroMemory(&si, sizeof(SYSTEM_INFO));
+
+    pGNSI = (PGNSI)GetProcAddress(
+        GetModuleHandle(TEXT("kernel32.dll")),
+        "GetNativeSystemInfo");
+    if (NULL != pGNSI)
+    {
+        pGNSI(&si);
+    }
+    else
+    {
+        GetSystemInfo(&si);
+    }
+    return 0;
+}
