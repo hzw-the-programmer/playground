3572f62896f9aeda65bcc6007ace1f11d3c33633
hezhiwen <hezhiwen.coder@bytedance.com>
2023-12-19 20:11:31 +0800

    play

---
+++ c/play3/play3/play/play.c
@@ -0,0 +1,15 @@
+﻿#include "play.h"
+
+void array_param(int a[16]);
+
+int main()
+{
+	int a[16] = { 0 };
+	array_param(a);
+	return 0;
+}
+
+void array_param(int a[16]) {
+	a[0] = 1;
+	a[1] = 2;
+}
