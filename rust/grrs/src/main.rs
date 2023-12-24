893d60251ac30f674626bd2e620d287d5917f18d
hezhiwen <hezhiwen.coder@bytedance.com>
2023-12-24 23:46:17 +0800

    grrs v5

--- rust/grrs/src/main.rs
+++ rust/grrs/src/main.rs
@@ -1,13 +1,14 @@
 use clap::Parser;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)?;
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line)
         }
     }
+    Ok(())
 }
 
 #[derive(Parser)]
