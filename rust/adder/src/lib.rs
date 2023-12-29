9313320843e20c5f0540d07e98e584487ca7b7e9
hezhiwen <hezhiwen.coder@bytedance.com>
2023-12-29 15:32:57 +0800

    rust test

--- rust/adder/src/lib.rs
+++ rust/adder/src/lib.rs
@@ -21,4 +21,28 @@ mod tests {
     fn another() {
         panic!("Make this test fail");
     }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
