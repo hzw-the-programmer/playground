a789b69f1462dc3cd2e0a2a6f70e2064e7a0ac12
hezhiwen <hezhiwen.coder@bytedance.com>
2023-12-30 23:16:33 +0800

    clap

---
+++ rust/t1/examples/clap2/hashmap.rs
@@ -0,0 +1,3 @@
+pub fn hashmap() {
+    println!("hashmap called")
+}
---
+++ rust/t1/examples/clap2/main.rs
@@ -0,0 +1,13 @@
+use clap::{Command};
+mod hashmap;
+
+fn main() {
+    let matches = Command::new("root")
+        .subcommand(Command::new("hashmap"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("hashmap", _)) => hashmap::hashmap(),
+        _ => unreachable!("parser should ensure only valid subcommand names are used"),
+    }
+}
