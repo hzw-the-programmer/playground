9d6dcd5bb9742487b8d3a94830e274dea7a750ae
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-02 16:01:17 +0800

    rust test

--- rust/adder/tests/integration_test.rs
+++ rust/adder/tests/integration_test.rs
@@ -6,3 +6,11 @@ use adder;
 fn it_adds_two() {
     assert_eq!(4, adder::adds_two(2));
 }
+
+mod common;
+
+#[test]
+fn another_test() {
+    common::setup();
+    assert_eq!(4, adder::adds_two(2));
+}
