086ed604092e63551819c879361c4a5f9b8dca2a
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-02 20:24:12 +0800

    rust vec

--- rust/t1/examples/vec_1.rs
+++ rust/t1/examples/vec_1.rs
@@ -1,3 +1,5 @@
+// cargo run --example vec_1
+
 fn test_1() {
     println!();
     println!("****** test_1 ******");
@@ -163,6 +165,166 @@ fn test_8() {
     println!("after loop");
 }
 
+// fn skip_while<P>(self, predicate: P) -> SkipWhile<Self, P>
+// where
+//     Self: Sized,
+//     P: FnMut(&Self::Item) -> bool,
+
+fn test_9() {
+    println!();
+    println!("****** test_9 ******");
+    println!();
+
+    let mut foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
+
+    let iter = foos.iter().skip_while(|foo| {
+        // let i: i32 = foo;
+        println!("Foo {} in predicate", foo.id);
+        foo.id < 3
+    });
+
+    println!("before loop");
+    for foo in iter {
+        // let i: i32 = foo;
+        println!("Foo {} in loop", foo.id);
+    }
+    println!("after loop");
+
+    foos.push(Foo { id: 5 });
+
+    println!("finish");
+}
+
+fn test_10() {
+    println!();
+    println!("****** test_10 ******");
+    println!();
+
+    let foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
+
+    let iter = foos.into_iter().skip_while(|foo| {
+        // let i: i32 = foo;
+        println!("Foo {} in predicate", foo.id);
+        foo.id < 3
+    });
+
+    println!("before loop");
+    for foo in iter {
+        println!("Foo {} in loop", foo.id);
+    }
+    println!("after loop");
+}
+
+// fn take_while<P>(self, predicate: P) -> TakeWhile<Self, P>
+// where
+//     Self: Sized,
+//     P: FnMut(&Self::Item) -> bool,
+
+fn test_11() {
+    println!();
+    println!("****** test_11 ******");
+    println!();
+
+    let mut foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
+
+    let iter = foos.iter().take_while(|foo| {
+        // let i: i32 = foo;
+        println!("Foo {} in predicate", foo.id);
+        foo.id < 3
+    });
+
+    println!("before loop");
+    for foo in iter {
+        // let i: i32 = foo;
+        println!("Foo {} in loop", foo.id);
+    }
+    println!("after loop");
+
+    foos.push(Foo { id: 5 });
+
+    println!("finish");
+}
+
+fn test_12() {
+    println!();
+    println!("****** test_12 ******");
+    println!();
+
+    let foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
+
+    let iter = foos.into_iter().take_while(|foo| {
+        // let i: i32 = foo;
+        println!("Foo {} in predicate", foo.id);
+        foo.id < 3
+    });
+
+    println!("before loop");
+    for foo in iter {
+        // let i: i32 = foo;
+        println!("Foo {} in loop", foo.id);
+    }
+    println!("after loop");
+}
+
+// fn map_while<B, P>(self, predicate: P) -> MapWhile<Self, P>
+// where
+//     Self: Sized,
+//     P: FnMut(Self::Item) -> Option<B>,
+
+fn test_13() {
+    println!();
+    println!("****** test_13 ******");
+    println!();
+
+    let mut foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
+
+    let iter = foos.iter().map_while(|foo| {
+        // let i: i32 = foo;
+        println!("Foo {} in predicate", foo.id);
+        if foo.id < 3 {
+            Some(foo.id)
+        } else {
+            None
+        }
+    });
+
+    println!("before loop");
+    for foo in iter {
+        // let i: i32 = foo;
+        println!("Foo {} in loop", foo);
+    }
+    println!("after loop");
+
+    foos.push(Foo { id: 5 });
+
+    println!("finish");
+}
+
+fn test_14() {
+    println!();
+    println!("****** test_14 ******");
+    println!();
+
+    let foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
+
+    let iter = foos.into_iter().map_while(|foo| {
+        // let i: i32 = foo;
+        println!("Foo {} in predicate", foo.id);
+        if foo.id < 3 {
+            Some(foo.id)
+        } else {
+            None
+        }
+    });
+
+    println!("before loop");
+    for foo in iter {
+        // let i: i32 = foo;
+        println!("Foo {} in loop", foo);
+    }
+    println!("after loop");
+}
+
 fn main() {
     test_1();
     test_2();
@@ -172,6 +334,12 @@ fn main() {
     test_6();
     test_7();
     test_8();
+    test_9();
+    test_10();
+    test_11();
+    test_12();
+    test_13();
+    test_14();
 }
 
 struct Foo {
