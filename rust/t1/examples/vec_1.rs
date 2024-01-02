c490ad0a86f4f54cc22fdfb7df9c0a43673f5866
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-02 19:33:27 +0800

    vec

--- rust/t1/examples/vec_1.rs
+++ rust/t1/examples/vec_1.rs
@@ -1,4 +1,8 @@
 fn test_1() {
+    println!();
+    println!("****** test_1 ******");
+    println!();
+
     let foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }];
 
     for _foo in foos {
@@ -8,26 +12,44 @@ fn test_1() {
     // for _foo in foos {
     //     println!("in loop");
     // }
+
+    println!("finish");
 }
 
 fn test_2() {
+    println!();
+    println!("****** test_2 ******");
+    println!();
+
     let foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }];
 
     for _foo in &foos {
         println!("in loop");
     }
+
+    println!("finish");
 }
 
 fn test_3() {
+    println!();
+    println!("****** test_3 ******");
+    println!();
+
     let mut foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }];
 
     for foo in &mut foos {
         println!("in loop");
         foo.id += 1;
     }
+
+    println!("finish");
 }
 
 fn test_4() {
+    println!();
+    println!("****** test_4 ******");
+    println!();
+
     let ids = vec![1, 2, 3, 4];
 
     for id in ids {
@@ -37,21 +59,39 @@ fn test_4() {
     // for id in ids {
     //     println!("id={id}");
     // }
+
+    println!("finish");
 }
 
 fn test_5() {
+    println!();
+    println!("****** test_5 ******");
+    println!();
+
     let mut foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
 
-    let filtered = foos.iter().filter(|foo| return foo.id % 2 == 0);
+    let filtered = foos.iter().filter(|foo| {
+        let ret = foo.id % 2 == 0;
+        println!("Foo {} filtered: {}", foo.id, ret);
+        ret
+    });
 
+    println!("before loop");
     for foo in filtered {
         println!("Foo {} in loop", foo.id);
     }
+    println!("after loop");
 
     foos.push(Foo { id: 5 });
+
+    println!("finish");
 }
 
 fn test_6() {
+    println!();
+    println!("****** test_6 ******");
+    println!();
+
     let mut foos = vec![Foo { id: 1 }, Foo { id: 2 }, Foo { id: 3 }, Foo { id: 4 }];
 
     let filtered = foos.into_iter().filter(|foo| {
