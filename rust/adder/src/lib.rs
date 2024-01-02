72dc5d41c7b07dfec43ea447a963fdb68c2993f9
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-02 14:53:35 +0800

    rust

--- rust/adder/src/lib.rs
+++ rust/adder/src/lib.rs
@@ -67,7 +67,7 @@ mod tests {
 
     #[test]
     fn it_works_1() -> Result<(), String> {
-        if 2 + 2 == 4 {
+        if 2 + 2 != 4 {
             Ok(())
         } else {
             Err(String::from("two plus two does not equal four"))
