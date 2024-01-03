8cca94bff79cabfeea3ed1c51667404c5854585b
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-03 23:21:25 +0800

    drop

--- rust/t1/examples/drop_1.rs
+++ rust/t1/examples/drop_1.rs
@@ -40,9 +40,21 @@ fn test_3() {
     println!("finish");
 }
 
+fn test_4() {
+    println!("\ntest_4\n");
+    let foo = Foo { id: 1 };
+    let bar = Bar2 {
+        id: 1,
+        foo1: Foo { id: 2 },
+        foo2: Foo { id: 3 },
+    };
+    let b = Bar3 { id: 1 };
+}
+
 fn main() {
     test_1();
     test_2();
+    test_4();
 }
 
 struct Bar {
@@ -61,4 +73,8 @@ struct Bar2 {
     id: u64,
     foo1: Foo,
     foo2: Foo,
+}
+
+struct Bar3 {
+    id: u64,
 }
