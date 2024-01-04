f2ecf115b3cf10baf3c15ff089446439344dea7a
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-04 15:57:03 +0800

    ptr

--- rust/t1/examples/alloc.rs
+++ rust/t1/examples/alloc.rs
@@ -1,13 +1,16 @@
 use std::alloc::{alloc, dealloc, handle_alloc_error, Layout};
+use std::ptr;
 use t1::Foo;
 
 fn main() {
     test1();
     test2();
     test3();
+    test4();
 }
 
 fn test1() {
+    println!("\ntest1\n");
     unsafe {
         let layout = Layout::new::<u16>();
         let ptr = alloc(layout);
@@ -21,6 +24,7 @@ fn test1() {
 }
 
 fn test2() {
+    println!("\ntest2\n");
     unsafe {
         let layout = Layout::new::<Foo>();
         let ptr = alloc(layout);
@@ -39,6 +43,7 @@ fn test2() {
 }
 
 fn test3() {
+    println!("\ntest3\n");
     unsafe {
         let layout = Layout::new::<Foo>();
         let ptr = alloc(layout) as *mut Foo;
@@ -47,5 +52,16 @@ fn test3() {
         }
         let f = Box::from_raw(ptr);
         // let f1 = Box::from_raw(ptr);
+    }
+}
+
+fn test4() {
+    println!("\ntest4\n");
+    let f = Foo { id: 1 };
+    let b = Box::new(f);
+    let p = Box::into_raw(b);
+    unsafe {
+        ptr::drop_in_place(p);
+        dealloc(p as *mut u8, Layout::new::<Foo>());
     }
 }
---
+++ rust/t1/examples/ptr.rs
@@ -0,0 +1,37 @@
+use std::ptr;
+use t1::Foo;
+
+fn main() {
+    test1();
+    test2();
+    test3();
+}
+
+fn test1() {
+    println!("\ntest1\n");
+    let mut a = [1, 2, 3];
+    // let i: i32 = a;
+    let p = a.as_mut_ptr();
+    // let i: i32 = p;
+    unsafe {
+        println!("{}", *p.offset(1));
+        println!("{}", *p.offset(2));
+    }
+}
+
+fn test2() {
+    println!("\ntest2\n");
+    let f = Foo { id: 1 };
+    let p = &f as *const Foo;
+    let mut f1 = unsafe { ptr::read(p) };
+    f1.id = 2;
+}
+
+fn test3() {
+    println!("\ntest3\n");
+    let mut f = Foo { id: 1 };
+    let p = &mut f as *mut Foo;
+    unsafe {
+        ptr::write(p, Foo { id: 2 });
+    };
+}
