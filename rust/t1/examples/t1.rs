0c0757f8d7c75802f95e50d9e6ff04b7607f65ef
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-04 23:37:02 +0800

    mut

--- rust/t1/examples/t1.rs
+++ rust/t1/examples/t1.rs
@@ -8,6 +8,29 @@ impl Drop for Foo {
     }
 }
 fn main() {
+    test1();
+    test2();
+    test3();
+}
+
+fn test1() {
+    println!("\ntest1\n");
     let mut _foo1 = Foo { id: 1 };
     let mut _foo2 = Foo { id: 2 };
+}
+
+fn test2() {
+    println!("\ntest2\n");
+    let v = vec![Foo { id: 1 }, Foo { id: 2 }];
+    let mut i = &v[0];
+    // i.id = 3;
+    i = &v[1];
+}
+
+fn test3() {
+    println!("\ntest3\n");
+    let mut v = vec![Foo { id: 1 }, Foo { id: 2 }];
+    let i = &mut v[0];
+    i.id = 3;
+    // i = &mut v[1];
 }
