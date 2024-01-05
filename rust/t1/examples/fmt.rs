9a9a0f10f0c3e3f64800a046e047900ea9794654
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-05 16:48:16 +0800

    fmt

--- rust/t1/examples/fmt.rs
+++ rust/t1/examples/fmt.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::io::{Result, Write};
 
 // impl<A: Allocator> Write for Vec<u8, A>
@@ -19,5 +20,38 @@ fn main() -> Result<()> {
     println!("len={},cap={}", v.len(), v.capacity());
     assert_eq!(v, b"hello world!");
 
+    // write!(
+    //     v,
+    //     " I'm here: {:?}",
+    //     Position {
+    //         longitude: 121.48054,
+    //         latitude: 31.23593
+    //     }
+    // );
+    // println!("len={},cap={}", v.len(), v.capacity());
+    // assert_eq!(v, b"hello world! I'm here: Position { longitude: 121.48054, latitude: 31.23593 }");
+    write!(
+        v,
+        " I'm here: {}",
+        Position {
+            longitude: 121.48054,
+            latitude: 31.23593
+        }
+    );
+    println!("len={},cap={}", v.len(), v.capacity());
+    assert_eq!(v, b"hello world! I'm here: (121.48054, 31.23593)");
+
     Ok(())
+}
+
+#[derive(Debug)]
+struct Position {
+    longitude: f32,
+    latitude: f32,
+}
+
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.longitude, self.latitude)
+    }
 }
