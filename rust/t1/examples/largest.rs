1ed65a1365c4eb1aca0aadee69c9c4a9df444d7e
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-05 11:03:09 +0800

    generics

--- rust/t1/examples/largest.rs
+++ rust/t1/examples/largest.rs
@@ -1,11 +1,11 @@
 fn main() {
     let nums = vec![1, 2, 3];
     // let n: i32 = v;
-    let largest = largest(&nums);
+    let result = largest(&nums);
     // let n: i32 = v;
     println!("&nums[0]={:?}", &nums[0] as *const i32);
     println!("&nums[2]={:?}", &nums[2] as *const i32);
-    println!("largest ={:?}", largest as *const i32);
+    println!("result  ={:?}", result as *const i32);
 
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest_i32(&number_list);
@@ -14,13 +14,21 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
 
-fn largest(nums: &[i32]) -> &i32 {
-    let mut largest = &nums[0];
-    for num in nums {
-        if num > largest {
-            largest = num;
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
     largest
