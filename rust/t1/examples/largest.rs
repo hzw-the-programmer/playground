83e29fda85d9c301413331d45e9da7bd19c87d49
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-05 10:56:35 +0800

    rust

--- rust/t1/examples/largest.rs
+++ rust/t1/examples/largest.rs
@@ -6,6 +6,14 @@ fn main() {
     println!("&nums[0]={:?}", &nums[0] as *const i32);
     println!("&nums[2]={:?}", &nums[2] as *const i32);
     println!("largest ={:?}", largest as *const i32);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
 
 fn largest(nums: &[i32]) -> &i32 {
@@ -15,5 +23,29 @@ fn largest(nums: &[i32]) -> &i32 {
             largest = num;
         }
     }
+    largest
+}
+
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
     largest
 }
