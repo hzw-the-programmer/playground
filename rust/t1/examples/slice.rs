718940134a9fa53d32fb34f42c2ec71e058d5648
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-12 18:46:10 +0800

    slice

--- rust/t1/examples/slice.rs
+++ rust/t1/examples/slice.rs
@@ -8,9 +8,20 @@ fn main() {
     println!("{}", mem::size_of::<Foo>());
     println!("{}", mem::size_of::<&Foo>());
     println!("{}", mem::size_of::<&[u8]>());
+
+    test2();
 }
 
 struct Foo {
     n1: u8,
     n2: u8,
+}
+
+fn test2() {
+    let a = [1, 2, 3];
+    // let n: i32 = a;
+    let b = &[1, 2, 3];
+    // let n: i32 = b;
+    let e = unsafe { a.get_unchecked(0) };
+    println!("{}", e);
 }
