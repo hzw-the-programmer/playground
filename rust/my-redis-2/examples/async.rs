650fbffc378eef89eb0b911aa7e5bcab843ed413
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-17 10:55:02 +0800

    async

--- rust/my-redis-2/examples/async.rs
+++ rust/my-redis-2/examples/async.rs
@@ -15,17 +15,19 @@ async fn say_world() {
 //     op.await;
 // }
 
+async fn new_task() {
+    // Calling `say_world()` does not execute the body of `say_world()`.
+    let op = say_world();
+    // let n: i32 = op;
+
+    // This println! comes first
+    println!("hello");
+
+    // Calling `.await` on `op` starts executing `say_world`.
+    op.await;
+}
+
 fn main() {
     let mut rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(async {
-        // Calling `say_world()` does not execute the body of `say_world()`.
-        let op = say_world();
-        // let n: i32 = op;
-
-        // This println! comes first
-        println!("hello");
-
-        // Calling `.await` on `op` starts executing `say_world`.
-        op.await;
-    })
+    rt.block_on(new_task())
 }
