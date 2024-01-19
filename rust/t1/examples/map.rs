9d9a377fae0ff63c6af193bb9056cb94b27e6d7c
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-19 15:10:15 +0800

    map

--- rust/t1/examples/map.rs
+++ rust/t1/examples/map.rs
@@ -1,10 +1,15 @@
 use std::collections::HashMap;
 
+use t1::Foo;
+
 fn main() {
     test1();
+    test2();
 }
 
 fn test1() {
+    print!("\n* test1\n");
+
     let mut map = HashMap::new();
 
     assert_eq!(map.insert(37, "a"), None);
@@ -14,4 +19,17 @@ fn test1() {
 
     assert_eq!(map.insert(37, "c"), Some("b"));
     assert_eq!(map[&37], "c");
+}
+
+fn test2() {
+    print!("\n* test2\n");
+
+    let mut map = HashMap::new();
+    map.insert(1, Foo { id: 1 });
+    map.insert(2, Foo { id: 2 });
+
+    {
+        let f = map.insert(1, Foo { id: 3 }).unwrap();
+    }
+    println!("finish");
 }
