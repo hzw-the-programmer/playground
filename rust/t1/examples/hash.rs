9e69e1fe2328374e55a228c19ea0883b66f94894
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-19 16:11:28 +0800

    Hasher

--- rust/t1/examples/hash.rs
+++ rust/t1/examples/hash.rs
@@ -4,6 +4,7 @@ use std::hash::Hasher;
 fn main() {
     test1();
     test2();
+    test3();
 }
 
 fn test1() {
@@ -30,6 +31,17 @@ fn test2() {
     // let n: i32 = data[..];
     // let a = data[..];
     hasher.write(&data);
+
+    println!("Hash is {:x}!", hasher.finish());
+}
+
+fn test3() {
+    let mut hasher = DefaultHasher::new();
+
+    hasher.write_u32(1989);
+    hasher.write_u8(11);
+    hasher.write_u8(9);
+    hasher.write(b"Huh?");
 
     println!("Hash is {:x}!", hasher.finish());
 }
