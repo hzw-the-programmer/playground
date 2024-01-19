115d0a0bfa1ee624e4920c78b391226558629954
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-19 16:59:57 +0800

    map

--- rust/t1/examples/map.rs
+++ rust/t1/examples/map.rs
@@ -6,6 +6,7 @@ fn main() {
     test1();
     test2();
     test3();
+    test4();
 }
 
 fn test1() {
@@ -42,4 +43,31 @@ fn test3() {
     map.insert(2, Foo { id: 2 });
     map.remove(&2);
     println!("finish");
+}
+
+#[derive(Eq, PartialEq, Hash)]
+struct Bar {
+    id: i32,
+}
+
+impl Drop for Bar {
+    fn drop(&mut self) {
+        println!("Bar {} drop", self.id);
+    }
+}
+
+fn test4() {
+    print!("\n* test4\n");
+    let mut map = HashMap::new();
+    map.insert(Bar { id: 1 }, Foo { id: 1 });
+    map.insert(Bar { id: 2 }, Foo { id: 2 });
+    map.insert(Bar { id: 3 }, Foo { id: 3 });
+    {
+        let k = Bar { id: 4 };
+        let v = Foo { id: 4 };
+        map.insert(k, v);
+    }
+    let v = map.get(&Bar { id: 1 }).unwrap();
+    println!("{v:?}");
+    println!("finish")
 }
