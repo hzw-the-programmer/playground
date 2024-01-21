9fa0001dd63f9f8b2173dd3078a6c997e20df1bd
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-21 22:21:51 +0800

    rocket

---
+++ rust/hello-rocket/src/main.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/world")] // <- route attribute
+fn world() -> &'static str {
+    // <- request handler
+    "hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/hello", routes![world])
+        .mount("/hi", routes![world])
+}
