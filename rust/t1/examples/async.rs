9d6bac3fd7f6a5b9859249458e561f706c75967a
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-22 11:43:41 +0800

    async

--- rust/t1/examples/async.rs
+++ rust/t1/examples/async.rs
@@ -8,6 +8,10 @@ async fn hello_world() {
 }
 
 fn main() {
+    println!("1");
     let future = hello_world(); // Nothing is printed
+                                // let n: i32 = future;
+    println!("2");
     block_on(future); // `future` is run and "hello, world!" is printed
+    println!("3");
 }
