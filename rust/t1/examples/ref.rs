c7d7618550edca3655a13026116ac35d55056b43
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-23 14:37:34 +0800

    ref

--- rust/t1/examples/ref.rs
+++ rust/t1/examples/ref.rs
@@ -2,6 +2,10 @@ trait Work {
     fn work(self);
 }
 
+trait WorkRef {
+    fn work_ref(&self);
+}
+
 struct Foo {
     id: i32,
 }
@@ -12,15 +16,43 @@ impl Work for Foo {
     }
 }
 
+impl WorkRef for Foo {
+    fn work_ref(&self) {
+        println!("Foo.work_ref");
+    }
+}
+
 impl Work for &Foo {
     fn work(self) {
         println!("&Foo.work");
     }
 }
 
-fn main() {
+impl WorkRef for &Foo {
+    fn work_ref(&self) {
+        println!("&Foo.work_ref");
+    }
+}
+
+fn test1() {
+    print!("\ntest1\n\n");
     let f = Foo { id: 1 };
     f.work();
     let f = &Foo { id: 2 };
     f.work();
+}
+
+fn test2() {
+    print!("\ntest2\n\n");
+    let f = Foo { id: 1 };
+    f.work_ref();
+    let f = &Foo { id: 2 };
+    f.work_ref();
+    let f = &&Foo { id: 2 };
+    f.work_ref();
+}
+
+fn main() {
+    test1();
+    test2();
 }
