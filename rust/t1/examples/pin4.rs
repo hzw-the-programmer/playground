2133d5a764e46f074caf9511e15feef1bd14dd78
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-23 15:10:38 +0800

    pin

--- rust/t1/examples/pin4.rs
+++ rust/t1/examples/pin4.rs
@@ -1,5 +1,6 @@
 fn main() {
     test1();
+    test2();
 }
 
 struct Foo {
@@ -10,17 +11,53 @@ struct Foo {
 impl Foo {
     fn consume(self) {
         println!("{:p}", &self.id);
+        println!("{:p}", &self.ptr);
         println!("{:p}", self.ptr);
     }
 }
 
 fn test1() {
+    print!("\ntest1\n\n");
+
     let mut f = Foo {
         id: 1,
         ptr: std::ptr::null(),
     };
     f.ptr = &f.id;
+
     println!("{:p}", &f.id);
+    println!("{:p}", &f.ptr);
     println!("{:p}", f.ptr);
+
     f.consume();
+}
+
+fn test2() {
+    print!("\ntest2\n\n");
+
+    let mut f = Foo {
+        id: 1,
+        ptr: std::ptr::null(),
+    };
+    f.ptr = &f.id;
+
+    println!("{:p}", &f.id);
+    println!("{:p}", &f.ptr);
+    println!("{:p}", f.ptr);
+
+    let func_ref = || {
+        println!("{:p}", &f.id);
+        println!("{:p}", &f.ptr);
+        println!("{:p}", f.ptr);
+    };
+    println!();
+    func_ref();
+
+    let func = move || {
+        println!("{:p}", &f.id);
+        println!("{:p}", &f.ptr);
+        println!("{:p}", f.ptr);
+    };
+    println!();
+    func();
 }
