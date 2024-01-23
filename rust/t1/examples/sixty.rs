ab09dba20b647b2a84d370bccd5fffd88647b1e0
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-23 19:58:27 +0800

    signal

--- rust/t1/examples/sixty.rs
+++ rust/t1/examples/sixty.rs
@@ -2,6 +2,9 @@ use std::process;
 use std::thread::sleep;
 use std::time;
 
+// kill -SIGSTOP 23221
+// kill -SIGCONT 23221
+
 fn main() {
     let delay = time::Duration::from_secs(1);
     let pid = process::id();
