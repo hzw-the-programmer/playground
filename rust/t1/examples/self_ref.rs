4acd6840f84536481963f7272684d8962b6048fe
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-24 12:03:45 +0800

    ownership

--- rust/t1/examples/self_ref.rs
+++ rust/t1/examples/self_ref.rs
@@ -1,11 +1,11 @@
 fn main() {
-    test1();
-    test2();
-    test3();
-    test4();
-    test5();
-    test6();
-    test7();
+    let tests = vec![
+        test1, test2, test2_1, test3, test3_1, test4, test5, test6, test6_1, test7, test8,
+    ];
+    for (i, test) in tests.iter().enumerate() {
+        print!("\n*** test {i} ***\n\n");
+        test();
+    }
 }
 
 struct Foo {
@@ -27,8 +27,6 @@ impl Drop for Foo {
 }
 
 fn test1() {
-    print!("\ntest1\n\n");
-
     let mut f = Foo {
         id: 1,
         ptr: std::ptr::null(),
@@ -44,64 +42,168 @@ fn test1() {
 }
 
 fn test2() {
-    print!("\ntest2\n\n");
-
     let mut f = Foo {
         id: 1,
         ptr: std::ptr::null(),
     };
     f.ptr = &f.id;
 
+    println!("{:016p}", &f);
     println!("{:016p}", &f.id);
+    println!("{:016p}", &f.ptr);
     println!("{:016p}", f.ptr);
 
     let func_ref = || {
+        println!("{:016p}", &f);
         println!("{:016p}", &f.id);
+        println!("{:016p}", &f.ptr);
         println!("{:016p}", f.ptr);
     };
-    println!();
+    println!("\nref");
     func_ref();
 
     let func = move || {
+        println!("{:016p}", &f);
         println!("{:016p}", &f.id);
+        println!("{:016p}", &f.ptr);
         println!("{:016p}", f.ptr);
     };
-    println!();
+    println!("\nmove");
     func();
 
     println!("finish");
 }
 
-fn test3() {
-    print!("\ntest3\n\n");
+fn test2_1() {
+    let mut f = Foo {
+        id: 1,
+        ptr: std::ptr::null(),
+    };
+    f.ptr = &f.id;
 
+    println!("{:016p}", &f);
+    println!("{:016p}", &f.id);
+    println!("{:016p}", &f.ptr);
+    println!("{:016p}", f.ptr);
+
+    {
+        let func = || {
+            println!("{:016p}", &f);
+            println!("{:016p}", &f.id);
+            println!("{:016p}", &f.ptr);
+            println!("{:016p}", f.ptr);
+        };
+
+        println!("\nref");
+        func();
+    }
+
+    {
+        let func = move || {
+            println!("{:016p}", &f.id);
+            println!("{:016p}", &f.ptr);
+            println!("{:016p}", f.ptr);
+        };
+
+        println!("\nmove");
+        func();
+    }
+
+    {
+        let func = move || {
+            println!("{:016p}", &f);
+            println!("{:016p}", &f.id);
+            println!("{:016p}", &f.ptr);
+            println!("{:016p}", f.ptr);
+        };
+
+        println!("\nmove");
+        func();
+    }
+
+    println!("finish");
+}
+
+fn test3() {
     let f = Foo {
         id: 1,
         ptr: std::ptr::null(),
     };
     println!("stack");
+    println!("{:016p}", &f);
     println!("{:016p}", &f.id);
+    println!("{:016p}", &f.ptr);
     println!("{:016p}", f.ptr);
 
     let mut p = Box::new(f);
     p.ptr = &p.id;
     println!("heap");
+    println!("{:016p}", p);
     println!("{:016p}", &p.id);
+    println!("{:016p}", &p.ptr);
     println!("{:016p}", p.ptr);
 
     let func_ref = || {
+        println!("{:016p}", p);
         println!("{:016p}", &p.id);
+        println!("{:016p}", &p.ptr);
         println!("{:016p}", p.ptr);
     };
-    println!();
+    println!("\nref");
     func_ref();
 
     let func = move || {
+        println!("{:016p}", p);
         println!("{:016p}", &p.id);
+        println!("{:016p}", &p.ptr);
         println!("{:016p}", p.ptr);
     };
-    println!();
+    println!("\nmove");
     func();
+
+    println!("finish");
+}
+
+fn test3_1() {
+    let f = Foo {
+        id: 1,
+        ptr: std::ptr::null(),
+    };
+    println!("stack");
+    println!("{:016p}", &f);
+    println!("{:016p}", &f.id);
+    println!("{:016p}", &f.ptr);
+    println!("{:016p}", f.ptr);
+
+    let mut p = Box::new(f);
+    p.ptr = &p.id;
+    println!("heap");
+    println!("{:016p}", p);
+    println!("{:016p}", &p.id);
+    println!("{:016p}", &p.ptr);
+    println!("{:016p}", p.ptr);
+
+    {
+        let func = || {
+            println!("{:016p}", p);
+            println!("{:016p}", &p.id);
+            println!("{:016p}", &p.ptr);
+            println!("{:016p}", p.ptr);
+        };
+        println!("\nref");
+        func();
+    }
+
+    {
+        let func = move || {
+            println!("{:016p}", p);
+            println!("{:016p}", &p.id);
+            println!("{:016p}", &p.ptr);
+            println!("{:016p}", p.ptr);
+        };
+        println!("\nmove");
+        func();
+    }
 
     println!("finish");
 }
@@ -117,8 +219,6 @@ impl Drop for Bar {
 }
 
 fn test4() {
-    print!("\ntest4\n\n");
-
     let b = Bar { id: 1 };
     println!("{:016p}", &b.id);
 
@@ -131,8 +231,6 @@ fn test4() {
 }
 
 fn test5() {
-    print!("\ntest5\n\n");
-
     let b = Bar { id: 1 };
     println!("{:016p}", &b);
 
@@ -145,8 +243,6 @@ fn test5() {
 }
 
 fn test6() {
-    print!("\ntest6\n\n");
-
     let h;
     {
         let b = Bar { id: 1 };
@@ -162,15 +258,43 @@ fn test6() {
     println!("finish");
 }
 
-fn test7() {
-    print!("\ntest7\n\n");
+fn test6_1() {
+    let h;
+    {
+        let b = Bar { id: 1 };
+        println!("{:016p}", &b.id);
 
+        h = std::thread::spawn(move || {
+            println!("{:016p}", &b);
+            println!("{}", b.id);
+        });
+    }
+    println!("before join");
+    h.join();
+    println!("finish");
+}
+
+fn test7() {
     let h;
     {
         let v = vec![1, 2, 3];
 
         h = std::thread::spawn(move || {
             println!("{:?}", v);
+        });
+    }
+    println!("before join");
+    h.join();
+    println!("finish");
+}
+
+fn test8() {
+    let h;
+    {
+        let v = vec![1, 2, 3];
+
+        h = std::thread::spawn(move || {
+            println!("{:?}", v[0]);
         });
     }
     println!("before join");
