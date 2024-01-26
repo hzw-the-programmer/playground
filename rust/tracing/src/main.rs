1b830669a1d1753d3dbffd9bc367301bd147a51a
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-26 20:26:08 +0800

    tracing

--- rust/tracing/src/main.rs
+++ rust/tracing/src/main.rs
@@ -1,3 +1,5 @@
+use tracing::{event, instrument, span, Level};
+
 fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::TRACE)
@@ -5,4 +7,45 @@ fn main() {
 
     log::debug!("this is a log line");
     tracing::debug!("this is a tracing line");
+
+    {
+        let span = span!(Level::ERROR, "ERROR_SPAN");
+        let _enter = span.enter();
+        event!(Level::ERROR, "ERROR");
+        event!(Level::WARN, "WARN");
+        event!(Level::INFO, "INFO");
+        event!(Level::DEBUG, "DEBUG");
+        event!(Level::TRACE, "TRACE");
+    }
+
+    {
+        let span = span!(Level::WARN, "WARN_SPAN");
+        let _enter = span.enter();
+        event!(Level::ERROR, "ERROR");
+        event!(Level::WARN, "WARN");
+        event!(Level::INFO, "INFO");
+        event!(Level::DEBUG, "DEBUG");
+        event!(Level::TRACE, "TRACE");
+    }
+
+    parent(0);
+}
+
+#[instrument]
+pub fn parent(arg: usize) {
+    event!(Level::ERROR, "ERROR");
+    event!(Level::WARN, "WARN");
+    event!(Level::INFO, "INFO");
+    event!(Level::DEBUG, "DEBUG");
+    event!(Level::TRACE, "TRACE");
+    child(arg + 1);
+}
+
+#[instrument]
+pub fn child(arg: usize) {
+    event!(Level::ERROR, "ERROR");
+    event!(Level::WARN, "WARN");
+    event!(Level::INFO, "INFO");
+    event!(Level::DEBUG, "DEBUG");
+    event!(Level::TRACE, "TRACE");
 }
