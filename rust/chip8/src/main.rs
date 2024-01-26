13a0df2c21c72b15bcebdf5296c8d2f9561ce534
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-26 11:36:53 +0800

    chip8 v3

--- rust/chip8/src/main.rs
+++ rust/chip8/src/main.rs
@@ -3,33 +3,42 @@ fn main() {
         registers: [0; 16],
         position_in_memory: 0,
         memory: [0; 0x1000],
+        stack: [0; 16],
+        stack_pointer: 0,
     };
 
     let memory = &mut cpu.memory;
 
-    memory[0] = 0x80;
-    memory[1] = 0x14;
-    memory[2] = 0x80;
-    memory[3] = 0x24;
-    memory[4] = 0x80;
-    memory[5] = 0x34;
+    memory[0] = 0x21;
+    memory[1] = 0x00;
+    memory[2] = 0x21;
+    memory[3] = 0x00;
+    memory[4] = 0x00;
+    memory[5] = 0x00;
+
+    memory[0x100] = 0x80;
+    memory[0x101] = 0x14;
+    memory[0x102] = 0x80;
+    memory[0x103] = 0x14;
+    memory[0x104] = 0x00;
+    memory[0x105] = 0xEE;
 
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
-    cpu.registers[2] = 10;
-    cpu.registers[3] = 10;
 
     cpu.run();
 
-    assert_eq!(cpu.registers[0], 35);
+    assert_eq!(cpu.registers[0], 45);
 
-    println!("5 + 10 + 10 + 10 = {}", cpu.registers[0]);
+    println!("5 + 10 + 10 + 10 + 10 = {}", cpu.registers[0]);
 }
 
 struct CPU {
     registers: [u8; 16],
     position_in_memory: usize,
     memory: [u8; 0x1000], // 4096, 4k, 2_i32.pow(12)
+    stack: [u16; 16],
+    stack_pointer: usize,
 }
 
 impl CPU {
@@ -43,9 +52,13 @@ impl CPU {
             let y = ((opcode & 0x00F0) >> 4) as u8;
             let d = ((opcode & 0x000F) >> 0) as u8;
 
+            let nnn = opcode & 0x0FFF;
+
             match (c, x, y, d) {
-                (0x00, 0x00, 0x00, 0x00) => return,
                 (0x08, _, _, 0x04) => self.add_xy(x, y),
+                (0x02, _, _, _) => self.call(nnn),
+                (0x00, 0x00, 0x0E, 0x0E) => self.ret(),
+                (0x00, 0x00, 0x00, 0x00) => return,
                 _ => todo!("opcode 0x{:04x}", opcode),
             }
         }
@@ -69,5 +82,16 @@ impl CPU {
         } else {
             self.registers[0x0F] = 0;
         }
+    }
+
+    fn call(&mut self, addr: u16) {
+        self.stack[self.stack_pointer] = self.position_in_memory as u16;
+        self.stack_pointer += 1;
+        self.position_in_memory = addr as usize;
+    }
+
+    fn ret(&mut self) {
+        self.stack_pointer -= 1;
+        self.position_in_memory = self.stack[self.stack_pointer] as usize;
     }
 }
