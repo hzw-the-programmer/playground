0085dc382498f05d051c7a86d3a0db0443d6879a
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-30 20:08:44 +0800

    crawler

--- rust/rust_crawler/src/main.rs
+++ rust/rust_crawler/src/main.rs
@@ -9,13 +9,15 @@ use std::io::Read;
 use std::path::Path;
 use std::time::Instant;
 
+// https://rolisz.ro/2020/03/01/web-crawler-in-rust/
+
 fn main() {
     let now = Instant::now();
 
     let client = reqwest::blocking::Client::new();
     let origin_url = "https://rolisz.ro/";
 
-    let body = fetch_url(&client, origin_url);
+    let body = fetch_url(&client, origin_url).unwrap();
     write_file("", &body);
 
     let mut visited = HashSet::new();
@@ -27,29 +29,42 @@ fn main() {
         .collect::<HashSet<String>>();
 
     while !new_urls.is_empty() {
-        let found_urls: HashSet<String> = new_urls
+        let (found_urls, errors): (Vec<Result<HashSet<String>>>, Vec<_>) = new_urls
             .par_iter()
-            .map(|url| {
-                let body = fetch_url(&client, url);
-                write_file(&url[origin_url.len() - 1..], &body);
+            .map(|url| -> Result<HashSet<String>> {
+                let body = fetch_url(&client, url)?;
+                write_file(&url[origin_url.len() - 1..], &body)?;
 
                 let links = get_links_from_html(&body);
                 println!("Visited: {} found {} links", url, links.len());
 
-                links
+                Ok(links)
             })
-            .reduce(HashSet::new, |mut acc, x| {
-                acc.extend(x);
-                acc
-            });
+            .partition(Result::is_ok);
+
         visited.extend(new_urls);
 
         new_urls = found_urls
+            .into_par_iter()
+            .map(Result::unwrap)
+            .reduce(HashSet::new, |mut acc, x| {
+                acc.extend(x);
+                acc
+            })
             .difference(&visited)
             .map(|x| x.to_string())
             .collect::<HashSet<String>>();
-        println!("New urls: {}", new_urls.len())
+
+        println!("New urls: {}", new_urls.len());
+        println!(
+            "Errors: {:#?}",
+            errors
+                .into_iter()
+                .map(Result::unwrap_err)
+                .collect::<Vec<Error>>()
+        );
     }
+
     println!("URLs: {:#?}", found_urls);
     println!("{}", now.elapsed().as_secs());
 }
@@ -84,20 +99,49 @@ fn normalize_url(url: &str) -> Option<String> {
     }
 }
 
-fn fetch_url(client: &reqwest::blocking::Client, url: &str) -> String {
-    let mut res = client.get(url).send().unwrap();
+fn fetch_url(client: &reqwest::blocking::Client, url: &str) -> Result<String> {
+    let mut res = client.get(url).send().map_err(|e| (url, e))?;
     println!("Status for {}: {}", url, res.status());
 
     let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    body
+    res.read_to_string(&mut body).map_err(|e| (url, e))?;
+    Ok(body)
 }
 
-fn write_file(path: &str, content: &str) {
-    fs::create_dir_all(format!("static{}", path)).unwrap();
-    fs::write(format!("static{}/index.html", path), content);
+fn write_file(path: &str, content: &str) -> Result<()> {
+    let dir = format!("static{}", path);
+    fs::create_dir_all(&dir).map_err(|e| (&dir, e))?;
+    let index = dir + "/index.html";
+    fs::write(&index, content).map_err(|e| (&index, e))?;
+    Ok(())
 }
 
 fn has_extension(url: &&str) -> bool {
     Path::new(url).extension().is_none()
+}
+
+#[derive(Debug)]
+enum Error {
+    Write { url: String, e: std::io::Error },
+    Fetch { url: String, e: reqwest::Error },
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+impl<S: AsRef<str>> From<(S, std::io::Error)> for Error {
+    fn from((url, e): (S, std::io::Error)) -> Self {
+        Error::Write {
+            url: url.as_ref().to_string(),
+            e,
+        }
+    }
+}
+
+impl<S: AsRef<str>> From<(S, reqwest::Error)> for Error {
+    fn from((url, e): (S, reqwest::Error)) -> Self {
+        Error::Fetch {
+            url: url.as_ref().to_string(),
+            e,
+        }
+    }
 }
