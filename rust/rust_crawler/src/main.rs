fe2878b5b96034576369194c971b46b9e73a7ead
hezhiwen <hezhiwen.coder@bytedance.com>
2024-01-30 15:54:51 +0800

    url

--- rust/rust_crawler/src/main.rs
+++ rust/rust_crawler/src/main.rs
@@ -1,5 +1,7 @@
+use reqwest::Url;
 use select::document::Document;
 use select::predicate::Name;
+use select::predicate::Predicate;
 use std::collections::HashSet;
 use std::io::Read;
 
@@ -13,10 +15,35 @@ fn main() {
     res.read_to_string(&mut body).unwrap();
     println!("HTML: {}", &body[0..40]);
 
-    let found_urls = Document::from(body.as_str())
-        .find(Name("a"))
-        .filter_map(|n| n.attr("href"))
-        .map(str::to_string)
-        .collect::<HashSet<String>>();
+    let found_urls = get_links_from_html(&body);
     println!("URLs: {:#?}", found_urls)
+}
+
+fn get_links_from_html(html: &str) -> HashSet<String> {
+    Document::from(html)
+        .find(Name("a").or(Name("link")))
+        .filter_map(|n| n.attr("href"))
+        .filter_map(normalize_url)
+        .collect::<HashSet<String>>()
+}
+
+fn normalize_url(url: &str) -> Option<String> {
+    let new_url = Url::parse(url);
+    match new_url {
+        Ok(new_url) => {
+            if new_url.has_host() && new_url.host_str().unwrap() == "ghost.rolisz.ro" {
+                Some(url.to_string())
+            } else {
+                None
+            }
+        }
+        Err(_e) => {
+            // Relative urls are not parsed by Reqwest
+            if url.starts_with('/') {
+                Some(format!("https://rolisz.ro{}", url))
+            } else {
+                None
+            }
+        }
+    }
 }
