148a5b3bfc651fc2fe8015f6becc0d29ea158413
hezhiwen <hezhiwen.coder@bytedance.com>
2024-02-02 15:18:30 +0800

    env

--- rust/PracticalSystemProgramming/examples/ch4_3_3.rs
+++ rust/PracticalSystemProgramming/examples/ch4_3_3.rs
@@ -1,7 +1,9 @@
-use std::env;
+use std::env::{self, current_dir};
 
 fn main() {
+    println!("{:?}", current_dir());
+    println!("{:?}", dotenv::dotenv());
     for (k, v) in env::vars() {
-        println!("{}: {}", k, v);
+        println!("{k}: {v}");
     }
 }
