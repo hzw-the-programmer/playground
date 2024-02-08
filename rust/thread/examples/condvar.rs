2e27b0c34d4a7c011ca1526ee7ce677cb01b9c1a
hezhiwen <hezhiwen.coder@bytedance.com>
2024-02-08 17:51:29 +0800

    thread

---
+++ rust/thread/examples/condvar.rs
@@ -0,0 +1,28 @@
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let pair = Arc::new((Mutex::new(false), Condvar::new()));
+    let pair2 = Arc::clone(&pair);
+
+    // Inside of our lock, spawn a new thread, and then wait for it to start.
+    thread::spawn(move || {
+        let (lock, cvar) = &*pair2;
+        let mut started = lock.lock().unwrap();
+        *started = true;
+        // We notify the condvar that the value has changed.
+        cvar.notify_one();
+        println!("after notify, sleep 3 secs");
+        thread::sleep(Duration::from_secs(3));
+        println!("after sleep");
+    });
+
+    // Wait for the thread to start up.
+    let (lock, cvar) = &*pair;
+    let mut started = lock.lock().unwrap();
+    while !*started {
+        started = cvar.wait(started).unwrap();
+        println!("after wait");
+    }
+}
