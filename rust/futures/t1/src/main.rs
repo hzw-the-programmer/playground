fbe7d300dd94fecc1bf1cc2fb231b02953cdead2
hezhiwen <hezhiwen.coder@bytedance.com>
2024-02-08 21:45:20 +0800

    bulder

---
+++ rust/futures/t1/src/main.rs
@@ -0,0 +1,32 @@
+struct Multiply {
+    num: u16,
+    factor: u16,
+}
+
+impl Multiply {
+    /// Construct a new instance of `Multiply`.
+    pub fn new(num: u16, factor: u16) -> Self {
+        Self { num, factor }
+    }
+
+    /// Set the number to multiply by the factor.
+    pub fn number(mut self, num: u16) -> Self {
+        self.num = num;
+        self
+    }
+
+    /// Set the factor to multiply the number with.
+    pub fn factor(mut self, factor: u16) -> Self {
+        self.factor = factor;
+        self
+    }
+}
+
+fn main() {
+    let a = Multiply::new(2, 3);
+    let b = a.number(3);
+    let c = b.factor(4);
+    // println!("{} {}", a.num, a.factor);
+    // println!("{} {}", b.num, b.factor);
+    println!("{} {}", c.num, c.factor);
+}
