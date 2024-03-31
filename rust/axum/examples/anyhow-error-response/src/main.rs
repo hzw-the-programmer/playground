3c6af76c13e82cca9d7c3e07c42789d8ae4616f0
hezhiwen <hezhiwen.coder@bytedance.com>
2024-03-31 11:26:31 +0800

    add docs

--- rust/axum/examples/anyhow-error-response/src/main.rs
+++ rust/axum/examples/anyhow-error-response/src/main.rs
@@ -1,3 +1,9 @@
+//! Run with
+//! 
+//! ```not_rust
+//! cargo run -p example-anyhow-error-response
+//! ```
+
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
@@ -8,9 +14,11 @@ use axum::{
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:30000")
+    
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:30000")
         .await
         .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -20,21 +28,25 @@ async fn handler() -> Result<(), AppError> {
 }
 
 fn try_thing() -> Result<(), anyhow::Error> {
-    anyhow::bail!("")
+    anyhow::bail!("it failed!")
 }
 
+// Make our own error that wraps `anyhow::Error`.
 struct AppError(anyhow::Error);
 
+// Tell axum how to convert `AppError` into a response.
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("internal server error: {}", self.0),
+            format!("Something went wrong: {}", self.0),
         )
             .into_response()
     }
 }
 
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
 impl<E> From<E> for AppError
 where
     E: Into<anyhow::Error>,
