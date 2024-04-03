96ca61bb083819e2abb68840ae4ca89ae522f1de
hezhiwen <hezhiwen.coder@bytedance.com>
2024-04-03 18:25:47 +0800

    deref

---
+++ rust/workspace/examples/deref/src/main.rs
@@ -0,0 +1,5 @@
+mod test1;
+
+fn main() {
+    test1::test();
+}
---
+++ rust/workspace/examples/deref/src/test1.rs
@@ -0,0 +1,11 @@
+struct Foo<'a> {
+    i: &'a i32,
+}
+
+impl Foo<'_> {
+    fn get(&self) -> &i32 {
+        // let i: i32 = &self.i;
+        &self.i
+    }
+}
+pub fn test() {}
