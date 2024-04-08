410554706747713b43cca233d82f8bc54d5c098c
hezhiwen <hezhiwen.coder@bytedance.com>
2024-04-08 19:30:21 +0800

    size_of

--- rust/workspace/examples/sizeof/src/test1.rs
+++ rust/workspace/examples/sizeof/src/test1.rs
@@ -12,4 +12,8 @@ pub fn test() {
     assert_eq!(mem::size_of::<&dyn Write>(), 16);
     assert_eq!(mem::size_of::<*const dyn Write>(), 16);
     assert_eq!(mem::size_of::<Box<dyn Write>>(), 16);
+
+    assert_eq!(mem::size_of::<&str>(), 16);
+    assert_eq!(mem::size_of::<*const str>(), 16);
+    assert_eq!(mem::size_of::<Box<str>>(), 16);
 }
