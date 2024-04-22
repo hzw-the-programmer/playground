d484f6dcfac4e2b5b156e99345c5115d4623e3e2
hezhiwen <hezhiwen.coder@bytedance.com>
2024-04-22 14:15:35 +0800

    vga

--- rust/blog_os/src/main.rs
+++ rust/blog_os/src/main.rs
@@ -10,7 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"hello";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &c) in HELLO.iter().enumerate() {
+        unsafe {
+            vga_buf.offset(i as isize * 2).write_volatile(c);
+            vga_buf.offset(i as isize * 2 + 1).write_volatile(0x0b);
+        }
+    }
+
     loop {}
 }
