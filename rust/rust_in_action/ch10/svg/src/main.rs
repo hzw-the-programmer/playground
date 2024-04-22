33a48d8d02971444e72fdb4b5d12937ce78ec77c
hezhiwen <hezhiwen.coder@bytedance.com>
2024-04-22 19:06:35 +0800

    svg

---
+++ rust/rust_in_action/ch10/svg/src/main.rs
@@ -0,0 +1,22 @@
+use svg::node::element::path::Data;
+use svg::node::element::Path;
+use svg::Document;
+
+fn main() {
+    let data = Data::new()
+        .move_to((10, 10))
+        .line_by((0, 50))
+        .line_by((50, 0))
+        .line_by((0, -50))
+        .close();
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data);
+
+    let document = Document::new().set("viewBox", (0, 0, 70, 70)).add(path);
+
+    svg::save("img.svg", &document).unwrap();
+}
