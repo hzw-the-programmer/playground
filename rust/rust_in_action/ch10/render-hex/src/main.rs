605012a42e162fad68d4a39ef6f2701ed29ac071
hezhiwen <hezhiwen.coder@bytedance.com>
2024-04-23 11:25:21 +0800

    parse

--- rust/rust_in_action/ch10/render-hex/src/main.rs
+++ rust/rust_in_action/ch10/render-hex/src/main.rs
@@ -1,3 +1,27 @@
+use self::Operation::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+enum Operation {
+    Forward(isize),
+    TurnLeft,
+    TurnRight,
+    Home,
+    Invalid(char),
+}
+
+fn parse(hex: &str) -> Vec<Operation> {
+    let mut ops = vec![];
+    for b in hex.chars() {
+        ops.push(match b {
+            '0' => Home,
+            '1'..='9' => Forward(b as isize - '0' as isize),
+            'a' | 'b' | 'c' => TurnLeft,
+            'd' | 'e' | 'f' => TurnRight,
+            _ => Invalid(b),
+        })
+    }
+    ops
 }
