f96350be387eeeb9c783efed7f45a46658d9760f
hezhiwen <hezhiwen.coder@bytedance.com>
2024-11-11 20:49:13 +0800

    rust

--- rust/workspace/examples/try/src/poll_result_custom_test.rs
+++ rust/workspace/examples/try/src/poll_result_custom_test.rs
@@ -12,102 +12,130 @@ pub fn test() {
 
 fn test1() {
     println!("\ntest1: enter");
-    let r = f1();
-    println!("{:?}", r);
+
+    fn f() -> PollCustom<ResultCustom<Foo, Bar>> {
+        println!("f: enter");
+
+        let r = PollCustom::Pending;
+        let r = r?;
+        println!("f: {:?}", r);
+
+        r.map(ResultCustom::Ok)
+        // try {
+        //     r
+        // }
+    }
+
+    let r = f();
+    println!("test1: {:?}", r);
+
     println!("test1: leave");
 }
 
 fn test2() {
     println!("\ntest2: enter");
-    let r = f2();
-    println!("{:?}", r);
+
+    fn f() -> PollCustom<ResultCustom<Foo, Bar>> {
+        println!("f: enter");
+
+        let r = PollCustom::Ready(ResultCustom::Ok(Foo));
+        let r = r?;
+        println!("f: {:?}", r);
+
+        // r.map(ResultCustom::Ok)
+        try { r }
+    }
+
+    let r = f();
+    println!("test2: {:?}", r);
+
     println!("test2: leave");
 }
 
 fn test3() {
     println!("\ntest3: enter");
-    let r = f3();
-    println!("{:?}", r);
+
+    fn f() -> PollCustom<ResultCustom<Foo, Bar>> {
+        println!("f: enter");
+
+        let r = PollCustom::Ready(ResultCustom::Err(Bar));
+        let r = r?;
+        println!("f: {:?}", r);
+
+        // r.map(ResultCustom::Ok)
+        try { r }
+    }
+
+    let r = f();
+    println!("test3: {:?}", r);
+
     println!("test3: leave");
 }
 
 fn test4() {
     println!("\ntest4: enter");
-    let r = f4();
-    println!("{:?}", r);
+
+    fn f() -> PollCustom<ResultCustom<Foo, Bar>> {
+        println!("f: enter");
+
+        let r = ResultCustom::Err(Bar);
+        let r = r?;
+        println!("f: {:?}", r);
+
+        // r.map(ResultCustom::Ok)
+        try { r }
+    }
+
+    let r = f();
+    println!("test4: {:?}", r);
+
     println!("test4: leave");
 }
 
 fn test5() {
     println!("\ntest5: enter");
-    let r = f5();
-    println!("{:?}", r);
+
+    fn f() -> PollCustom<ResultCustom<Foo, Bar>> {
+        println!("f: enter");
+
+        let r = ResultCustom::Ok(Foo);
+        let r = r?;
+        println!("f: {:?}", r);
+
+        // r.map(ResultCustom::Ok)
+        try { PollCustom::Ready(r) }
+    }
+
+    let r = f();
+    println!("test5: {:?}", r);
+
     println!("test5: leave");
 }
 
 fn test6() {
     println!("\ntest6: enter");
-    let r = f6();
-    println!("{:?}", r);
+
+    fn f() -> PollCustom<ResultCustom<Foo, Bar>> {
+        println!("f: enter");
+
+        use core::ops::{ControlFlow, FromResidual, Try};
+
+        let r = PollCustom::Ready(ResultCustom::Ok(Foo));
+        // let r = PollCustom::Ready(ResultCustom::Err(Bar));
+
+        // let r = r?;
+        let r = match r.branch() {
+            ControlFlow::Continue(c) => c,
+            ControlFlow::Break(b) => return FromResidual::from_residual(b),
+        };
+        println!("f: {:?}", r);
+
+        // try { r }
+        Try::from_output(r)
+    }
+
+    let r = f();
+    println!("test6: {:?}", r);
+
     println!("test6: leave");
-}
-
-fn f1() -> PollCustom<ResultCustom<Foo, Bar>> {
-    let r = PollCustom::Pending;
-    let r = r?;
-    println!("after ?");
-    r.map(ResultCustom::Ok)
-    // try {
-    //     r
-    // }
-}
-
-fn f2() -> PollCustom<ResultCustom<Foo, Bar>> {
-    let r = PollCustom::Ready(ResultCustom::Ok(Foo));
-    let r = r?;
-    println!("after ?");
-    // r.map(ResultCustom::Ok)
-    try { r }
-}
-
-fn f3() -> PollCustom<ResultCustom<Foo, Bar>> {
-    let r = PollCustom::Ready(ResultCustom::Err(Bar));
-    let r = r?;
-    println!("after ?");
-    // r.map(ResultCustom::Ok)
-    try { r }
-}
-
-fn f4() -> PollCustom<ResultCustom<Foo, Bar>> {
-    let r = ResultCustom::Err(Bar);
-    let r = r?;
-    println!("after ?");
-    // r.map(ResultCustom::Ok)
-    try { r }
-}
-
-fn f5() -> PollCustom<ResultCustom<Foo, Bar>> {
-    let r = ResultCustom::Ok(Foo);
-    let r = r?;
-    println!("after ?");
-    // r.map(ResultCustom::Ok)
-    try { PollCustom::Ready(r) }
-}
-
-fn f6() -> PollCustom<ResultCustom<Foo, Bar>> {
-    use core::ops::{ControlFlow, FromResidual, Try};
-
-    let r = PollCustom::Ready(ResultCustom::Ok(Foo));
-    // let r = PollCustom::Ready(ResultCustom::Err(Bar));
-
-    // let r = r?;
-    let r = match r.branch() {
-        ControlFlow::Continue(c) => c,
-        ControlFlow::Break(b) => return FromResidual::from_residual(b),
-    };
-
-    println!("after ?");
-
-    // try { r }
-    Try::from_output(r)
 }
