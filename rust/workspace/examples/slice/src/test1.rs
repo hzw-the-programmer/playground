788e97553648b94cab5d4e83ab5f91c1dea73fb6
hezhiwen <hezhiwen.coder@bytedance.com>
2024-12-16 20:30:12 +0800

    rust

--- rust/workspace/examples/slice/src/test1.rs
+++ rust/workspace/examples/slice/src/test1.rs
@@ -1,8 +1,11 @@
 pub fn test() {
     // get();
-    get_mut();
+    // get_mut();
+    // get_unchecked();
+    get_unchecked_mut();
 }
 
+// src\slice\mod.rs
 fn get() {
     let v = [1, 2, 3];
     assert_eq!(v.get(1), Some(&2));
@@ -14,6 +17,22 @@ fn get() {
 fn get_mut() {
     let mut v = [1, 2, 3];
     if let Some(e) = v.get_mut(1) {
+        *e = 4;
+    }
+    assert_eq!(v, [1, 4, 3]);
+}
+
+fn get_unchecked() {
+    let v = [1, 2, 3];
+    unsafe {
+        assert_eq!(v.get_unchecked(1), &2);
+    }
+}
+
+fn get_unchecked_mut() {
+    let mut v = [1, 2, 3];
+    unsafe {
+        let e = v.get_unchecked_mut(1);
         *e = 4;
     }
     assert_eq!(v, [1, 4, 3]);
