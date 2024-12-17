b013f6e1a52044f89f60eb1e859a04e92183834d
hezhiwen <hezhiwen.coder@bytedance.com>
2024-12-17 12:48:13 +0800

    rust

--- rust/workspace/examples/array/src/test1.rs
+++ rust/workspace/examples/array/src/test1.rs
@@ -2,12 +2,13 @@ pub fn test() {
     test1();
 }
 
+// src\array\iter.rs
 fn test1() {
     let a = [Foo(0), Foo(1), Foo(2), Foo(3)];
     // this method call resolves to `<&[T; N] as IntoIterator>::into_iter` (due to backwards compatibility), but will resolve to <[T; N] as IntoIterator>::into_iter in Rust 2021
     // #[warn(array_into_iter)]
-    // let mut i = a.into_iter();
-    let mut i = IntoIterator::into_iter(a);
+    let mut i = a.into_iter();
+    // let mut i = IntoIterator::into_iter(a);
     i.next();
     println!("finish");
 }
