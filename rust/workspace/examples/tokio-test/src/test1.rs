f30f7c37ae7a878080bbc19dc96c68e85defa4b2
hezhiwen <hezhiwen.coder@bytedance.com>
2024-12-21 09:23:30 +0800

    rust

--- rust/workspace/examples/tokio-test/src/test1.rs
+++ rust/workspace/examples/tokio-test/src/test1.rs
@@ -4,7 +4,8 @@ use tokio::runtime;
 
 pub fn test() {
     // test1();
-    test2();
+    // test2();
+    test3();
 }
 
 fn test1() {
@@ -31,4 +32,29 @@ fn test2() {
         .build()
         .unwrap();
     thread::sleep(time::Duration::from_secs(5));
+}
+
+fn test3() {
+    let rt = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .thread_name_fn(|| {
+            static ATOMIC_ID: atomic::AtomicUsize = atomic::AtomicUsize::new(0);
+            let id = ATOMIC_ID.fetch_add(1, atomic::Ordering::SeqCst);
+            format!("my-pool-{}", id)
+        })
+        .on_thread_start(|| println!("{} start", thread::current().name().unwrap()))
+        .on_thread_stop(|| println!("{} stop", thread::current().name().unwrap()))
+        .thread_keep_alive(time::Duration::from_secs(3))
+        .on_thread_park(|| println!("{} park", thread::current().name().unwrap()))
+        .on_thread_unpark(|| println!("{} unpark", thread::current().name().unwrap()))
+        .worker_threads(2)
+        .build()
+        .unwrap();
+
+    thread::sleep(time::Duration::from_secs(1));
+
+    rt.spawn_blocking(|| println!("{} spawn blocking", thread::current().name().unwrap()));
+    println!("after spawn blocking");
+
+    thread::sleep(time::Duration::from_secs(6));
 }
