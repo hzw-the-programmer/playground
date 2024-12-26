41977081c24b871c8961e34de35503124993f250
hezhiwen <hezhiwen.coder@bytedance.com>
2024-12-26 20:21:21 +0800

    rust

--- rust/workspace/examples/drop/src/test3.rs
+++ rust/workspace/examples/drop/src/test3.rs
@@ -1,5 +1,6 @@
 pub fn test() {
-    test1();
+    // test1();
+    test2();
 }
 
 fn test1() {
@@ -23,5 +24,37 @@ fn test1() {
     }
 
     let _foo = Foo(Bar, Baz);
+    println!("finish");
+}
+
+fn test2() {
+    struct Foo(Bar, Baz);
+    struct Bar;
+    struct Baz;
+    impl Foo {
+        fn into_bar(self) -> Bar {
+            println!("Foo::into_bar");
+            self.0
+        }
+    }
+    // cannot move out of type `test3::test2::Foo`, which implements the `Drop` trait
+    // impl Drop for Foo {
+    //     fn drop(&mut self) {
+    //         println!("Foo drop");
+    //     }
+    // }
+    impl Drop for Bar {
+        fn drop(&mut self) {
+            println!("Bar::drop");
+        }
+    }
+    impl Drop for Baz {
+        fn drop(&mut self) {
+            println!("Baz::drop");
+        }
+    }
+
+    let foo = Foo(Bar, Baz);
+    let _bar = foo.into_bar();
     println!("finish");
 }
