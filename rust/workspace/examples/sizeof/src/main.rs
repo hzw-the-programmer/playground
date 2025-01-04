c025244afd4689c78b9f658e7dcba11bbd17ffb2
hezhiwen <hezhiwen.coder@bytedance.com>
2025-01-04 10:29:41 +0800

    rust

--- rust/workspace/examples/sizeof/src/main.rs
+++ rust/workspace/examples/sizeof/src/main.rs
@@ -4,7 +4,7 @@ mod test2;
 mod test3;
 mod trait_object;
 
-use slice as test;
+use test1 as test;
 
 fn main() {
     test::test();
--- rust/workspace/examples/sizeof/src/test1.rs
+++ rust/workspace/examples/sizeof/src/test1.rs
@@ -3,6 +3,12 @@
 use std::{io::Write, mem};
 
 pub fn test() {
+    // test1();
+    // test2();
+    test3();
+}
+
+fn test1() {
     assert_eq!(mem::size_of::<&u8>(), 8);
     assert_eq!(mem::size_of::<*const u8>(), 8);
     assert_eq!(mem::size_of::<Box<u8>>(), 8);
@@ -18,4 +24,26 @@ pub fn test() {
     assert_eq!(mem::size_of::<&str>(), 16);
     assert_eq!(mem::size_of::<*const str>(), 16);
     assert_eq!(mem::size_of::<Box<str>>(), 16);
+}
+
+fn test2() {
+    // required by the implicit `Sized` requirement on this type parameter in `size_of`
+    // consider relaxing the implicit `Sized` restriction
+    // fn size_of<T>() -> usize {
+    fn size_of<T: ?Sized>() -> usize {
+        mem::size_of::<&T>()
+    }
+
+    assert_eq!(size_of::<u8>(), 8);
+    assert_eq!(size_of::<[u8]>(), 16);
+}
+
+fn test3() {
+    fn size_of<T>() -> usize {
+        mem::size_of::<T>()
+    }
+
+    assert_eq!(size_of::<u8>(), 1);
+    assert_eq!(size_of::<&u8>(), 8);
+    assert_eq!(size_of::<&[u8]>(), 16);
 }
