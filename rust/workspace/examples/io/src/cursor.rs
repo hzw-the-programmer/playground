21f483d0265ae5f93618abbf6d90ead2836da62b
hezhiwen <hezhiwen.coder@bytedance.com>
2025-01-21 20:05:49 +0800

    rust

--- rust/workspace/examples/io/src/cursor.rs
+++ rust/workspace/examples/io/src/cursor.rs
@@ -1,7 +1,8 @@
 use std::io::{Cursor, Read, Write};
 
 pub fn test() {
-    test1();
+    // test1();
+    test2();
 }
 
 fn test1() {
@@ -17,4 +18,14 @@ fn test1() {
     assert_eq!(c.position(), 4);
     assert_eq!(c.remaining_slice(), &[5]);
     assert_eq!(c.get_ref(), &vec![1, 2, 1, 2, 5]);
+}
+
+fn test2() {
+    let mut c = Cursor::new(vec![1, 2, 3]);
+    assert_eq!(c.position(), 0);
+    c.set_position(4);
+    let n = c.write(&[0; 1][0..0]).unwrap();
+    assert_eq!(n, 0);
+    assert_eq!(c.get_ref(), &[1, 2, 3, 0]);
+    assert_eq!(c.position(), 4);
 }
--- rust/workspace/examples/io/src/main.rs
+++ rust/workspace/examples/io/src/main.rs
@@ -4,8 +4,8 @@
 mod buf_read;
 mod cursor;
 
-// use cursor as test;
-use buf_read as test;
+use cursor as test;
+// use buf_read as test;
 
 fn main() {
     test::test();
