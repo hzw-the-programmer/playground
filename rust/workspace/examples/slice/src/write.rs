fbf05cc881186ba7b6aba0b22838e679c9a1dc09
hezhiwen <hezhiwen.coder@bytedance.com>
2025-01-21 12:05:03 +0800

    rust

--- rust/workspace/examples/slice/src/write.rs
+++ rust/workspace/examples/slice/src/write.rs
@@ -2,7 +2,8 @@ use std::io::Write;
 
 pub fn test() {
     // test1();
-    test2();
+    // test2();
+    test3();
 }
 
 fn test1() {
@@ -31,4 +32,14 @@ fn test2() {
     assert_eq!(n, 1);
     assert_eq!(s, []);
     assert_eq!(v, [1, 2, 1]);
+}
+
+fn test3() {
+    let mut v = vec![0; 3];
+    let n = v.write(&[1, 2][..]).unwrap();
+    assert_eq!(n, 2);
+    assert_eq!(v, [0, 0, 0, 1, 2]);
+    let n = v.write(&[3, 4, 5][..]).unwrap();
+    assert_eq!(n, 3);
+    assert_eq!(v, [0, 0, 0, 1, 2, 3, 4, 5]);
 }
