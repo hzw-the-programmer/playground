5652cfa2cd9fbb635e7de5c204e249b7a77cd646
hezhiwen <hezhiwen.coder@bytedance.com>
2025-02-25 16:21:03 +0800

    leetcode

--- rust/workspace/leetcode/solution/src/max_sum_of_unique.rs
+++ rust/workspace/leetcode/solution/src/max_sum_of_unique.rs
@@ -1,24 +1,40 @@
 // 2841. Maximum Sum of Almost Unique Subarray
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 pub fn max_sum_of_unique(nums: &[i32], k: usize, m: usize) -> i32 {
-    let mut set = HashSet::new();
+    let mut map = HashMap::new();
     let mut sum = 0;
     for i in 0..k {
-        set.insert(nums[i]);
+        // map.entry(nums[i])
+        //     .and_modify(|counter| *counter += 1)
+        //     .or_insert(1);
+        *map.entry(nums[i]).or_insert(0) += 1;
         sum += nums[i];
     }
 
     let mut max = 0;
-    if set.len() >= m {
+    if map.len() >= m {
         max = sum;
     }
 
     for i in k..nums.len() {
-        set.remove(&nums[i - k]);
-        set.insert(nums[i]);
+        // if let Some(count) = map.get_mut(&nums[i - k]) {
+        //     *count -= 1;
+        //     if *count == 0 {
+        //         map.remove(&nums[i - k]);
+        //     }
+        // }
+        let count = map.get_mut(&nums[i - k]).unwrap();
+        *count -= 1;
+        if *count == 0 {
+            map.remove(&nums[i - k]);
+        }
+        // map.entry(nums[i])
+        //     .and_modify(|counter| *counter += 1)
+        //     .or_insert(1);
+        *map.entry(nums[i]).or_insert(0) += 1;
         sum += nums[i] - nums[i - k];
-        if set.len() >= m {
+        if map.len() >= m {
             max = max.max(sum);
         }
     }
@@ -35,5 +51,7 @@ mod tests {
         assert_eq!(18, max_sum_of_unique(&[2, 6, 7, 3, 1, 7], 4, 3));
         assert_eq!(23, max_sum_of_unique(&[5, 9, 9, 2, 4, 5, 4], 3, 1));
         assert_eq!(0, max_sum_of_unique(&[1, 2, 1, 2, 1, 2, 1], 3, 3));
+        assert_eq!(26, max_sum_of_unique(&[5, 8, 8, 9, 9], 3, 2));
+        assert_eq!(26, max_sum_of_unique(&[5, 8, 8, 9, 9, 9], 3, 2));
     }
 }
