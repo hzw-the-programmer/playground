6eb0057a77727ac89be9e5a9cc9b860a038db58e
hezhiwen <hezhiwen.coder@bytedance.com>
2025-02-25 09:42:34 +0800

    leetcode

--- rust/workspace/leetcode/solution/src/lib.rs
+++ rust/workspace/leetcode/solution/src/lib.rs
@@ -2,4 +2,5 @@
 
 mod max_average;
 mod max_vowels;
+mod minimum_recolors;
 mod num_of_subarrays;
---
+++ rust/workspace/leetcode/solution/src/minimum_recolors.rs
@@ -0,0 +1,32 @@
+pub fn minimum_recolors(s: &[u8], k: usize) -> usize {
+    let mut count = 0;
+    for i in 0..k {
+        if s[i] == b'W' {
+            count += 1;
+        }
+    }
+
+    let mut min = count;
+    for i in k..s.len() {
+        if s[i] == b'W' {
+            count += 1;
+        }
+        if s[i - k] == b'W' {
+            count -= 1;
+        }
+        min = min.min(count);
+    }
+
+    min
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(3, minimum_recolors(b"WBBWWBBWBW", 7));
+        assert_eq!(0, minimum_recolors(b"WBWBBBW", 2));
+    }
+}
