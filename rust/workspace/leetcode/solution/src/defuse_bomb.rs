9c2d6d9418ccfbfc8dd75582bb8621a09223bbec
hezhiwen <hezhiwen.coder@bytedance.com>
2025-02-27 11:07:26 +0800

    refactor

--- rust/workspace/leetcode/solution/src/defuse_bomb.rs
+++ rust/workspace/leetcode/solution/src/defuse_bomb.rs
@@ -1,24 +1,22 @@
 // 1652. Defuse the Bomb
 pub fn decrypt(code: &[i32], k: i32) -> Vec<i32> {
-    let mut res = vec![0; code.len()];
+    let n = code.len();
+    let mut res = vec![0; n];
 
     if k == 0 {
         return res;
     }
 
     if k > 0 {
-        let k = k as usize;
-        for i in 0..code.len() {
-            for j in i + 1..i + 1 + k {
-                res[i] += code[j % code.len()];
+        for i in 0..n {
+            for j in 1..=k as usize {
+                res[i] += code[(i + j) % n];
             }
         }
     } else {
-        let k = -k as usize;
-        for i in 0..code.len() {
-            let s = i + code.len() - k;
-            for j in s..s + k {
-                res[i] += code[j % code.len()];
+        for i in 0..n {
+            for j in 1..=-k as usize {
+                res[i] += code[(i + n - j) % n];
             }
         }
     }
