21b5a62bd2e020a442a5ab774387e0e74eb93428
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-06 15:12:39 +0800

    contains duplicate iii

--- rust/workspace/leetcode/solution/src/sliding_window/contains_duplicate_iii.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/contains_duplicate_iii.rs
@@ -1,4 +1,5 @@
-pub fn contains_nearby_almost_duplicate(nums: &[i32], index_diff: i32, value_diff: i32) -> bool {
+// 220. Contains Duplicate III
+pub fn contains_nearby_almost_duplicate_1(nums: &[i32], index_diff: i32, value_diff: i32) -> bool {
     let n = nums.len();
     let k = index_diff as usize + 1;
     for i in 0..k - 1 {
@@ -18,16 +19,50 @@ pub fn contains_nearby_almost_duplicate(nums: &[i32], index_diff: i32, value_dif
     false
 }
 
+use std::collections::BTreeSet;
+
+pub fn contains_nearby_almost_duplicate_2(nums: &[i32], index_diff: i32, value_diff: i32) -> bool {
+    let n = nums.len();
+    let k = index_diff as usize;
+    let mut set = BTreeSet::new();
+    for i in 0..n {
+        if let Some(v) = set.range(nums[i] - value_diff..).next() {
+            if *v <= nums[i] + value_diff {
+                return true;
+            }
+        }
+
+        set.insert(nums[i]);
+
+        if i >= k {
+            set.remove(&nums[i - k]);
+        }
+    }
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test() {
-        assert_eq!(true, contains_nearby_almost_duplicate(&[1, 2, 3, 1], 3, 0));
+        assert_eq!(
+            true,
+            contains_nearby_almost_duplicate_1(&[1, 2, 3, 1], 3, 0)
+        );
         assert_eq!(
             false,
-            contains_nearby_almost_duplicate(&[1, 5, 9, 1, 5, 9], 2, 3)
+            contains_nearby_almost_duplicate_1(&[1, 5, 9, 1, 5, 9], 2, 3)
+        );
+
+        assert_eq!(
+            true,
+            contains_nearby_almost_duplicate_2(&[1, 2, 3, 1], 3, 0)
+        );
+        assert_eq!(
+            false,
+            contains_nearby_almost_duplicate_2(&[1, 5, 9, 1, 5, 9], 2, 3)
         );
     }
 }
