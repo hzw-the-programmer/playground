fcaf46d30a2437633505c7314c71e77f8e4f3185
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-07 12:33:57 +0800

    count complete substrings

--- rust/workspace/leetcode/solution/src/sliding_window/count_complete_substrings.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/count_complete_substrings.rs
@@ -4,37 +4,33 @@ pub fn count_complete_substrings(word: String, k: i32) -> i32 {
     let k = k as usize;
     let n = word.len();
     let mut res = 0;
-    let (mut start, mut i) = (0, 1);
+    let mut i = 0;
     while i < n {
-        if (word[i] as i32 - word[i - 1] as i32).abs() > 2 {
-            res += find(&word[start..i], k);
-            start = i;
-        }
+        let start = i;
         i += 1;
+        while i < n && (word[i] as i32 - word[i - 1] as i32).abs() <= 2 {
+            i += 1;
+        }
+        res += find(&word[start..i], k);
     }
-    res += find(&word[start..i], k);
     res
 }
 
 fn find(s: &[u8], k: usize) -> i32 {
     let n = s.len();
     let mut res = 0;
-    for i in (1..26usize).rev() {
+    for i in 1..27usize {
         if i * k > n {
-            continue;
+            break;
         }
         let mut count = [0; 26];
-        for j in 0..i * k {
+        for j in 0..n {
             count[(s[j] - b'a') as usize] += 1;
-        }
-        if count.iter().filter(|&&x| x == k).count() == i {
-            res += 1;
-        }
-        for j in i * k..n {
-            count[(s[j] - b'a') as usize] += 1;
-            count[(s[j - i * k] - b'a') as usize] -= 1;
-            if count.iter().filter(|&&x| x == k).count() == i {
-                res += 1;
+            if j >= i * k - 1 {
+                if count.iter().filter(|&&x| x == k).count() == i {
+                    res += 1;
+                }
+                count[(s[j + 1 - i * k] - b'a') as usize] -= 1;
             }
         }
     }
