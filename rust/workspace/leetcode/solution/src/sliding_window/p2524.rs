24fe2b6728fe05a4ad0a890a4b32201e7d81ba2d
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-08 21:10:35 +0800

    optimize p2524

--- rust/workspace/leetcode/solution/src/sliding_window/p2524.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/p2524.rs
@@ -1,25 +1,60 @@
+// 2524. Maximum Frequency Score of a Subarray
+
 use std::collections::HashMap;
 
-// 2524. Maximum Frequency Score of a Subarray
 pub fn max_frequency_score(nums: Vec<i32>, k: usize) -> i32 {
     let n = nums.len();
-    let d = 10_i32.pow(9) + 7;
+    let modulus = 10_i32.pow(9) + 7;
+    
     let mut map = HashMap::new();
     for i in 0..k {
         *map.entry(nums[i]).or_insert(0) += 1;
     }
 
-    let mut max = map.iter().map(|(k, v)| k.pow(*v)).sum::<i32>() % d;
+    let mut cur = 0;
+    for (&k, &v) in map.iter() {
+        cur = (cur + quick_power(k, v, modulus)) % modulus;
+    }
+
+    let mut max = cur;
     for i in k..n {
-        *map.entry(nums[i]).or_insert(0) += 1;
-        let v = map.get_mut(&nums[i-k]).unwrap();
-        *v -= 1;
-        if *v == 0 {
-            map.remove(&nums[i-k]);
-        }
-        max = max.max(map.iter().map(|(k, v)| k.pow(*v)).sum::<i32>() % d);
+        let added = nums[i];
+        let removed = nums[i-k];
+        if added != removed {
+            if let Some(&v) = map.get(&added) {
+                cur += (added - 1) * quick_power(added, v, modulus);
+            } else {
+                cur += added;
+            }
+
+            let v = map.get(&removed).unwrap();
+            if *v > 1 {
+                cur -= (removed - 1) * quick_power(removed, *v - 1, modulus);
+            } else {
+                cur -= removed;
+            }
+
+            cur = (cur + modulus) % modulus;
+
+            *map.entry(added).or_insert(0) += 1;
+            *map.entry(removed).or_insert(0) -= 1;
+
+            max = max.max(cur);
+        } 
     }
     max
+}
+
+pub fn quick_power(mut base: i32, mut exponent: i32, modulus: i32) -> i32 {
+    let mut result = 1;
+    while exponent > 0 {
+        if exponent & 1 == 1 {
+            result = result * base % modulus;
+        }
+        base = base * base % modulus;
+        exponent >>= 1;
+    }
+    result
 }
 
 #[cfg(test)]
