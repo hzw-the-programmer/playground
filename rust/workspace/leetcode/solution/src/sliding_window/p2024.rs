ad90823e7ed9a114d3f1097456b25deba93b4b93
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-13 15:14:57 +0800

    optimize p2024

--- rust/workspace/leetcode/solution/src/sliding_window/p2024.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/p2024.rs
@@ -1,26 +1,22 @@
 // 2024. Maximize the Confusion of an Exam
 
+/*
+  T 84 0x54 1001 1000
+  F 70 0x46 0100 0110
+  分别统计 T,F 的次数，保证 T,F 的次数不能同时 >k
+  如果一个 >k，另一个 <=k，那么操作 <= k 的字符
+*/
 pub fn max_consecutive_answers(answer_key: String, k: i32) -> i32 {
-    let keys = answer_key.as_bytes();
-    helper(keys, b'F', k).max(helper(keys, b'T', k))
-}
-
-fn helper(s: &[u8], b: u8, k: i32) -> i32 {
+    let s = answer_key.as_bytes();
     let mut res = 0;
+    let mut cnt = [0; 2];
     let mut l = 0;
-    let mut count = 0;
-    for r in 0..s.len() {
-        if s[r] == b {
-            count += 1;
-        }
-
-        while count > k {
-            if s[l] == b {
-                count -= 1;
-            }
+    for (r, &b) in s.iter().enumerate() {
+        cnt[(b >> 1 & 1) as usize] += 1;
+        while cnt[0] > k && cnt[1] > k {
+            cnt[(s[l] >> 1 & 1) as usize] -= 1;
             l += 1;
         }
-
         res = res.max(r - l + 1);
     }
     res as _
