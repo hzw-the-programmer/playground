ca0c2729ed260234b365bee9889e23480f8f3cb5
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-15 17:04:08 +0800

    actix

--- rust/workspace/programming_rust/ch02/web-actix/src/main.rs
+++ rust/workspace/programming_rust/ch02/web-actix/src/main.rs
@@ -1,8 +1,19 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    m: u64,
+    n: u64,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
     println!("Serving on http://localhost:3000 ...");
     server.bind("127.0.0.1:3000")?.run().await
 }
@@ -12,10 +23,25 @@ async fn get_index() -> HttpResponse {
         r#"
         <title>GCD Caculator</title>
         <form action="/gcd" method="post">
-            <input type="text" name="n"/>
             <input type="text" name="m"/>
+            <input type="text" name="n"/>
             <button type="submit">Compute GCD</button>
         </form>
     "#,
     )
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.m == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+        form.m,
+        form.n,
+        math::gcd(form.m, form.n)
+    );
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
