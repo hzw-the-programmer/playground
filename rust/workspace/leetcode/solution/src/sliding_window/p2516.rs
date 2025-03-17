d6633aae222a5d9b69d28a059efddcea7b555f68
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-17 12:30:04 +0800

    optimize p2516

--- rust/workspace/leetcode/solution/src/sliding_window/p2516.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/p2516.rs
@@ -4,28 +4,23 @@ pub fn take_characters(s: String, k: i32) -> i32 {
     let s = s.as_bytes();
     let n = s.len();
 
-    let mut cnt1 = [0; 3];
+    let mut cnt = [0; 3];
     for &b in s.iter() {
-        cnt1[(b - b'a') as usize] += 1;
+        cnt[(b - b'a') as usize] += 1;
     }
-    if cnt1[0] < k || cnt1[1] < k || cnt1[2] < k {
+    if cnt[0] < k || cnt[1] < k || cnt[2] < k {
         return -1;
-    } else if cnt1[0] == k && cnt1[1] == k && cnt1[2] == k {
+    } else if cnt[0] == k && cnt[1] == k && cnt[2] == k {
         return n as _;
     }
 
-    for c in &mut cnt1 {
-        *c -= k;
-    }
-
     let mut res = 0;
-    let mut cnt2 = [0; 3];
     let mut l = 0;
     for r in 0..n {
         let i = (s[r] - b'a') as usize;
-        cnt2[i] += 1;
-        while cnt2[i] > cnt1[i] {
-            cnt2[(s[l] - b'a') as usize] -= 1;
+        cnt[i] -= 1;
+        while cnt[i] < k {
+            cnt[(s[l] - b'a') as usize] += 1;
             l += 1;
         }
         if r >= l {
