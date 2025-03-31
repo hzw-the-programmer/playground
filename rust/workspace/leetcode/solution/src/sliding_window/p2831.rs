ceb23fc526ca548c786fed24cb83fe1a91ab58a2
hezhiwen <hezhiwen.coder@bytedance.com>
2025-03-31 11:55:13 +0800

    p2831

--- rust/workspace/leetcode/solution/src/sliding_window/p2831.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/p2831.rs
@@ -1,29 +1,23 @@
 // 2831. Find the Longest Equal Subarray
 
-use std::collections::HashMap;
-
 pub fn longest_equal_subarray(nums: Vec<i32>, k: i32) -> i32 {
-    let mut map: HashMap<i32, (usize, usize, usize, usize)> = HashMap::new();
+    let mut map = std::collections::HashMap::new();
     for (i, &n) in nums.iter().enumerate() {
-        if let Some(e) = map.get_mut(&n) {
-            *e = (
-                i,
-                e.1 + i - e.0 - 1,
-                e.2 + 1,
-                if i == e.0 + 1 { e.3 + 1 } else { e.3 },
-            );
-        } else {
-            map.insert(n, (i, 0, 1, 1));
-        }
+        let e = map.entry(n).or_insert(vec![]);
+        e.push(i);
     }
+
     let mut res = 0;
-    for v in map.values() {
-        if v.1 <= k as usize {
-            res = res.max(v.2);
-        } else {
-            res = res.max(v.3);
+    for pos_list in map.values() {
+        let mut left = 0;
+        for right in 0..pos_list.len() {
+            while pos_list[right] - pos_list[left] + 1 - (right - left + 1) > k as usize {
+                left += 1;
+            }
+            res = res.max(right - left + 1);
         }
     }
+
     res as _
 }
 
@@ -36,5 +30,6 @@ mod tests {
         assert_eq!(longest_equal_subarray(vec![1, 3, 2, 3, 1, 3], 3), 3);
         assert_eq!(longest_equal_subarray(vec![1, 1, 2, 2, 1, 1], 2), 4);
         assert_eq!(longest_equal_subarray(vec![3, 1, 5, 3, 1, 1], 0), 2);
+        assert_eq!(longest_equal_subarray(vec![4, 2, 7, 2, 1, 7, 2], 1), 2);
     }
 }
