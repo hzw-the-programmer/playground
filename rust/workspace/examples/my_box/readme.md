18fd4c017781a618a35f8d1e7f671625509534f1
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-06 21:57:39 +0800

    refactor my box

--- rust/workspace/examples/my_box/readme.md
+++ rust/workspace/examples/my_box/readme.md
@@ -1,2 +1,3 @@
 cargo fmt -p my_box
 cargo run -p my_box
+cargo test -p my_box
--- rust/workspace/examples/my_box/src/main.rs
+++ rust/workspace/examples/my_box/src/main.rs
@@ -1,6 +1,6 @@
-mod my_box;
+mod my_box_raw_ptr;
 mod my_global_alloc;
 
 fn main() {
-    my_box::main::test1();
+    my_box_raw_ptr::main::test1();
 }
