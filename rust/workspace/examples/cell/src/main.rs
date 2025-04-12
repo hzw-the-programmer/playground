bcda439e7543db94fb5138ae4df27427cf2a9f51
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-12 17:43:50 +0800

    cell

--- rust/workspace/examples/cell/src/main.rs
+++ rust/workspace/examples/cell/src/main.rs
@@ -1,96 +1,110 @@
 fn main() {
     test1();
+    test2();
+    test3();
 }
 
 fn test1() {
-    // let i = 32;
-    // let r = &i as *const i32 as *mut i32;
-    // let i: i32 = r;
-    // let r = unsafe { &mut *r };
-    // *r = 33;
-    // std::mem::replace(unsafe { &mut *r }, 33);
-    // unsafe { std::ptr::write(r, 33); }
-    // println!("{i}");
+    let i = 32;
+    println!("{i}");
+    let r = &i;
+    let ptr = r as *const i32 as *mut i32;
 
     {
-        let i = 32;
-        println!("{i}");
-        let r = &i;
-        let r = &mut unsafe { *(r as *const i32 as *mut i32) };
-        // let r = unsafe { &mut *(r as *const i32 as *mut i32) };
+        // assigning to `&T` is undefined behavior, consider using an `UnsafeCell`
+        // unsafe { *ptr = 33 };
+
+        // unsafe { *get(r) = 33 };
+
+        // unsafe { ptr.write(33) };
+    }
+
+    {
+        // casting `&T` to `&mut T` is undefined behavior, even if the reference is unused, consider instead using an `UnsafeCell`
+        // let r = unsafe { &mut *ptr };
+
+        // let r = unsafe { &mut *get(r) };
+
+        // let r = &mut unsafe { *ptr };
+        let r = &mut unsafe { *get(r) };
+
         *r = 33;
-        println!("{i}");
+    }
+
+    println!("{i}");
+
+    fn get(r: &i32) -> *mut i32 {
+        r as *const i32 as *mut i32
+    }
+}
+
+fn test2() {
+    let i = UnsafeCellCustom { v: 32 };
+    println!("{i:?}");
+    let r = &i;
+    let ptr = r as *const UnsafeCellCustom<i32> as *mut UnsafeCellCustom<i32>;
+
+    {
+        // assigning to `&T` is undefined behavior, consider using an `UnsafeCell`
+        // unsafe { *ptr = UnsafeCellCustom { v: 33 } };
+
+        // unsafe { *get(r) = UnsafeCellCustom { v: 33 } };
+
+        // unsafe { ptr.write(UnsafeCellCustom { v: 33 }) };
     }
 
     {
-        let i = 32;
-        println!("{i}");
-        unsafe {
-            let i = &i;
-            let i = i as *const i32 as *mut i32;
-            // *i = 33;
-            // let _ = std::mem::replace(unsafe { &mut *i }, 33);
-            i.write(33);
-        }
-        println!("{i}");
+        // casting `&T` to `&mut T` is undefined behavior, even if the reference is unused, consider instead using an `UnsafeCell`
+        // let r = unsafe { &mut *ptr };
+
+        let r = unsafe { &mut *get(r) };
+
+        // cannot move out of `*ptr` which is behind a raw pointer
+        // let r = &mut unsafe { *ptr };
+        // cannot move out of a raw pointer
+        // let r = &mut unsafe { *get(r) };
+
+        *r = UnsafeCellCustom { v: 33 };
+    }
+
+    println!("{i:?}");
+
+    fn get<T>(r: &UnsafeCellCustom<T>) -> *mut UnsafeCellCustom<T> {
+        r as *const UnsafeCellCustom<T> as *mut UnsafeCellCustom<T>
+    }
+}
+
+fn test3() {
+    let i = UnsafeCellCustom { v: 32 };
+    println!("{i:?}");
+    let r = &i;
+    let ptr = r as *const UnsafeCellCustom<i32> as *const i32 as *mut i32;
+
+    {
+        // assigning to `&T` is undefined behavior, consider using an `UnsafeCell`
+        // unsafe { *ptr = 33 };
+
+        // unsafe { *i.get() = 33 };
+
+        // unsafe { ptr.write(33) };
     }
 
     {
-        let i = 32;
-        println!("{i}");
-        unsafe {
-            (&i as *const i32 as *mut i32).write(33);
-        }
-        println!("{i}");
+        // casting `&T` to `&mut T` is undefined behavior, even if the reference is unused, consider instead using an `UnsafeCell`
+        // let r = unsafe { &mut *ptr };
+
+        // let r = unsafe { &mut *i.get() };
+
+        // let r = &mut unsafe { *ptr };
+        let r = &mut unsafe { *i.get() };
+
+        *r = 33;
     }
 
-    {
-        let i = UnsafeCellCustom { v: 32 };
-        println!("{i:?}");
+    println!("{i:?}");
 
-        unsafe {
-            let ptr = &i as *const UnsafeCellCustom<i32> as *const i32 as *mut i32;
-            // *ptr = 33;
-            // let _ = std::mem::replace(unsafe { &mut *ptr }, 33);
-            // let _ = std::mem::replace(unsafe { &mut *i.get() }, 33);
-            let _ = std::mem::replace(unsafe { &mut *get(&i) }, 33);
-            // let _ = std::mem::replace(&mut unsafe { *ptr }, 33);
-            // let _ = std::mem::replace(&mut unsafe { *i.get() }, 33);
-            // ptr.write(33);
-        }
-        println!("{i:?}");
-    }
-
-    // {
-    //     let i = UnsafeCellCustom { v: 32 };
-    //     println!("{i:?}");
-
-    //     unsafe {
-    //         let i = &i as *const UnsafeCellCustom<i32> as *mut UnsafeCellCustom<i32>;
-    //         // let i: i32 = i;
-    //         // *i = UnsafeCellCustom { v: 33 };
-    //         // let _ = std::mem::replace(unsafe { &mut *i }, UnsafeCellCustom { v: 33 });
-    //         // unsafe {
-    //         //     let _ = std::mem::replace(&mut *i, UnsafeCellCustom { v: 33 });
-    //         // }
-    //         // let _ = std::mem::replace(&mut unsafe { *i }, UnsafeCellCustom { v: 33 });
-    //         i.write(UnsafeCellCustom { v: 33 });
-    //     }
-    //     println!("{i:?}");
-    // }
-
-    // {
-    //     let i = UnsafeCellCustom { v: 32 };
-    //     println!("{i:?}");
-
-    //     unsafe {
-    //         i.get().write(33);
-    //     }
-    //     println!("{i:?}");
-
-    //     i.replace(34);
-    //     println!("{i:?}");
-    // }
+    i.replace(34);
+    println!("{i:?}");
 }
 
 #[derive(Debug)]
@@ -105,9 +119,7 @@ impl<T> UnsafeCellCustom<T> {
 
     fn replace(&self, n: T) -> T {
         std::mem::replace(unsafe { &mut *self.get() }, n)
+        // cannot move out of a raw pointer
+        // std::mem::replace(&mut unsafe { *self.get() }, n)
     }
-}
-
-fn get<T>(r: &UnsafeCellCustom<T>) -> *mut T {
-    r as *const UnsafeCellCustom<T> as *const T as *mut T
 }
