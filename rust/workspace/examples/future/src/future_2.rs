3535628cd7ce9e8623bab467a3e20a6bb13b36e3
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-13 08:15:38 +0800

    future

---
+++ rust/workspace/examples/future/src/future_2.rs
@@ -0,0 +1,45 @@
+use futures_util::FutureExt;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll, Waker};
+
+pub fn test() {
+    test1();
+}
+
+fn test1() {
+    let mut cx = Context::from_waker(Waker::noop());
+    let mut foo = Foo::new(3);
+    let r = foo.poll_unpin(&mut cx);
+    println!("{r:?}");
+    let r = foo.poll_unpin(&mut cx);
+    println!("{r:?}");
+    let r = foo.poll_unpin(&mut cx);
+    println!("{r:?}");
+    let r = foo.poll_unpin(&mut cx);
+    println!("{r:?}");
+}
+
+struct Foo {
+    count: usize,
+}
+
+impl Foo {
+    fn new(count: usize) -> Self {
+        Foo { count }
+    }
+}
+
+impl Future for Foo {
+    type Output = i32;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        println!("Foo::poll: count={}", self.count);
+        if self.count > 0 {
+            self.count -= 1;
+            Poll::Pending
+        } else {
+            Poll::Ready(23)
+        }
+    }
+}
--- rust/workspace/examples/future/src/main.rs
+++ rust/workspace/examples/future/src/main.rs
@@ -1,8 +1,9 @@
-#![feature(noop_waker)]
+// #![feature(noop_waker)]
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
 mod drop;
+mod future_2;
 mod future_async_await;
 mod future_state_machine;
 mod futures;
@@ -13,8 +14,9 @@ mod streams;
 // use future_state_machine as test;
 // use sink as test;
 // use futures as test;
-use streams as test;
+// use streams as test;
 // use drop as test;
+use future_2 as test;
 
 fn main() {
     test::test();
