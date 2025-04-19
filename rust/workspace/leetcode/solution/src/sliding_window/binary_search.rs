188810eeeec45c0ff5deef569fba97caa5c09c57
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-19 21:49:26 +0800

    binary search

--- rust/workspace/leetcode/solution/src/sliding_window/binary_search.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/binary_search.rs
@@ -1,7 +1,7 @@
 pub fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
     let mut left = 0;
-    let mut right = arr.len() - 1;
-    while left <= right {
+    let mut right = arr.len();
+    while left < right {
         // 避免整数溢出
         let mid = left + (right - left) / 2;
         if arr[mid] == target {
@@ -9,11 +9,7 @@ pub fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
         } else if arr[mid] < target {
             left = mid + 1;
         } else {
-            // 避免索引越界
-            if mid == 0 {
-                break;
-            }
-            right = mid - 1;
+            right = mid;
         }
     }
     None
@@ -24,8 +20,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn binary_search_test() {
+        assert_eq!(None, binary_search(&[], 1));
         assert_eq!(None, binary_search(&[2], 1));
+        assert_eq!(None, binary_search(&[2, 4], 3));
+        assert_eq!(None, binary_search(&[2, 4, 6], 3));
+        assert_eq!(None, binary_search(&[2, 4, 6, 8], 7));
+        assert_eq!(None, binary_search(&[2, 4, 6, 8, 10], 7));
+
         assert_eq!(Some(4), binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9], 5));
         assert_eq!(Some(4), binary_search(&[1, 2, 3, 4, 5, 6, 7, 8], 5));
     }
