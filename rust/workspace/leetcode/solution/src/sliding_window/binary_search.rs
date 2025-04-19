4257e915928914199d9e1aed842d56f6d4e28487
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-19 22:04:56 +0800

    lower bound

--- rust/workspace/leetcode/solution/src/sliding_window/binary_search.rs
+++ rust/workspace/leetcode/solution/src/sliding_window/binary_search.rs
@@ -15,6 +15,22 @@ pub fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
     None
 }
 
+pub fn lower_bound(arr: &[i32], target: i32) -> usize {
+    let mut left = 0;
+    let mut right = arr.len();
+
+    while left < right {
+        let mid = left + (right - left) / 2;
+        if arr[mid] < target {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    left
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +46,16 @@ mod tests {
 
         assert_eq!(Some(4), binary_search(&[1, 2, 3, 4, 5, 6, 7, 8, 9], 5));
         assert_eq!(Some(4), binary_search(&[1, 2, 3, 4, 5, 6, 7, 8], 5));
+    }
+
+    #[test]
+    fn lower_bound_test() {
+        assert_eq!(0, lower_bound(&[], 1));
+        assert_eq!(0, lower_bound(&[2], 1));
+        assert_eq!(1, lower_bound(&[2, 4], 3));
+        assert_eq!(2, lower_bound(&[2, 4, 6], 5));
+        assert_eq!(2, lower_bound(&[2, 4, 6, 8], 5));
+        assert_eq!(5, lower_bound(&[2, 4, 6, 8, 10], 11));
+        assert_eq!(3, lower_bound(&[2, 4, 6, 8, 10], 8));
     }
 }
