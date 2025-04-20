024aacc93b5783b2a129de9ee13a04b3365768c6
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-20 11:18:57 +0800

    binary search

--- rust/workspace/examples/array/src/binary_search.rs
+++ rust/workspace/examples/array/src/binary_search.rs
@@ -1,3 +1,13 @@
+pub fn test() {
+    test1();
+}
+
+fn test1() {
+    let s = [0, 1, 1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+
+    println!("{:?}", s.binary_search(&1));
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- rust/workspace/examples/array/src/main.rs
+++ rust/workspace/examples/array/src/main.rs
@@ -1,7 +1,8 @@
 mod binary_search;
 mod test1;
 
-use test1 as test;
+// use test1 as test;
+use binary_search as test;
 
 fn main() {
     test::test();
