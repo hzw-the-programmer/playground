1a7c7511ed863c0f58cce9e382d1b37e55fcaab2
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-20 21:44:57 +0800

    binary search tree

---
+++ rust/workspace/data_structures/src/binary_search_tree.rs
@@ -0,0 +1,69 @@
+struct TreeNode<T> {
+    value: T,
+    left: Option<Box<TreeNode<T>>>,
+    right: Option<Box<TreeNode<T>>>,
+}
+
+struct BinarySearchTree<T> {
+    node: Option<Box<TreeNode<T>>>,
+}
+
+impl<T: Ord + Clone> BinarySearchTree<T> {
+    fn new() -> Self {
+        BinarySearchTree { node: None }
+    }
+
+    fn insert(&mut self, value: T) {
+        let mut current = &mut self.node;
+        loop {
+            match current {
+                None => {
+                    *current = Some(Box::new(TreeNode {
+                        value,
+                        left: None,
+                        right: None,
+                    }));
+                    break;
+                }
+                Some(node) => {
+                    if value < node.value {
+                        current = &mut node.left;
+                    } else {
+                        current = &mut node.right;
+                    }
+                }
+            }
+        }
+    }
+
+    fn to_vec_recursive(&self, node: &Option<Box<TreeNode<T>>>, v: &mut Vec<T>) {
+        if let Some(node) = node.as_ref() {
+            self.to_vec_recursive(&node.left, v);
+            v.push(node.value.clone());
+            self.to_vec_recursive(&node.right, v);
+        }
+    }
+
+    fn to_vec(&self) -> Vec<T> {
+        let mut v = vec![];
+        self.to_vec_recursive(&self.node, &mut v);
+        v
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn binary_search_tree() {
+        let mut bst = BinarySearchTree::new();
+        bst.insert(5);
+        bst.insert(4);
+        bst.insert(3);
+        bst.insert(2);
+        bst.insert(1);
+        bst.insert(0);
+        assert_eq!(vec![0, 1, 2, 3, 4, 5], bst.to_vec());
+    }
+}
