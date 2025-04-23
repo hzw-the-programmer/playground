3e6f24ffca429d4ce9314119cded5280b3d0a711
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-23 21:35:45 +0800

    drop

--- rust/workspace/examples/drop/src/box1.rs
+++ rust/workspace/examples/drop/src/box1.rs
@@ -1,6 +1,7 @@
 pub fn test() {
     // test1();
-    test2();
+    // test2();
+    test3();
 }
 
 fn test1() {
@@ -72,4 +73,40 @@ fn test2() {
     }
 
     println!("leave test2");
+}
+
+fn test3() {
+    struct Bar(i32);
+    impl Drop for Bar {
+        fn drop(&mut self) {
+            println!("Bar {} drop", self.0);
+        }
+    }
+    struct Foo {
+        i: i32,
+        bar1: Option<Bar>,
+        bar2: Bar,
+    }
+    impl Drop for Foo {
+        fn drop(&mut self) {
+            println!("Foo {} drop", self.i);
+        }
+    }
+
+    let _bar;
+    {
+        let mut foo = Box::new(Foo {
+            i: 1,
+            bar1: Some(Bar(1)),
+            bar2: { Bar(2) },
+        });
+        // let mut foo = Foo {
+        //     i: 1,
+        //     bar1: Some(Bar(1)),
+        //     bar2: Bar(2),
+        // };
+        _bar = foo.bar1.take();
+    }
+
+    println!("leave test3");
 }
