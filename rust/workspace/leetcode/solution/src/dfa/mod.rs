255471451d8265ba5a9223f6b4242e48cded6b22
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-26 17:57:00 +0800

    move p8 p65 to dfa

--- rust/workspace/leetcode/solution/src/dfa/mod.rs
+++ rust/workspace/leetcode/solution/src/dfa/mod.rs
@@ -1,3 +1,7 @@
+// 8. String to Integer (atoi)
+// 65. Valid Number
 // 393. UTF-8 Validation
 
 mod p393;
+mod p65;
+mod p8;
--- rust/workspace/leetcode/solution/src/lib.rs
+++ rust/workspace/leetcode/solution/src/lib.rs
@@ -1,11 +1,6 @@
 #![allow(dead_code)]
 
-// 8. String to Integer (atoi)
-// 65. Valid Number
-
 struct Solution;
 
 mod dfa;
-mod p65;
-mod p8;
 mod sliding_window;
