2705692e4c0db2dd92d4a63629328bfe9398637e
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-30 19:35:46 +0800

    s2461

--- rust/workspace/leetcode/solution/src/s2461.rs
+++ rust/workspace/leetcode/solution/src/s2461.rs
@@ -1,6 +1,42 @@
 // 2461. Maximum Sum of Distinct Subarrays With Length K
 
+// 1 <= nums[i] <= 100000
 pub fn maximum_subarray_sum(nums: Vec<i32>, k: i32) -> i64 {
+    let k = k as usize;
+
+    let mut sum = 0;
+    let mut map = vec![0; 100001];
+    let mut count = 0;
+    for i in 0..k {
+        sum += nums[i] as i64;
+        map[nums[i] as usize] += 1;
+        if map[nums[i] as usize] == 1 {
+            count += 1;
+        }
+    }
+    let mut ans = if count == k { sum } else { 0 };
+
+    for i in k..nums.len() {
+        sum += (nums[i] - nums[i - k]) as i64;
+        map[nums[i] as usize] += 1;
+        if map[nums[i] as usize] == 1 {
+            count += 1;
+        }
+
+        map[nums[i - k] as usize] -= 1;
+        if map[nums[i - k] as usize] == 0 {
+            count -= 1;
+        }
+
+        if count == k {
+            ans = ans.max(sum);
+        }
+    }
+
+    ans
+}
+
+pub fn maximum_subarray_sum_v2(nums: Vec<i32>, k: i32) -> i64 {
     let k = k as usize;
 
     let mut sum = 0;
@@ -32,9 +68,18 @@ pub fn maximum_subarray_sum(nums: Vec<i32>, k: i32) -> i64 {
 mod tests {
     use super::*;
 
+    fn test(f: fn(Vec<i32>, i32) -> i64) {
+        assert_eq!(f(vec![1, 5, 4, 2, 9, 9, 9], 3), 15);
+        assert_eq!(f(vec![4, 4, 4], 3), 0);
+    }
+
     #[test]
     fn test_maximum_subarray_sum() {
-        assert_eq!(maximum_subarray_sum(vec![1, 5, 4, 2, 9, 9, 9], 3), 15);
-        assert_eq!(maximum_subarray_sum(vec![4, 4, 4], 3), 0);
+        test(maximum_subarray_sum);
+    }
+
+    #[test]
+    fn test_maximum_subarray_sum_v2() {
+        test(maximum_subarray_sum_v2);
     }
 }
