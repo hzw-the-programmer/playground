6df7a510a330a8935cf9aced293dc299c79b08ed
hezhiwen <hezhiwen.coder@bytedance.com>
2025-04-30 22:33:23 +0800

    s1423

--- rust/workspace/leetcode/solution/src/s1423.rs
+++ rust/workspace/leetcode/solution/src/s1423.rs
@@ -25,16 +25,42 @@ pub fn max_score(card_points: Vec<i32>, k: i32) -> i32 {
     total - ans
 }
 
+pub fn max_score_v2(card_points: Vec<i32>, k: i32) -> i32 {
+    let n = card_points.len();
+    let k = n - k as usize;
+
+    let mut sum: i32 = card_points[0..k].iter().sum();
+    let mut total = sum;
+    let mut min = sum;
+
+    for i in k..n {
+        total += card_points[i];
+        sum += card_points[i] - card_points[i - k];
+        min = min.min(sum);
+    }
+
+    total - min
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn test(f: fn(Vec<i32>, i32) -> i32) {
+        assert_eq!(f(vec![1, 2, 3, 4, 5, 6, 1], 3), 12);
+        assert_eq!(f(vec![2, 2, 2], 2), 4);
+        assert_eq!(f(vec![9, 7, 7, 9, 7, 7, 9], 7), 55);
+        assert_eq!(f(vec![1, 1000, 1], 1), 1);
+        assert_eq!(f(vec![1, 79, 80, 1, 1, 1, 200, 1], 3), 202);
+    }
+
     #[test]
     fn test_max_score() {
-        assert_eq!(max_score(vec![1, 2, 3, 4, 5, 6, 1], 3), 12);
-        assert_eq!(max_score(vec![2, 2, 2], 2), 4);
-        assert_eq!(max_score(vec![9, 7, 7, 9, 7, 7, 9], 7), 55);
-        assert_eq!(max_score(vec![1, 1000, 1], 1), 1);
-        assert_eq!(max_score(vec![1, 79, 80, 1, 1, 1, 200, 1], 3), 202);
+        test(max_score);
+    }
+
+    #[test]
+    fn test_max_score_v2() {
+        test(max_score_v2);
     }
 }
