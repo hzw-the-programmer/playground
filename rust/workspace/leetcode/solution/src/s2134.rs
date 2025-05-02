72c8ec2cda811e17aa1ce4ef1e224e5a530705e7
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-03 07:21:50 +0800

    s2134

--- rust/workspace/leetcode/solution/src/s2134.rs
+++ rust/workspace/leetcode/solution/src/s2134.rs
@@ -28,14 +28,41 @@ pub fn min_swaps(nums: Vec<i32>) -> i32 {
     ans
 }
 
+pub fn min_swaps_v2(nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    let k = nums.iter().sum::<i32>() as usize;
+    if k <= 1 || k == n {
+        return 0;
+    }
+
+    let mut zeros = k as i32 - nums[0..k].iter().sum::<i32>();
+    let mut ans = zeros;
+
+    for i in 1..n {
+        zeros += nums[i - 1] - nums[(i + k - 1) % n];
+        ans = ans.min(zeros);
+    }
+
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn test(f: fn(Vec<i32>) -> i32) {
+        assert_eq!(f(vec![0, 1, 0, 1, 1, 0, 0]), 1);
+        assert_eq!(f(vec![0, 1, 1, 1, 0, 0, 1, 1, 0]), 2);
+        assert_eq!(f(vec![1, 1, 0, 0, 1]), 0);
+    }
+
     #[test]
     fn test_min_swaps() {
-        assert_eq!(min_swaps(vec![0, 1, 0, 1, 1, 0, 0]), 1);
-        assert_eq!(min_swaps(vec![0, 1, 1, 1, 0, 0, 1, 1, 0]), 2);
-        assert_eq!(min_swaps(vec![1, 1, 0, 0, 1]), 0);
+        test(min_swaps);
+    }
+
+    #[test]
+    fn test_min_swaps_v2() {
+        test(min_swaps_v2);
     }
 }
