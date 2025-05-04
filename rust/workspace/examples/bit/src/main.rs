8894f77e93b94e134cfcfab5ac0c9776f9408a48
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-04 21:38:45 +0800

    bit op

---
+++ rust/workspace/examples/bit/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let a = -1i8;
+    println!("{a:#b}");
+    let a = -2i8;
+    println!("{a:#b}");
+    let a = -3i8;
+    println!("{a:#b}");
+    let a = a >> 1;
+    println!("{a:#b}");
+    let a = 3i8;
+    println!("{a:#010b}");
+    let a = a >> 1;
+    println!("{a:#010b}");
+}
