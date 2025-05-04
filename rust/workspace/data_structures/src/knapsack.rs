7ad54ce4483fd90c7b4c2f621680ae18eff8335f
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-04 09:36:46 +0800

    knapsack

--- rust/workspace/data_structures/src/knapsack.rs
+++ rust/workspace/data_structures/src/knapsack.rs
@@ -71,6 +71,32 @@ fn knapsack01_memo_recursive(
     memo[n][capacity] as usize
 }
 
+pub fn knapsack01_dp(weights: &[usize], values: &[usize], capacity: usize) -> usize {
+    let n = weights.len();
+    let mut dp = vec![vec![0; capacity + 1]; n + 1];
+
+    // Build table dp[][] in bottom-up manner
+    for i in 0..=n {
+        for j in 0..=capacity {
+            // If there is no item or the knapsack's capacity is 0
+            if i == 0 || j == 0 {
+                dp[i][j] = 0;
+            } else {
+                // Pick the ith item if it does not exceed the capacity of knapsack
+                let mut pick = 0;
+                if weights[i - 1] <= j {
+                    pick = values[i - 1] + dp[i - 1][j - weights[i - 1]]
+                }
+                // Don't pick the ith item
+                let not_pick = dp[i - 1][j];
+                dp[i][j] = pick.max(not_pick);
+            }
+        }
+    }
+
+    dp[n][capacity]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -81,6 +107,7 @@ mod tests {
         let values = [3, 4, 5, 6];
         assert_eq!(knapsack01(&weights, &values, 5), 7);
         assert_eq!(knapsack01_memo(&weights, &values, 5), 7);
+        assert_eq!(knapsack01_dp(&weights, &values, 5), 7);
     }
 
     #[test]
@@ -89,12 +116,14 @@ mod tests {
         let values = [1, 4, 5, 7];
         assert_eq!(knapsack01(&weights, &values, 7), 9);
         assert_eq!(knapsack01_memo(&weights, &values, 7), 9);
+        assert_eq!(knapsack01_dp(&weights, &values, 7), 9);
     }
 
     #[test]
     fn test_empty_items() {
         assert_eq!(knapsack01(&[], &[], 10), 0);
         assert_eq!(knapsack01_memo(&[], &[], 10), 0);
+        assert_eq!(knapsack01_dp(&[], &[], 10), 0);
     }
 
     #[test]
@@ -103,6 +132,7 @@ mod tests {
         let values = [2, 3, 4];
         assert_eq!(knapsack01(&weights, &values, 0), 0);
         assert_eq!(knapsack01_memo(&weights, &values, 0), 0);
+        assert_eq!(knapsack01_dp(&weights, &values, 0), 0);
     }
 
     #[test]
