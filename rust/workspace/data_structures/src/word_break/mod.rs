a0039755889669381e24c1a49dd4560221c61a6f
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-05 10:02:17 +0800

    word break

--- rust/workspace/data_structures/src/word_break/mod.rs
+++ rust/workspace/data_structures/src/word_break/mod.rs
@@ -1,29 +1,5 @@
-use crate::trie::Trie;
-
-// https://www.geeksforgeeks.org/word-break-problem-trie-solution/
-// Time Complexity: O(n*m) where n is the length of the string and m is the length of the longest word in the dictionary.
-// Auxiliary Space: O(n)
-pub fn word_break(s: &str, dictionary: &[&str]) -> bool {
-    let mut trie = Trie::new();
-    dictionary.iter().for_each(|w| trie.insert(w));
-
-    word_break_recursive(s, &trie)
-}
-
-fn word_break_recursive(s: &str, trie: &Trie) -> bool {
-    let n = s.len();
-    if n == 0 {
-        true
-    } else {
-        for i in 1..=n {
-            if trie.starts_with(&s[..i]) && word_break_recursive(&s[i..], trie) {
-                return true;
-            }
-        }
-
-        false
-    }
-}
+mod trie;
+pub use trie::word_break;
 
 #[cfg(test)]
 mod tests {
---
+++ rust/workspace/data_structures/src/word_break/trie.rs
@@ -0,0 +1,29 @@
+/*
+  https://www.geeksforgeeks.org/word-break-problem-trie-solution/
+  Time Complexity: O(n*m) where n is the length of the string and m is the length of the longest word in the dictionary.
+  Auxiliary Space: O(n)
+*/
+
+use crate::trie::Trie;
+
+pub fn word_break(s: &str, dictionary: &[&str]) -> bool {
+    let mut trie = Trie::new();
+    dictionary.iter().for_each(|w| trie.insert(w));
+
+    word_break_recursive(s, &trie)
+}
+
+fn word_break_recursive(s: &str, trie: &Trie) -> bool {
+    let n = s.len();
+    if n == 0 {
+        true
+    } else {
+        for i in 1..=n {
+            if trie.starts_with(&s[..i]) && word_break_recursive(&s[i..], trie) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
