cb32ddc1ce92e7351e0a2ebad58bca9f3a27fb60
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-05 18:46:02 +0800

    refactor fibonacci tests

--- rust/workspace/data_structures/src/fibonacci/tests.rs
+++ rust/workspace/data_structures/src/fibonacci/tests.rs
@@ -2,15 +2,8 @@ use super::fibonacci;
 
 #[test]
 fn test_fibonacci() {
-    assert_eq!(fibonacci(0), 0);
-    assert_eq!(fibonacci(1), 1);
-    assert_eq!(fibonacci(2), 1);
-    assert_eq!(fibonacci(3), 2);
-    assert_eq!(fibonacci(4), 3);
-    assert_eq!(fibonacci(5), 5);
-    assert_eq!(fibonacci(6), 8);
-    assert_eq!(fibonacci(7), 13);
-    assert_eq!(fibonacci(8), 21);
-    assert_eq!(fibonacci(9), 34);
-    assert_eq!(fibonacci(10), 55);
+    let expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    for (i, &ans) in expected.iter().enumerate() {
+        assert_eq!(fibonacci(i), ans);
+    }
 }
