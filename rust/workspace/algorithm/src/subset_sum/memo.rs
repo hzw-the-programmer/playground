7c56f0ade4647ef9c86e23ca14d8bbed4191994c
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-06 19:45:15 +0800

    subset sum memo

---
+++ rust/workspace/algorithm/src/subset_sum/memo.rs
@@ -0,0 +1,22 @@
+pub fn subset_sum(nums: &[u32], sum: u32) -> bool {
+    let n = nums.len();
+    let mut memo = vec![vec![-1; sum as usize + 1]; n + 1];
+    subset_sum_recursive(nums, n, sum, &mut memo)
+}
+
+fn subset_sum_recursive(nums: &[u32], n: usize, sum: u32, memo: &mut Vec<Vec<i8>>) -> bool {
+    if sum == 0 {
+        true
+    } else if n == 0 {
+        false
+    } else {
+        if memo[n][sum as usize] == -1 {
+            let mut res = false;
+            if nums[n - 1] <= sum {
+                res = subset_sum_recursive(nums, n - 1, sum - nums[n - 1], memo);
+            }
+            memo[n][sum as usize] = (res || subset_sum_recursive(nums, n - 1, sum, memo)) as i8;
+        }
+        memo[n][sum as usize] != 0
+    }
+}
--- rust/workspace/algorithm/src/subset_sum/mod.rs
+++ rust/workspace/algorithm/src/subset_sum/mod.rs
@@ -1,7 +1,10 @@
 // https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
 
-mod recursive;
-pub use recursive::subset_sum;
+// mod recursive;
+// pub use recursive::subset_sum;
+
+mod memo;
+pub use memo::subset_sum;
 
 #[cfg(test)]
 mod tests;
