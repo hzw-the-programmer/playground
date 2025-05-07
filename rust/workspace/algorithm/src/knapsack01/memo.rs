5a9baf95ca565c3e87ad5936c458390149d54392
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-08 07:32:05 +0800

    knapsack01

--- rust/workspace/algorithm/src/knapsack01/memo.rs
+++ rust/workspace/algorithm/src/knapsack01/memo.rs
@@ -9,43 +9,37 @@ pub fn knapsack01(weights: &[usize], values: &[usize], capacity: usize) -> usize
     );
 
     let mut memo = vec![vec![-1; capacity + 1]; weights.len() + 1];
-    knapsack01_recursive(weights, values, capacity, &mut memo)
+    knapsack01_recursive(weights, values, weights.len(), capacity, &mut memo)
 }
 
 fn knapsack01_recursive(
     weights: &[usize],
     values: &[usize],
+    len: usize,
     capacity: usize,
     memo: &mut [Vec<i32>],
 ) -> usize {
-    let n = weights.len();
-
     // Base Case
-    if n == 0 || capacity == 0 {
+    if len == 0 || capacity == 0 {
         return 0;
     }
 
     // Check if we have previously calculated the same subproblem
-    if memo[n][capacity] != -1 {
-        return memo[n][capacity] as usize;
+    if memo[len][capacity] != -1 {
+        return memo[len][capacity] as usize;
     }
 
     // Pick nth item if it does not exceed the capacity of knapsack
     let mut pick = 0;
-    if weights[n - 1] <= capacity {
-        pick = values[n - 1]
-            + knapsack01_recursive(
-                &weights[..n - 1],
-                &values[..n - 1],
-                capacity - weights[n - 1],
-                memo,
-            );
+    if weights[len - 1] <= capacity {
+        pick = values[len - 1]
+            + knapsack01_recursive(weights, values, len - 1, capacity - weights[len - 1], memo);
     }
 
     // Don't pick the nth item
-    let not_pick = knapsack01_recursive(&weights[..n - 1], &values[..n - 1], capacity, memo);
+    let not_pick = knapsack01_recursive(weights, values, len - 1, capacity, memo);
 
     // Store the result in memo[n][capacity] and return it
-    memo[n][capacity] = pick.max(not_pick) as i32;
-    memo[n][capacity] as usize
+    memo[len][capacity] = pick.max(not_pick) as i32;
+    memo[len][capacity] as usize
 }
--- rust/workspace/algorithm/src/knapsack01/recursive.rs
+++ rust/workspace/algorithm/src/knapsack01/recursive.rs
@@ -7,26 +7,21 @@ pub fn knapsack01(weights: &[usize], values: &[usize], capacity: usize) -> usize
         "weights and values must have the same length"
     );
 
-    let n = weights.len();
+    knapsack01_recursive(weights, values, weights.len(), capacity)
+}
 
-    // Base Case
-    if n == 0 || capacity == 0 {
+fn knapsack01_recursive(weights: &[usize], values: &[usize], len: usize, capacity: usize) -> usize {
+    if len == 0 || capacity == 0 {
         return 0;
     }
 
-    // Pick nth item if it does not exceed the capacity of knapsack
     let mut pick = 0;
-    if weights[n - 1] <= capacity {
-        pick = values[n - 1]
-            + knapsack01(
-                &weights[..n - 1],
-                &values[..n - 1],
-                capacity - weights[n - 1],
-            );
+    if weights[len - 1] <= capacity {
+        pick = values[len - 1]
+            + knapsack01_recursive(weights, values, len - 1, capacity - weights[len - 1]);
     }
 
-    // Don't pick the nth item
-    let not_pick = knapsack01(&weights[..n - 1], &values[..n - 1], capacity);
+    let not_pick = knapsack01_recursive(weights, values, len - 1, capacity);
 
     pick.max(not_pick)
 }
