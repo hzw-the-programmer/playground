40e173e53e2119c67096406ef5f4734cd0496004
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-07 10:07:21 +0800

    cut rod memo

---
+++ rust/workspace/algorithm/src/cut_rod/memo.rs
@@ -0,0 +1,20 @@
+// time: O(n^2)
+// space: O(n)
+pub fn cut_rod(prices: &[i32], n: usize) -> i32 {
+    let mut memo = vec![-1; n + 1];
+    cut_rod_recursive(prices, n, &mut memo)
+}
+
+fn cut_rod_recursive(prices: &[i32], n: usize, memo: &mut [i32]) -> i32 {
+    if n == 0 {
+        0
+    } else {
+        if memo[n] == -1 {
+            memo[n] = 0;
+            for i in 1..(n + 1).min(prices.len() + 1) {
+                memo[n] = memo[n].max(prices[i - 1] + cut_rod_recursive(prices, n - i, memo));
+            }
+        }
+        memo[n]
+    }
+}
--- rust/workspace/algorithm/src/cut_rod/mod.rs
+++ rust/workspace/algorithm/src/cut_rod/mod.rs
@@ -1,7 +1,10 @@
 // https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
 
-mod recursive;
-pub use recursive::cut_rod;
+// mod recursive;
+// pub use recursive::cut_rod;
+
+mod memo;
+pub use memo::cut_rod;
 
 #[cfg(test)]
 mod tests;
