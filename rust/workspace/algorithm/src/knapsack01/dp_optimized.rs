0a0030f2c7cc6fa6dd313c64d1489bbe1720d39d
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-07 17:57:40 +0800

    knapsack01 dp optimized

--- rust/workspace/algorithm/src/knapsack01/dp_optimized.rs
+++ rust/workspace/algorithm/src/knapsack01/dp_optimized.rs
@@ -1,25 +1,46 @@
-pub fn knapsack01(weights: &[u32], values: &[u32], capacity: u32) -> u32 {
+// Bottom-Up DP (Space-Optimized)
+// time: O(n*capacity)
+// space: O(capacity)
+pub fn knapsack01(weights: &[usize], values: &[usize], capacity: usize) -> usize {
     assert_eq!(
         weights.len(),
         values.len(),
         "weights and values must have the same length"
     );
 
-    let n = weights.len();
-    let w = capacity as usize;
-    let mut dp = vec![0; w + 1];
+    let mut dp = vec![0; capacity + 1];
 
-    for i in 1..=n {
-        let mut j = w;
-        while j as u32 >= weights[i - 1] {
-            dp[j] = dp[j].max(values[i - 1] + dp[j - weights[i - 1] as usize]);
-            if j > 0 {
-                j -= 1;
-            } else {
-                break;
-            }
+    for (&w, &v) in weights.iter().zip(values.iter()) {
+        for j in (w..=capacity).rev() {
+            dp[j] = dp[j].max(v + dp[j - w]);
         }
     }
 
-    dp[w]
+    dp[capacity]
 }
+
+// pub fn knapsack01(weights: &[u32], values: &[u32], capacity: u32) -> u32 {
+//     assert_eq!(
+//         weights.len(),
+//         values.len(),
+//         "weights and values must have the same length"
+//     );
+
+//     let n = weights.len();
+//     let w = capacity as usize;
+//     let mut dp = vec![0; w + 1];
+
+//     for i in 1..=n {
+//         let mut j = w;
+//         while j as u32 >= weights[i - 1] {
+//             dp[j] = dp[j].max(values[i - 1] + dp[j - weights[i - 1] as usize]);
+//             if j > 0 {
+//                 j -= 1;
+//             } else {
+//                 break;
+//             }
+//         }
+//     }
+
+//     dp[w]
+// }
---
+++ rust/workspace/algorithm/src/knapsack01/mod.rs
@@ -0,0 +1,16 @@
+// https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+
+// mod recursive;
+// pub use recursive::knapsack01;
+
+// mod memo;
+// pub use memo::knapsack01;
+
+// mod dp;
+// pub use dp::knapsack01;
+
+mod dp_optimized;
+pub use dp_optimized::knapsack01;
+
+#[cfg(test)]
+mod tests;
