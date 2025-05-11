2f879e5602fc03bdc5ac41eb04f046529769dcd6
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-11 15:03:36 +0800

    s22

--- rust/workspace/leetcode/solution/src/s22/tests.rs
+++ rust/workspace/leetcode/solution/src/s22/tests.rs
@@ -2,9 +2,10 @@ use super::generate_parenthesis;
 
 #[test]
 fn test_generate_parenthesis() {
+    assert_eq!(generate_parenthesis(1), vec!["()"]);
+    assert_eq!(generate_parenthesis(2), vec!["(())", "()()"]);
     assert_eq!(
         generate_parenthesis(3),
         vec!["((()))", "(()())", "(())()", "()(())", "()()()"]
     );
-    assert_eq!(generate_parenthesis(1), vec!["()"]);
 }
