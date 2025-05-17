5c6be16768f5f37e28f9faa20c767e4c59303c39
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-17 19:04:59 +0800

    s122 greedy

--- rust/workspace/leetcode/solution/src/s122/greedy.rs
+++ rust/workspace/leetcode/solution/src/s122/greedy.rs
@@ -1,16 +1,9 @@
 pub fn max_profit(prices: Vec<i32>) -> i32 {
-    let mut buy = i32::MAX;
-    let mut max = 0;
     let mut ans = 0;
-    for i in 0..prices.len() {
-        if i > 0 && prices[i] < prices[i - 1] {
-            ans += max;
-            max = 0;
-            buy = prices[i];
-        } else {
-            buy = buy.min(prices[i]);
-            max = max.max(prices[i] - buy);
+    for i in 1..prices.len() {
+        if prices[i] > prices[i - 1] {
+            ans += prices[i] - prices[i - 1];
         }
     }
-    ans + max
+    ans
 }
