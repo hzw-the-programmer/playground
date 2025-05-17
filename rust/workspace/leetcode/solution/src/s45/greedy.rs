5c9400d9020e35a2cba1b7a3fab4f8f6b66ea2f3
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-17 10:41:14 +0800

    s45

--- rust/workspace/leetcode/solution/src/s45/greedy.rs
+++ rust/workspace/leetcode/solution/src/s45/greedy.rs
@@ -1,16 +1,15 @@
 pub fn jump(nums: Vec<i32>) -> i32 {
-    if nums.len() == 1 {
-        return 0;
-    }
-    let mut jump = 1;
-    let mut end = 0 + nums[0] as usize;
+    let mut jumps = 0;
+    let mut end = 0;
     let mut max = 0;
-    for i in 1..nums.len() {
-        if i > end {
-            jump += 1;
+
+    for (i, &n) in nums[..nums.len() - 1].iter().enumerate() {
+        max = max.max(i + n as usize);
+        if i == end {
+            jumps += 1;
             end = max;
         }
-        max = max.max(i + nums[i] as usize);
     }
-    jump
+
+    jumps
 }
