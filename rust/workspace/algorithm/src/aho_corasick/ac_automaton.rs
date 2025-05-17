cdd581320c7ef792cdc9d0dc469fdf909d0ec4f0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-17 16:55:41 +0800

    aho corasick

--- rust/workspace/algorithm/src/aho_corasick/ac_automaton.rs
+++ rust/workspace/algorithm/src/aho_corasick/ac_automaton.rs
@@ -9,6 +9,7 @@ fn get_index(c: char) -> usize {
 pub struct ACAutomaton {
     g: [[usize; CHARSET_LEN]; TOTAL_STATES],
     out: [usize; TOTAL_STATES],
+    f: [usize; TOTAL_STATES],
 }
 
 impl ACAutomaton {
@@ -16,6 +17,7 @@ impl ACAutomaton {
         Self {
             g: [[INVALID_STATE; CHARSET_LEN]; TOTAL_STATES],
             out: [0; TOTAL_STATES],
+            f: [INVALID_STATE; TOTAL_STATES],
         }
     }
 
@@ -34,8 +36,64 @@ impl ACAutomaton {
             self.out[current] |= 1 << i;
         }
 
+        let mut queue = std::collections::VecDeque::new();
+        for i in 0..CHARSET_LEN {
+            if self.g[0][i] == INVALID_STATE {
+                self.g[0][i] = 0;
+            } else {
+                self.f[self.g[0][i]] = 0;
+                queue.push_back(self.g[0][i]);
+            }
+        }
+
+        while let Some(state) = queue.pop_front() {
+            for i in 0..CHARSET_LEN {
+                let next = self.g[state][i];
+                if next != INVALID_STATE {
+                    let mut failure = self.f[state];
+                    while self.g[failure][i] == INVALID_STATE {
+                        failure = self.f[failure];
+                    }
+                    failure = self.g[failure][i];
+
+                    self.f[next] = failure;
+                    self.out[next] |= self.out[failure];
+                    queue.push_back(next);
+                }
+            }
+        }
+
         #[cfg(test)]
-        self.dump_g(state);
+        self.dump(state);
+    }
+
+    fn next_state(&self, mut current: usize, c: char) -> usize {
+        let i = get_index(c);
+        while self.g[current][i] == INVALID_STATE {
+            current = self.f[current]
+        }
+        self.g[current][i]
+    }
+
+    pub fn search(&self, text: &str, words: &[&str]) -> Vec<(usize, usize, usize)> {
+        let mut result = Vec::new();
+
+        let mut current = 0;
+        for (i, c) in text.chars().enumerate() {
+            current = self.next_state(current, c);
+
+            if self.out[current] == 0 {
+                continue;
+            }
+
+            for (j, word) in words.iter().enumerate() {
+                if self.out[current] & (1 << j) != 0 {
+                    result.push((j, i - word.len() + 1, i));
+                }
+            }
+        }
+
+        result
     }
 
     pub fn starts_with(&self, text: &str) -> bool {
@@ -51,6 +109,13 @@ impl ACAutomaton {
     }
 
     #[cfg(test)]
+    fn dump(&self, total_states: usize) {
+        self.dump_g(total_states);
+        println!("");
+        self.dump_f(total_states);
+    }
+
+    #[cfg(test)]
     fn dump_g(&self, total_states: usize) {
         print!("   ");
         "abcdefghijklmnopqrstuvwxyz"
@@ -60,11 +125,22 @@ impl ACAutomaton {
         for i in 0..total_states {
             print!("{i:02} ");
             for j in 0..CHARSET_LEN {
-                if self.g[i][j] == INVALID_STATE {
-                    print!("   ");
-                } else {
-                    print!("{:02} ", self.g[i][j]);
+                match self.g[i][j] {
+                    0 | INVALID_STATE => print!("   "),
+                    s => print!("{s:02} "),
                 }
+            }
+            println!("");
+        }
+    }
+
+    #[cfg(test)]
+    fn dump_f(&self, total_states: usize) {
+        for i in 0..total_states {
+            print!("{i:02} ");
+            match self.f[i] {
+                0 | INVALID_STATE => print!("   "),
+                s => print!("{s:02} "),
             }
             println!("");
         }
--- rust/workspace/algorithm/src/aho_corasick/mod.rs
+++ rust/workspace/algorithm/src/aho_corasick/mod.rs
@@ -1,4 +1,5 @@
 // https://www.geeksforgeeks.org/aho-corasick-algorithm-pattern-searching/
+// https://web.stanford.edu/class/archive/cs/cs166/cs166.1166/lectures/02/Small02.pdf
 
 mod ac_automaton;
 pub use ac_automaton::*;
