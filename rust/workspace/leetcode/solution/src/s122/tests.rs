86fa3ef1b2821455045f1d9f31ca27b9c1e15735
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-17 18:57:15 +0800

    s122 test

--- rust/workspace/leetcode/solution/src/s122/tests.rs
+++ rust/workspace/leetcode/solution/src/s122/tests.rs
@@ -2,8 +2,8 @@ use super::*;
 
 #[test]
 fn test_max_profit() {
-    // assert_eq!(max_profit(vec![7, 1, 5, 3, 6, 4]), 7);
-    // assert_eq!(max_profit(vec![1, 2, 3, 4, 5]), 4);
-    // assert_eq!(max_profit(vec![7, 6, 4, 3, 1]), 0);
+    assert_eq!(max_profit(vec![7, 1, 5, 3, 6, 4]), 7);
+    assert_eq!(max_profit(vec![1, 2, 3, 4, 5]), 4);
+    assert_eq!(max_profit(vec![7, 6, 4, 3, 1]), 0);
     assert_eq!(max_profit(vec![2, 1, 2, 0, 1]), 2);
 }
