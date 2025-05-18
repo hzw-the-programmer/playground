0aafc6ad2800f7ad414a81ad8fe54c105ee716c0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-18 09:57:59 +0800

    s714

--- rust/workspace/leetcode/solution/src/s714/greedy.rs
+++ rust/workspace/leetcode/solution/src/s714/greedy.rs
@@ -1,24 +1,41 @@
 pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
-    let mut min_price = prices[0];
-    let mut max_profit = 0;
-    let mut ans = 0;
+    let mut profit = 0;
 
+    let mut buy = prices[0] + fee;
     for i in 1..prices.len() {
-        if min_price + max_profit > prices[i] + fee {
-            if max_profit > fee {
-                ans += max_profit - fee;
-            }
-            min_price = prices[i];
-            max_profit = 0;
-        } else {
-            min_price = min_price.min(prices[i]);
-            max_profit = max_profit.max(prices[i] - min_price);
+        let price = prices[i] + fee;
+        if price < buy {
+            buy = price;
+        } else if prices[i] > buy {
+            profit += prices[i] - buy;
+            buy = prices[i];
         }
     }
 
-    if max_profit > fee {
-        ans += max_profit - fee;
-    }
-
-    ans
+    profit
 }
+
+// pub fn max_profit(prices: Vec<i32>, fee: i32) -> i32 {
+//     let mut min_price = prices[0];
+//     let mut max_profit = 0;
+//     let mut ans = 0;
+
+//     for i in 1..prices.len() {
+//         if min_price + max_profit > prices[i] + fee {
+//             if max_profit > fee {
+//                 ans += max_profit - fee;
+//             }
+//             min_price = prices[i];
+//             max_profit = 0;
+//         } else {
+//             min_price = min_price.min(prices[i]);
+//             max_profit = max_profit.max(prices[i] - min_price);
+//         }
+//     }
+
+//     if max_profit > fee {
+//         ans += max_profit - fee;
+//     }
+
+//     ans
+// }
