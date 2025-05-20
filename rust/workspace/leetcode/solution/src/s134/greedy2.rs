8c8efe61ebe93bdfcf6cb041b5487cf92f49941b
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-21 06:47:42 +0800

    s134 greedy2

--- rust/workspace/leetcode/solution/src/s134/greedy2.rs
+++ rust/workspace/leetcode/solution/src/s134/greedy2.rs
@@ -3,19 +3,42 @@ pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
     let mut i = 0;
     while i < n {
         let mut remain = 0;
-        let mut j = i;
-        while j < i + n {
-            remain += gas[j % n] - cost[j % n];
+        let mut cnt = 0;
+        while cnt < n {
+            let j = (i + cnt) % n;
+            remain += gas[j] - cost[j];
             if remain < 0 {
                 break;
             }
-            j += 1;
+            cnt += 1;
         }
-        if j == i + n {
+        if cnt == n {
             return i as i32;
         }
-        i = j + 1;
+        i += cnt + 1;
     }
 
     -1
 }
+
+// pub fn can_complete_circuit(gas: Vec<i32>, cost: Vec<i32>) -> i32 {
+//     let n = gas.len();
+//     let mut i = 0;
+//     while i < n {
+//         let mut remain = 0;
+//         let mut j = i;
+//         while j < i + n {
+//             remain += gas[j % n] - cost[j % n];
+//             if remain < 0 {
+//                 break;
+//             }
+//             j += 1;
+//         }
+//         if j == i + n {
+//             return i as i32;
+//         }
+//         i = j + 1;
+//     }
+
+//     -1
+// }
