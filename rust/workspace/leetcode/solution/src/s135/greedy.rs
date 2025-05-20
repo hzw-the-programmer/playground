2ad5b3368b94fc9ba706a9730778ce6e93847653
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-21 06:26:19 +0800

    s135

--- rust/workspace/leetcode/solution/src/s135/greedy.rs
+++ rust/workspace/leetcode/solution/src/s135/greedy.rs
@@ -3,44 +3,55 @@ pub fn candy(ratings: Vec<i32>) -> i32 {
     let mut inc = 0;
     let mut dec = 0;
     let mut total = 0;
+
+    let check = |mut inc, mut dec| {
+        let mut delta = 0;
+        if dec == 0 {
+            delta += (inc + 1) * inc / 2;
+        } else {
+            if inc == 0 {
+                delta += (dec + 1) * dec / 2;
+            } else {
+                if dec > inc {
+                    inc -= 1;
+                } else {
+                    dec -= 1;
+                }
+                delta += (dec + 1) * dec / 2;
+                delta += (inc + 1) * inc / 2;
+            }
+        }
+        delta
+    };
+
     for i in 1..n {
         if ratings[i] > ratings[i - 1] {
             if dec > 0 {
-                if inc == 0 {
-                    total += (dec + 1) * dec / 2;
-                } else {
-                    if dec > inc {
-                        inc -= 1;
-                    } else {
-                        dec -= 1;
-                    }
-                    total += (dec + 1) * dec / 2;
-                    total += (inc + 1) * inc / 2;
-                }
+                total += check(inc, dec);
                 dec = 0;
                 inc = 0;
             }
             inc += 1;
         } else if ratings[i] < ratings[i - 1] {
             dec += 1;
-        }
-    }
-
-    if dec > 0 {
-        if inc == 0 {
-            total += (dec + 1) * dec / 2;
         } else {
-            if dec > inc {
-                inc -= 1;
-            } else {
-                dec -= 1;
+            if dec == 0 {
+                total += check(inc, dec);
+                inc = 0;
+            } else if dec == 1 {
+                if inc != 0 {
+                    dec = 0;
+                    total += check(inc, dec);
+                    inc = 0;
+                } else {
+                    total += check(inc, dec);
+                    dec = 0;
+                }
             }
-            total += (dec + 1) * dec / 2;
-            total += (inc + 1) * inc / 2;
         }
-    } else {
-        total += (inc + 1) * inc / 2;
     }
 
-    (n + total) as _
+    total += check(inc, dec);
+
+    n as i32 + total
 }
--- rust/workspace/leetcode/solution/src/s135/tests.rs
+++ rust/workspace/leetcode/solution/src/s135/tests.rs
@@ -4,9 +4,27 @@ use super::*;
 fn test_candy() {
     assert_eq!(candy(vec![1, 0, 2]), 5);
     assert_eq!(candy(vec![1, 2, 2]), 4);
+    assert_eq!(candy(vec![1, 3, 2, 2, 1]), 7);
+    assert_eq!(candy(vec![1, 2, 87, 87, 87, 2, 1]), 13);
 
     assert_eq!(candy(vec![2, 2, 2]), 3);
     assert_eq!(candy(vec![1, 2, 3]), 6);
     assert_eq!(candy(vec![1, 2, 3, 2]), 7);
     assert_eq!(candy(vec![1, 2, 3, 2, 1]), 9);
+
+    assert_eq!(candy(vec![1, 2, 3]), 6);
+    assert_eq!(candy(vec![1, 2, 3, 2]), 7);
+    assert_eq!(candy(vec![1, 2, 3, 2, 1]), 9);
+    assert_eq!(candy(vec![1, 2, 3, 2, 1, 0]), 13);
+
+    assert_eq!(candy(vec![3, 4, 5, 5]), 7);
+    assert_eq!(candy(vec![3, 4, 5, 5, 5]), 8);
+    assert_eq!(candy(vec![3, 4, 5, 5, 4]), 9);
+    assert_eq!(candy(vec![3, 4, 5, 5, 5, 4]), 10);
+
+    assert_eq!(candy(vec![3, 4, 5, 4, 4]), 8);
+    assert_eq!(candy(vec![3, 4, 5, 4, 4, 4]), 9);
+    assert_eq!(candy(vec![3, 4, 5, 4, 4, 4, 3]), 11);
+
+    assert_eq!(candy(vec![4, 3, 3, 2, 1]), 9);
 }
