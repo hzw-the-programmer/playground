1a2168819e3a17d22e55e87fa0f00eed656ca70d
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-21 11:21:52 +0800

    s135

--- rust/workspace/leetcode/solution/src/s135/greedy2.rs
+++ rust/workspace/leetcode/solution/src/s135/greedy2.rs
@@ -5,13 +5,12 @@ pub fn candy(ratings: Vec<i32>) -> i32 {
     let mut dec = 1;
     let mut inc = 1;
     for i in 1..n {
-        if ratings[i] > ratings[i - 1] {
-            pre += 1;
-            ret += pre;
-            inc = pre;
-            dec = 0;
-        } else if ratings[i] == ratings[i - 1] {
-            pre = 1;
+        if ratings[i] >= ratings[i - 1] {
+            pre = if ratings[i] > ratings[i - 1] {
+                pre + 1
+            } else {
+                1
+            };
             ret += pre;
             inc = pre;
             dec = 0;
