705de33d1219a80476e9905054072d2bfd948477
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-21 11:19:52 +0800

    s135

---
+++ rust/workspace/leetcode/solution/src/s135/greedy2.rs
@@ -0,0 +1,31 @@
+pub fn candy(ratings: Vec<i32>) -> i32 {
+    let n = ratings.len();
+    let mut pre = 1;
+    let mut ret = 1;
+    let mut dec = 1;
+    let mut inc = 1;
+    for i in 1..n {
+        if ratings[i] > ratings[i - 1] {
+            pre += 1;
+            ret += pre;
+            inc = pre;
+            dec = 0;
+        } else if ratings[i] == ratings[i - 1] {
+            pre = 1;
+            ret += pre;
+            inc = pre;
+            dec = 0;
+        } else {
+            pre = 1;
+
+            dec += 1;
+            if dec == inc {
+                dec += 1;
+            }
+
+            ret += dec;
+        }
+    }
+
+    ret
+}
--- rust/workspace/leetcode/solution/src/s135/mod.rs
+++ rust/workspace/leetcode/solution/src/s135/mod.rs
@@ -1,7 +1,11 @@
 // 135. Candy
+// 1 <= n <= 2 * 10^4
 
-mod greedy;
-pub use greedy::candy;
+// mod greedy;
+// pub use greedy::candy;
+
+mod greedy2;
+pub use greedy2::candy;
 
 #[cfg(test)]
 mod tests;
