4673f142444fa0078dfc5bfe15e16b2d4ddb881c
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-23 11:45:20 +0800

    pkcs7 padding

---
+++ rust/workspace/examples/crypto/src/padding/pkcs7_tests.rs
@@ -0,0 +1,30 @@
+use inout::block_padding::array::{
+    Array,
+    typenum::{U8, U10},
+};
+use inout::block_padding::{Padding, Pkcs7};
+
+#[test]
+fn test_1() {
+    // D:\github\RustCrypto\utils\block-padding\src\lib.rs
+    let msg = b"test";
+    let pos = msg.len();
+    let mut block: Array<u8, U8> = [0xff; 8].into();
+    block[..pos].copy_from_slice(msg);
+    Pkcs7::pad(&mut block, pos);
+    assert_eq!(&block[..], b"test\x04\x04\x04\x04");
+    let res = Pkcs7::unpad(&block).unwrap();
+    assert_eq!(res, msg);
+}
+
+#[test]
+fn test_2() {
+    let msg = b"test";
+    let pos = msg.len();
+    let mut block: Array<u8, U10> = [0xff; 10].into();
+    block[..pos].copy_from_slice(msg);
+    Pkcs7::pad(&mut block, pos);
+    assert_eq!(&block[..], b"test\x06\x06\x06\x06\x06\x06");
+    let res = Pkcs7::unpad(&block).unwrap();
+    assert_eq!(res, msg);
+}
