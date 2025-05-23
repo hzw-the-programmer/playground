e8c3e3295ef64f35a3aef4ecc99113504fa593c0
hezhiwen <hezhiwen.coder@bytedance.com>
2025-05-23 10:22:20 +0800

    aes test

---
+++ rust/workspace/examples/crypto/src/aes/tests.rs
@@ -0,0 +1,34 @@
+use aes::Aes128;
+use aes::cipher::{Array, BlockCipherDecrypt, BlockCipherEncrypt, KeyInit};
+
+#[test]
+fn test_aes() {
+    // D:\github\RustCrypto\block-ciphers\aes\src\lib.rs
+    let key = Array::from([0u8; 16]);
+    let mut block = Array::from([42u8; 16]);
+
+    let cipher = Aes128::new(&key);
+
+    let block_copy = block.clone();
+
+    cipher.encrypt_block(&mut block);
+    cipher.decrypt_block(&mut block);
+    assert_eq!(block, block_copy);
+
+    cipher.decrypt_block(&mut block);
+    cipher.encrypt_block(&mut block);
+    assert_eq!(block, block_copy);
+
+    let mut blocks = [block; 100];
+    cipher.encrypt_blocks(&mut blocks);
+    for block in blocks.iter_mut() {
+        cipher.decrypt_block(block);
+        assert_eq!(block, &block_copy);
+    }
+
+    cipher.decrypt_blocks(&mut blocks);
+    for block in blocks.iter_mut() {
+        cipher.encrypt_block(block);
+        assert_eq!(block, &block_copy);
+    }
+}
