b97155b0e65f362c73b1fe985abff40c17464433
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-02 17:13:02 +0800

    MaybeUinit & ManuallyDrop

--- rust/workspace/examples/drop/src/box2.rs
+++ rust/workspace/examples/drop/src/box2.rs
@@ -1,7 +1,9 @@
 pub fn test() {
     // test1();
     // test2();
-    test3();
+    // test3();
+    // test4();
+    test5();
 }
 
 fn test1() {
@@ -37,10 +39,38 @@ fn test3() {
     println!("leave");
 }
 
+fn test4() {
+    let mut x = std::mem::MaybeUninit::<&i32>::uninit();
+    // expected `i32`, found `MaybeUninit<&i32>`
+    // let i: i32 = x;
+    x.write(&0);
+    let x = unsafe { x.assume_init() };
+    // expected `i32`, found `&i32`
+    // let i: i32 = x;
+
+    let b = Box::new(Bar {
+        f1: unsafe { std::mem::MaybeUninit::zeroed().assume_init() },
+        f2: Foo(2),
+    });
+    println!("leave");
+}
+
+fn test5() {
+    let b = Box::new(Bar {
+        f1: unsafe { std::mem::MaybeUninit::zeroed().assume_init() },
+        f2: Foo(2),
+    });
+    let _ = std::mem::ManuallyDrop::new(b.f1);
+    println!("leave");
+}
+
 struct Foo(i32);
 
 impl Drop for Foo {
     fn drop(&mut self) {
+        if self.0 == 0 {
+            panic!("id is zero");
+        }
         println!("Foo {} drop", self.0);
     }
 }
