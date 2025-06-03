7588315ac890b0b63777d2e4be2e59f94c906712
hezhiwen <hezhiwen.coder@bytedance.com>
2025-06-03 18:28:02 +0800

    Option<Box<T>> ptrs

--- rust/workspace/examples/box/src/test1.rs
+++ rust/workspace/examples/box/src/test1.rs
@@ -13,6 +13,27 @@ fn test1() {
     println!("b: {:p}", b);
 
     let o = Some(b);
-    println!("o: {:p}", &o as *const Option<Box<i32>>);
-    println!("o: {:p}", o.as_ref().unwrap() as *const Box<i32>);
+    let o1 = &o as *const Option<Box<i32>>;
+    let o2 = o.as_ref().unwrap() as *const Box<i32>;
+    println!("o: {:p}", o1);
+    println!("o: {:p}", o2);
+
+    let n = o.as_ref();
+    let n1 = &n as *const Option<&Box<i32>>;
+    let n2 = n.unwrap() as *const Box<i32>;
+    println!("n: {:p}", n1);
+    println!("n: {:p}", n2);
+
+    assert!(o1 as u64 == o2 as u64);
+    assert!(o1 as u64 != n1 as u64);
+    assert!(o1 as u64 == n2 as u64);
+
+    let b = unsafe { Box::from_raw(r) };
+    println!("b: {:p}", b);
+
+    let p = Some(b);
+    println!("p: {:p}", &p as *const Option<Box<i32>>);
+    println!("p: {:p}", p.as_ref().unwrap() as *const Box<i32>);
+
+    let _ = std::mem::ManuallyDrop::new(p);
 }
