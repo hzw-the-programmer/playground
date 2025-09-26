55d550b2e7e9136ae5d3fe2b4686801c746e693a
hezhiwen <hezhiwen.coder@bytedance.com>
2025-09-27 07:56:17 +0800

    write

--- rust/workspace/examples/io/src/write.rs
+++ rust/workspace/examples/io/src/write.rs
@@ -1,5 +1,6 @@
 pub fn test() {
-    test1();
+    // test1();
+    test2();
 }
 
 fn test1() {
@@ -12,5 +13,17 @@ fn test1() {
     // println!("{:?}", a);
     let _ = s.write(b" world!");
     println!("{:?}", s);
+    println!("{:?}", a);
+}
+
+fn test2() {
+    use std::io::{Cursor, Write};
+
+    let mut a = [0; 16];
+    // let s = &mut a[..];
+    // let mut c = Cursor::new(s);
+    let mut c = Cursor::new(&mut a[..]);
+    let _ = c.write(b"hello");
+    // println!("{:?}", s);
     println!("{:?}", a);
 }
