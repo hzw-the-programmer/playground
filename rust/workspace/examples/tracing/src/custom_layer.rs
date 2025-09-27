d29e1ba135c35438e4f35685f364d689b14c0834
hezhiwen <hezhiwen.coder@bytedance.com>
2025-09-27 20:57:21 +0800

    tracing

--- rust/workspace/examples/tracing/src/custom_layer.rs
+++ rust/workspace/examples/tracing/src/custom_layer.rs
@@ -24,43 +24,60 @@ where
             println!("  target={}", span.metadata().target());
         }
     }
+
+        fn new_span(
+        &self,
+        attrs: &tracing::span::Attributes<'_>,
+        id: &tracing::span::Id,
+        ctx: tracing_subscriber::layer::Context<'_, S>,
+    ) {
+        let span = ctx.span(id).unwrap();
+        println!("Got new_span!");
+        println!("  level={:?}", span.metadata().level());
+        println!("  target={:?}", span.metadata().target());
+        println!("  name={:?}", span.metadata().name());
+
+        // Our old friend, `println!` exploration.
+        let mut visitor = PrintlnVisitor;
+        attrs.record(&mut visitor);
+    }
 }
 
-// struct PrintlnVisitor;
+struct PrintlnVisitor;
 
-// impl tracing::field::Visit for PrintlnVisitor {
-//     fn record_f64(&mut self, field: &tracing::field::Field, value: f64) {
-//         println!("  field={} value={}", field.name(), value)
-//     }
+impl tracing::field::Visit for PrintlnVisitor {
+    fn record_f64(&mut self, field: &tracing::field::Field, value: f64) {
+        println!("  field={} value={}", field.name(), value)
+    }
 
-//     fn record_i64(&mut self, field: &tracing::field::Field, value: i64) {
-//         println!("  field={} value={}", field.name(), value)
-//     }
+    fn record_i64(&mut self, field: &tracing::field::Field, value: i64) {
+        println!("  field={} value={}", field.name(), value)
+    }
 
-//     fn record_u64(&mut self, field: &tracing::field::Field, value: u64) {
-//         println!("  field={} value={}", field.name(), value)
-//     }
+    fn record_u64(&mut self, field: &tracing::field::Field, value: u64) {
+        println!("  field={} value={}", field.name(), value)
+    }
 
-//     fn record_bool(&mut self, field: &tracing::field::Field, value: bool) {
-//         println!("  field={} value={}", field.name(), value)
-//     }
+    fn record_bool(&mut self, field: &tracing::field::Field, value: bool) {
+        println!("  field={} value={}", field.name(), value)
+    }
 
-//     fn record_str(&mut self, field: &tracing::field::Field, value: &str) {
-//         println!("  field={} value={}", field.name(), value)
-//     }
+    fn record_str(&mut self, field: &tracing::field::Field, value: &str) {
+        println!("  field={} value={}", field.name(), value)
+    }
 
-//     fn record_error(
-//         &mut self,
-//         field: &tracing::field::Field,
-//         value: &(dyn std::error::Error + 'static),
-//     ) {
-//         println!("  field={} value={}", field.name(), value)
-//     }
+    fn record_error(
+        &mut self,
+        field: &tracing::field::Field,
+        value: &(dyn std::error::Error + 'static),
+    ) {
+        println!("  field={} value={}", field.name(), value)
+    }
 
-//     fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
-//         println!("  field={} value={:?}", field.name(), value)
-//     }
-// }
+    fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
+        println!("  field={} value={:?}", field.name(), value)
+    }
+}
 
 // use std::collections::BTreeMap;
 
