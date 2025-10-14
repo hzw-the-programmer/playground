0f2d039bc142bb1b52636e524df1d5d107ff2222
hezhiwen <hezhiwen.coder@bytedance.com>
2025-10-14 20:38:38 +0800

    current thread runtime

--- rust/workspace/examples/tokio-test/src/current_thread.rs
+++ rust/workspace/examples/tokio-test/src/current_thread.rs
@@ -1,8 +1,11 @@
-use tokio::runtime::Builder;
+use tokio::runtime::{Builder, Handle};
 // RUSTFLAGS="--cfg tokio_unstable" cargo run
 
 pub fn test() {
-    test1();
+    // test1();
+    // test2();
+    // test3();
+    test4();
 }
 
 fn test1() {
@@ -10,5 +13,25 @@ fn test1() {
     let metrics = rt.metrics();
     println!("{}", metrics.remote_schedule_count());
     rt.spawn(async {});
+    println!("{}", metrics.remote_schedule_count());
+}
+
+fn test2() {
+    // there is no reactor running, must be called from the context of a Tokio 1.x runtime
+    let handle = Handle::current();
+}
+
+fn test3() {
+    let rt = Builder::new_current_thread().build().unwrap();
+    // there is no reactor running, must be called from the context of a Tokio 1.x runtime
+    let handle = Handle::current();
+}
+
+fn test4() {
+    let rt = Builder::new_current_thread().build().unwrap();
+    let _guard = rt.enter();
+    let handle = Handle::current();
+    rt.spawn(async {});
+    let metrics = rt.metrics();
     println!("{}", metrics.remote_schedule_count());
 }
