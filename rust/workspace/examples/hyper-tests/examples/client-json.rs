5cd8a4738cf411edcf4e4384118dccc15842065d
hezhiwen <hezhiwen.coder@bytedance.com>
2026-03-31 21:12:58 +0800

    client json

---
+++ rust/workspace/examples/hyper-tests/examples/client-json.rs
@@ -0,0 +1,55 @@
+use bytes::Bytes;
+use http_body_util::{BodyExt, Empty};
+use hyper::{Request, body::Buf};
+use hyper_util::rt::TokioIo;
+use serde::Deserialize;
+use tokio::net::TcpStream;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = "http://jsonplaceholder.typicode.com/users".parse()?;
+    let users = fetch_json(url).await?;
+    println!("users: {:#?}", users);
+    let sum = users.iter().fold(0, |acc, user| acc + user.id);
+    println!("sum of ids: {}", sum);
+    Ok(())
+}
+
+async fn fetch_json(url: hyper::Uri) -> Result<Vec<User>> {
+    let host = url.host().expect("uri has no host");
+    let port = url.port_u16().unwrap_or(80);
+    let addr = format!("{host}:{port}");
+
+    let stream = TcpStream::connect(addr).await?;
+    let io = TokioIo::new(stream);
+
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+    tokio::spawn(async move {
+        if let Err(err) = conn.await {
+            eprintln!("Connection failed: {:?}", err);
+        }
+    });
+
+    let authority = url.authority().unwrap().clone();
+    let req = Request::builder()
+        .uri(url)
+        .header(hyper::header::HOST, authority.as_str())
+        .body(Empty::<Bytes>::new())?;
+
+    let res = sender.send_request(req).await?;
+
+    let body = res.collect().await?.aggregate();
+
+    let users = serde_json::from_reader(body.reader())?;
+
+    Ok(users)
+}
+
+#[derive(Deserialize, Debug)]
+struct User {
+    id: i32,
+    #[allow(unused)]
+    name: String,
+}
