09dc61857a2fbc2857005f5b2745bd88208ce18c
hezhiwen <hezhiwen.coder@bytedance.com>
2026-04-04 21:54:03 +0800

    form

---
+++ rust/workspace/examples/axum-tests/examples/form.rs
@@ -0,0 +1,59 @@
+use axum::{Router, extract::Form, response::Html, routing::get};
+use serde::Deserialize;
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "form=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(show_form).post(accept_form));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app.layer(TraceLayer::new_for_http()))
+        .await
+        .unwrap();
+}
+
+async fn show_form() -> Html<&'static str> {
+    Html(
+        r#"
+        <!doctype html>
+        <html>
+            <head></head>
+            <body>
+                <form action="/" method="post">
+                    <label for="name">
+                        Enter your name:
+                        <input type="text" name="name">
+                    </label>
+                    <label>
+                        Enter your email:
+                        <input type="text" name="email">
+                    </label>
+
+                    <input type="submit" value="Subscribe!">
+                </form>
+            </body>
+        </html>
+        "#,
+    )
+}
+
+async fn accept_form(Form(input): Form<Input>) {
+    dbg!(input);
+}
+
+#[derive(Deserialize, Debug)]
+struct Input {
+    name: String,
+    email: String,
+}
