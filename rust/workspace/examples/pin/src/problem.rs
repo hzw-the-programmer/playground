5557e99c7a7ebcc38529473b32a23889019ec073
hezhiwen <hezhiwen.coder@bytedance.com>
2026-04-26 16:35:36 +0800

    boxed

--- rust/workspace/examples/pin/src/problem.rs
+++ rust/workspace/examples/pin/src/problem.rs
@@ -30,4 +30,37 @@ pub fn test() {
     println!("b     : {:p}", &b);
     println!("b.data: {:p}", &b.data);
     println!("b.ptr : {:p}\n", b.ptr);
+
+    let mut a = SelfRef {
+        data: "hello".into(),
+        ptr: std::ptr::null(),
+    };
+    a.ptr = &a.data;
+    println!("a     : {:p}", &a);
+    println!("a.data: {:p}", &a.data);
+    println!("a.ptr : {:p}", a.ptr);
+    println!("{}\n", unsafe { &(*a.ptr) });
+
+    let b = a;
+    println!("b     : {:p}", &b);
+    println!("b.data: {:p}", &b.data);
+    println!("b.ptr : {:p}", b.ptr);
+    println!("{}\n", unsafe { &(*b.ptr) });
+
+    let mut a = Box::new(SelfRef {
+        data: "hello".into(),
+        ptr: std::ptr::null(),
+    });
+    a.ptr = &a.data;
+    println!("a     : {:p}", &a);
+    println!("a     : {:p}", a);
+    println!("a.data: {:p}", &a.data);
+    println!("a.ptr : {:p}", a.ptr);
+    println!("{}\n", unsafe { &(*a.ptr) });
+
+    let b = *a;
+    println!("b     : {:p}", &b);
+    println!("b.data: {:p}", &b.data);
+    println!("b.ptr : {:p}", b.ptr);
+    println!("{}\n", unsafe { &(*b.ptr) });
 }
