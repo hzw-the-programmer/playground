4fa28ddccad054e67bfa2f68a94ca48b57135017
hezhiwen <hezhiwen.coder@bytedance.com>
2026-05-09 18:34:30 +0800

    tracing_subscriber

--- rust/workspace/tower-tests/concurrency/src/main.rs
+++ rust/workspace/tower-tests/concurrency/src/main.rs
@@ -7,9 +7,18 @@ use std::{
 
 use futures_util::future::join_all;
 use tower::{Service, ServiceBuilder, ServiceExt, limit::ConcurrencyLimitLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "concurrency=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let svc = ServiceBuilder::new()
         .layer(ConcurrencyLimitLayer::new(2))
         .service(Svc(0));
@@ -39,15 +48,15 @@ impl<Req> Service<Req> for Svc {
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn poll_ready(&mut self, _ctx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        println!("poll_ready: {}", self.0);
+        tracing::debug!("poll_ready: {}", self.0);
         Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, _req: Req) -> Self::Future {
-        println!("call: {}", self.0);
+        tracing::debug!("call: {}", self.0);
         let i = self.0;
         Box::pin(async move {
-            println!("poll {}", i);
+            tracing::debug!("poll {}", i);
             tokio::time::sleep(Duration::from_secs(3)).await;
             Ok(123)
         })
