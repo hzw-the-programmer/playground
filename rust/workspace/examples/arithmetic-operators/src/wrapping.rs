4eb9df63a0d82cc5b8a003f831b2022508b1d24f
hezhiwen <hezhiwen.coder@bytedance.com>
2026-06-15 17:22:31 +0800

    wrapping

--- rust/workspace/examples/arithmetic-operators/src/wrapping.rs
+++ rust/workspace/examples/arithmetic-operators/src/wrapping.rs
@@ -5,14 +5,14 @@ mod tests {
     #[test]
     fn f1() {
         let mut n: u8 = 0;
-        assert_eq!(n.wrapping_sub(1), 255);
-        assert_eq!(n.wrapping_sub(2), 254);
-        assert_eq!(n.wrapping_sub(3), 253);
+        assert_eq!(n.wrapping_sub(1), (0 - 1 + 256) as u8);
+        assert_eq!(n.wrapping_sub(2), (0 - 2 + 256) as u8);
+        assert_eq!(n.wrapping_sub(3), (0 - 3 + 256) as u8);
 
         n = 255;
-        assert_eq!(n.wrapping_add(1), 0);
-        assert_eq!(n.wrapping_add(2), 1);
-        assert_eq!(n.wrapping_add(3), 2);
+        assert_eq!(n.wrapping_add(1), ((255 + 1) & 255) as u8);
+        assert_eq!(n.wrapping_add(2), ((255 + 2) & 255) as u8);
+        assert_eq!(n.wrapping_add(3), ((255 + 3) & 255) as u8);
 
         let mut n: i8 = -128;
         assert_eq!(n.wrapping_sub(1), 127);
